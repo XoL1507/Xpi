abb3d540f15ba7ad263469b59807248dab157aab
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:42:16 +0400

    Initialize chris-fix-ts-ci

---
+++ crates/test-utils/src/authority.rs
@@ -0,0 +1,177 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+use crate::{messages::make_certificates, TEST_COMMITTEE_SIZE};
+use rand::{prelude::StdRng, SeedableRng};
+use std::collections::BTreeMap;
+use std::sync::Arc;
+use std::time::Duration;
+use sui_config::{NetworkConfig, ValidatorInfo};
+use sui_core::{
+    authority_active::{
+        checkpoint_driver::{CheckpointMetrics, CheckpointProcessControl},
+        ActiveAuthority,
+    },
+    authority_aggregator::{AuthAggMetrics, AuthorityAggregator},
+    authority_client::{AuthorityAPI, NetworkAuthorityClient},
+};
+use sui_node::SuiNode;
+use sui_types::{
+    committee::Committee,
+    error::SuiResult,
+    messages::{Transaction, TransactionInfoResponse},
+    object::Object,
+};
+
+/// The default network buffer size of a test authority.
+pub const NETWORK_BUFFER_SIZE: usize = 65_000;
+
+/// Make an authority config for each of the `TEST_COMMITTEE_SIZE` authorities in the test committee.
+pub fn test_authority_configs() -> NetworkConfig {
+    test_and_configure_authority_configs(TEST_COMMITTEE_SIZE)
+}
+
+pub fn test_and_configure_authority_configs(committee_size: usize) -> NetworkConfig {
+    let config_dir = tempfile::tempdir().unwrap().into_path();
+    let rng = StdRng::from_seed([0; 32]);
+    let mut configs = NetworkConfig::generate_with_rng(&config_dir, committee_size, rng);
+    for config in configs.validator_configs.iter_mut() {
+        // Disable gossip by default to reduce non-determinism.
+        // TODO: Once this library is more broadly used, we can make this a config argument.
+        // Note: Enabling this will break checkpoint_catchup test, which needs a way to keep one
+        // authority behind the others.
+        config.enable_gossip = false;
+
+        let parameters = &mut config.consensus_config.as_mut().unwrap().narwhal_config;
+        // NOTE: the following parameters are important to ensure tests run fast. Using the default
+        // Narwhal parameters may result in tests taking >60 seconds.
+        parameters.header_size = 1;
+        parameters.max_header_delay = Duration::from_millis(200);
+        parameters.batch_size = 1;
+        parameters.max_batch_delay = Duration::from_millis(200);
+    }
+    configs
+}
+
+/// Spawn all authorities in the test committee into a separate tokio task.
+pub async fn spawn_test_authorities<I>(objects: I, config: &NetworkConfig) -> Vec<SuiNode>
+where
+    I: IntoIterator<Item = Object> + Clone,
+{
+    let mut handles = Vec::new();
+    for validator in config.validator_configs() {
+        let node = SuiNode::start(validator).await.unwrap();
+        let state = node.state();
+
+        for o in objects.clone() {
+            state.insert_genesis_object(o).await
+        }
+
+        handles.push(node);
+    }
+    handles
+}
+
+/// Spawn checkpoint processes with very short checkpointing intervals.
+pub async fn spawn_checkpoint_processes(
+    aggregator: &AuthorityAggregator<NetworkAuthorityClient>,
+    handles: &[SuiNode],
+) {
+    // Start active part of each authority.
+    for authority in handles {
+        let state = authority.state().clone();
+        let inner_agg = aggregator.clone();
+        let active_state =
+            Arc::new(ActiveAuthority::new_with_ephemeral_storage(state, inner_agg).unwrap());
+        let checkpoint_process_control = CheckpointProcessControl {
+            long_pause_between_checkpoints: Duration::from_millis(10),
+            ..CheckpointProcessControl::default()
+        };
+        let _active_authority_handle = active_state
+            .spawn_checkpoint_process_with_config(
+                checkpoint_process_control,
+                CheckpointMetrics::new_for_tests(),
+            )
+            .await;
+    }
+}
+
+/// Create a test authority aggregator.
+pub fn test_authority_aggregator(
+    config: &NetworkConfig,
+) -> AuthorityAggregator<NetworkAuthorityClient> {
+    let validators_info = config.validator_set();
+    let committee = Committee::new(0, ValidatorInfo::voting_rights(validators_info)).unwrap();
+    let clients: BTreeMap<_, _> = validators_info
+        .iter()
+        .map(|config| {
+            (
+                config.public_key(),
+                NetworkAuthorityClient::connect_lazy(config.network_address()).unwrap(),
+            )
+        })
+        .collect();
+    let metrics = AuthAggMetrics::new(&prometheus::Registry::new());
+    AuthorityAggregator::new(committee, clients, metrics)
+}
+
+/// Get a network client to communicate with the consensus.
+pub fn get_client(config: &ValidatorInfo) -> NetworkAuthorityClient {
+    NetworkAuthorityClient::connect_lazy(config.network_address()).unwrap()
+}
+
+/// Submit a certificate containing only owned-objects to all authorities.
+pub async fn submit_single_owner_transaction(
+    transaction: Transaction,
+    configs: &[ValidatorInfo],
+) -> Vec<TransactionInfoResponse> {
+    let certificate = make_certificates(vec![transaction]).pop().unwrap();
+
+    let mut responses = Vec::new();
+    for config in configs {
+        let client = get_client(config);
+        let reply = client
+            .handle_certificate(certificate.clone())
+            .await
+            .unwrap();
+        responses.push(reply);
+    }
+    responses
+}
+
+/// Keep submitting the certificates of a shared-object transaction until it is sequenced by
+/// at least one consensus node. We use the loop since some consensus protocols (like Tusk)
+/// may drop transactions. The certificate is submitted to every Sui authority.
+pub async fn submit_shared_object_transaction(
+    transaction: Transaction,
+    configs: &[ValidatorInfo],
+) -> Vec<SuiResult<TransactionInfoResponse>> {
+    let certificate = make_certificates(vec![transaction]).pop().unwrap();
+
+    loop {
+        let futures: Vec<_> = configs
+            .iter()
+            .map(|config| {
+                let client = get_client(config);
+                let cert = certificate.clone();
+                async move { client.handle_certificate(cert).await }
+            })
+            .collect();
+
+        let replies: Vec<_> = futures::future::join_all(futures)
+            .await
+            .into_iter()
+            // Remove all `FailedToHearBackFromConsensus` replies. Note that the original Sui error type
+            // `SuiError::FailedToHearBackFromConsensus(..)` is lost when the message is sent through the
+            // network (it is replaced by `RpcError`). As a result, the following filter doesn't work:
+            // `.filter(|result| !matches!(result, Err(SuiError::FailedToHearBackFromConsensus(..))))`.
+            .filter(|result| match result {
+                Err(e) => !e.to_string().contains("deadline has elapsed"),
+                _ => true,
+            })
+            .collect();
+
+        if !replies.is_empty() {
+            break replies;
+        }
+    }
+}
---
+++ explorer/client/src/components/displaybox/DisplayBox.tsx
@@ -0,0 +1,105 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useState, useCallback, useEffect } from 'react';
+
+import {
+    FALLBACK_IMAGE,
+    ImageModClient,
+} from '../../utils/imageModeratorClient';
+import { transformURL } from '../../utils/stringUtils';
+
+import styles from './DisplayBox.module.css';
+
+function DisplayBox({ display }: { display: string }) {
+    const [hasDisplayLoaded, setHasDisplayLoaded] = useState(false);
+    const [hasFailedToLoad, setHasFailedToLoad] = useState(false);
+
+    const [hasImgBeenChecked, setHasImgBeenChecked] = useState(false);
+    const [imgAllowState, setImgAllowState] = useState(false);
+
+    const imageStyle = hasDisplayLoaded ? {} : { display: 'none' };
+    const handleImageLoad = useCallback(() => {
+        setHasDisplayLoaded(true);
+        setHasFailedToLoad(false);
+    }, [setHasDisplayLoaded]);
+
+    useEffect(() => {
+        setHasFailedToLoad(false);
+        setHasImgBeenChecked(false);
+        setImgAllowState(false);
+
+        new ImageModClient()
+            .checkImage(transformURL(display))
+            .then(({ ok }) => {
+                setImgAllowState(ok);
+            })
+            .catch((error) => {
+                console.warn(error);
+                // default to allow, so a broken img check service doesn't break NFT display
+                setImgAllowState(true);
+            })
+            .finally(() => {
+                setHasImgBeenChecked(true);
+            });
+    }, [display]);
+
+    const handleImageFail = useCallback(
+        (error) => {
+            console.log(error);
+            setHasDisplayLoaded(true);
+            setHasFailedToLoad(true);
+        },
+        [setHasFailedToLoad]
+    );
+
+    const loadedWithoutAllowedState = hasDisplayLoaded && !imgAllowState;
+
+    let showAutoModNotice =
+        !hasFailedToLoad && hasImgBeenChecked && !imgAllowState;
+
+    if (loadedWithoutAllowedState && hasImgBeenChecked) {
+        display = FALLBACK_IMAGE;
+        showAutoModNotice = true;
+    }
+
+    if (showAutoModNotice) {
+        return (
+            <div className={styles['display-container']}>
+                {showAutoModNotice && (
+                    <div className={styles.automod} id="modnotice">
+                        NFT image hidden
+                    </div>
+                )}
+            </div>
+        );
+    } else {
+        return (
+            <div className={styles['display-container']}>
+                {!hasDisplayLoaded && (
+                    <div className={styles.imagebox} id="pleaseWaitImage">
+                        image loading...
+                    </div>
+                )}
+                {hasFailedToLoad && (
+                    <div className={styles.imagebox} id="noImage">
+                        No Image was Found
+                    </div>
+                )}
+                {!hasFailedToLoad && (
+                    <img
+                        id="loadedImage"
+                        className={styles.imagebox}
+                        style={imageStyle}
+                        alt="NFT"
+                        src={transformURL(display)}
+                        onLoad={handleImageLoad}
+                        onError={handleImageFail}
+                    />
+                )}
+            </div>
+        );
+    }
+}
+
+export default DisplayBox;
