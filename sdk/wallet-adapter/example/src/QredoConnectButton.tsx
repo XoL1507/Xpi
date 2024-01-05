c1e2c963be3c802eb0caff483b821f8a384c1fc9
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 19:15:10 +0000

    Initialize pc-wallet-ext-apps-709-custodial-connect

---
+++ sdk/wallet-adapter/example/src/QredoConnectButton.tsx
@@ -0,0 +1,48 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useWalletKit, type WalletWithFeatures } from "@mysten/wallet-kit";
+
+type QredoConnectInput = { service: string; apiUrl: string; token: string };
+type QredoConnectFeature = {
+  "qredo:connect": {
+    version: "0.0.1";
+    qredoConnect: (input: QredoConnectInput) => Promise<void>;
+  };
+};
+type QredoConnectWallet = WalletWithFeatures<Partial<QredoConnectFeature>>;
+
+export function QredoConnectButton() {
+  const { wallets } = useWalletKit();
+  // just select the first one, it will probably be SUI until any other wallets implement this feature.
+  // Then a select wallet modal might be useful.
+  const selectedWallet = wallets[0];
+  if (!selectedWallet || !("wallet" in selectedWallet)) {
+    return (
+      <a
+        href="https://chrome.google.com/webstore/detail/sui-wallet/opcgpfmipidbgpenhmajoajpbobppdil"
+        target="_blank"
+      >
+        Install Sui Wallet to stake SUI
+      </a>
+    );
+  }
+  const qredoConnectWallet = selectedWallet.wallet as QredoConnectWallet;
+  return (
+    <button
+      onClick={async () => {
+        try {
+          qredoConnectWallet.features["qredo:connect"]?.qredoConnect({
+            service: "qredo-testing",
+            apiUrl: "apiUrl",
+            token: "aToken",
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      }}
+    >
+      Connect
+    </button>
+  );
+}
