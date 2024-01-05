51f964021e05150a0b7b4cb9302d594df9d2569b
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:30:05 +0400

    Initialize rvt/sui-framework-coverage-scripts

---
+++ apps/wallet/src/ui/index.tsx
@@ -0,0 +1,90 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { GrowthBookProvider } from '@growthbook/growthbook-react';
+import { RpcClientContext } from '@mysten/core';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Fragment } from 'react';
+import { createRoot } from 'react-dom/client';
+import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+import App from './app';
+import { SuiLedgerClientProvider } from './app/components/ledger/SuiLedgerClientProvider';
+import { growthbook } from './app/experimentation/feature-gating';
+import { queryClient } from './app/helpers/queryClient';
+import { useAppSelector } from './app/hooks';
+import { ErrorBoundary } from '_components/error-boundary';
+import { initAppType } from '_redux/slices/app';
+import { getFromLocationSearch } from '_redux/slices/app/AppType';
+import { setAttributes } from '_src/shared/experimentation/features';
+import initSentry from '_src/shared/sentry';
+import store from '_store';
+import { api, thunkExtras } from '_store/thunk-extras';
+
+import './styles/global.scss';
+import '@fontsource/inter/variable.css';
+import '@fontsource/red-hat-mono/variable.css';
+import '_font-icons/output/sui-icons.scss';
+import 'bootstrap-icons/font/bootstrap-icons.scss';
+
+async function init() {
+    if (process.env.NODE_ENV === 'development') {
+        Object.defineProperty(window, 'store', { value: store });
+    }
+    store.dispatch(initAppType(getFromLocationSearch(window.location.search)));
+    await thunkExtras.background.init(store.dispatch);
+    const { apiEnv, customRPC } = store.getState().app;
+    setAttributes(growthbook, { apiEnv, customRPC });
+}
+
+function renderApp() {
+    const rootDom = document.getElementById('root');
+    if (!rootDom) {
+        throw new Error('Root element not found');
+    }
+    const root = createRoot(rootDom);
+    root.render(
+        <Provider store={store}>
+            <AppWrapper />
+        </Provider>
+    );
+}
+
+function AppWrapper() {
+    const network = useAppSelector(
+        ({ app: { apiEnv, customRPC } }) => `${apiEnv}_${customRPC}`
+    );
+
+    // NOTE: We set a top-level key here to force the entire react tree to be re-created when the network changes
+    // so that the RPC client instance (api.instance.fullNode) is updated correctly. In the future, we should look
+    // into making the API provider instance a reactive value and moving it out of the redux-thunk middleware
+    return (
+        <Fragment key={network}>
+            <GrowthBookProvider growthbook={growthbook}>
+                <HashRouter>
+                    <IntlProvider locale={navigator.language}>
+                        <QueryClientProvider client={queryClient}>
+                            <RpcClientContext.Provider
+                                value={api.instance.fullNode}
+                            >
+                                <SuiLedgerClientProvider>
+                                    <ErrorBoundary>
+                                        <App />
+                                    </ErrorBoundary>
+                                </SuiLedgerClientProvider>
+                            </RpcClientContext.Provider>
+                        </QueryClientProvider>
+                    </IntlProvider>
+                </HashRouter>
+            </GrowthBookProvider>
+        </Fragment>
+    );
+}
+
+(async () => {
+    await init();
+    initSentry();
+    renderApp();
+})();
---
+++ crates/sui-indexer/src/apis/event_api.rs
@@ -0,0 +1,92 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::errors::IndexerError;
+use crate::store::IndexerStore;
+use async_trait::async_trait;
+use jsonrpsee::core::RpcResult;
+use jsonrpsee::http_client::HttpClient;
+use jsonrpsee::types::{SubscriptionEmptyError, SubscriptionResult};
+use jsonrpsee::{RpcModule, SubscriptionSink};
+use sui_json_rpc::api::{EventReadApiClient, EventReadApiServer};
+use sui_json_rpc::SuiRpcModule;
+use sui_json_rpc_types::{EventPage, SuiEventFilter};
+use sui_open_rpc::Module;
+use sui_types::event::EventID;
+use sui_types::query::EventQuery;
+
+pub(crate) struct EventReadApi<S> {
+    state: S,
+    fullnode: HttpClient,
+    method_to_be_forwarded: Vec<String>,
+}
+
+impl<S: IndexerStore> EventReadApi<S> {
+    pub fn new(state: S, fullnode_client: HttpClient) -> Self {
+        Self {
+            state,
+            fullnode: fullnode_client,
+            // TODO: read from centralized config
+            method_to_be_forwarded: vec![],
+        }
+    }
+
+    pub fn get_events(
+        &self,
+        query: EventQuery,
+        cursor: Option<EventID>,
+        limit: Option<usize>,
+        descending_order: Option<bool>,
+    ) -> Result<EventPage, IndexerError> {
+        self.state
+            .get_events(query, cursor, limit, descending_order.unwrap_or_default())
+    }
+}
+
+#[async_trait]
+impl<S> EventReadApiServer for EventReadApi<S>
+where
+    S: IndexerStore + Sync + Send + 'static,
+{
+    async fn get_events(
+        &self,
+        query: EventQuery,
+        // exclusive cursor if `Some`, otherwise start from the beginning
+        cursor: Option<EventID>,
+        limit: Option<usize>,
+        descending_order: Option<bool>,
+    ) -> RpcResult<EventPage> {
+        if self
+            .method_to_be_forwarded
+            .contains(&"get_events".to_string())
+        {
+            return self
+                .fullnode
+                .get_events(query, cursor, limit, descending_order)
+                .await;
+        }
+        Ok(self.get_events(query, cursor, limit, descending_order)?)
+    }
+
+    fn subscribe_event(
+        &self,
+        mut _sink: SubscriptionSink,
+        _filter: SuiEventFilter,
+    ) -> SubscriptionResult {
+        // subscription not supported by subscription yet
+        Err(SubscriptionEmptyError)
+    }
+}
+
+impl<S> SuiRpcModule for EventReadApi<S>
+where
+    S: IndexerStore + Sync + Send + 'static,
+{
+    fn rpc(self) -> RpcModule<Self> {
+        self.into_rpc()
+    }
+
+    fn rpc_doc_module() -> Module {
+        sui_json_rpc::api::EventReadApiOpenRpc::module_doc()
+    }
+}
