0e70576a43106c5df97a53dfd64e14afcaab6cd7
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 18:59:54 +0000

    Initialize perf-03-27

---
+++ sdk/typescript/test/e2e/read-transactions.test.ts
@@ -0,0 +1,78 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { describe, it, expect, beforeAll } from 'vitest';
+import {
+  getTransactionDigest,
+  getTransactionKind,
+  SuiTransactionResponse,
+} from '../../src';
+import { executePaySuiNTimes, setup, TestToolbox } from './utils/setup';
+
+describe('Transaction Reading API', () => {
+  let toolbox: TestToolbox;
+  let transactions: SuiTransactionResponse[];
+  const NUM_TRANSACTIONS = 10;
+
+  beforeAll(async () => {
+    toolbox = await setup();
+    transactions = await executePaySuiNTimes(toolbox.signer, NUM_TRANSACTIONS);
+  });
+
+  it('Get Total Transactions', async () => {
+    const numTransactions = await toolbox.provider.getTotalTransactionNumber();
+    expect(numTransactions).toBeGreaterThan(0);
+  });
+
+  it('Get Transaction', async () => {
+    const digest = transactions[0].digest;
+    const txn = await toolbox.provider.getTransaction({ digest });
+    expect(getTransactionDigest(txn)).toEqual(digest);
+  });
+
+  it('Multi Get Pay Transactions', async () => {
+    const digests = transactions.map((t) => t.digest);
+    const txns = await toolbox.provider.multiGetTransactions({
+      digests,
+      options: { showBalanceChanges: true },
+    });
+    txns.forEach((txn, i) => {
+      expect(getTransactionDigest(txn)).toEqual(digests[i]);
+      expect(txn.balanceChanges?.length).toEqual(2);
+    });
+  });
+
+  it('Query Transactions with opts', async () => {
+    const options = { showEvents: true, showEffects: true };
+    const resp = await toolbox.provider.queryTransactions({
+      options,
+      limit: 1,
+    });
+    const digest = resp.data[0].digest;
+    const response2 = await toolbox.provider.getTransaction({
+      digest,
+      options,
+    });
+    expect(resp.data[0]).toEqual(response2);
+  });
+
+  it('Get Transactions', async () => {
+    const allTransactions = await toolbox.provider.queryTransactions({
+      limit: 10,
+    });
+    expect(allTransactions.data.length).to.greaterThan(0);
+  });
+
+  it('Genesis exists', async () => {
+    const allTransactions = await toolbox.provider.queryTransactions({
+      limit: 1,
+      order: 'ascending',
+    });
+    const resp = await toolbox.provider.getTransaction({
+      digest: allTransactions.data[0].digest,
+      options: { showInput: true },
+    });
+    const txKind = getTransactionKind(resp)!;
+    expect(txKind.kind === 'Genesis').toBe(true);
+  });
+});
