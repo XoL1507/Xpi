a375d73260edac16bbb2618b2ae9febe1c2f080d
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:02:23 +0000

    Initialize ds/airdrop

---
+++ apps/explorer/src/ui/ObjectVideoImage.tsx
@@ -0,0 +1,91 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { MediaPlay16 } from '@mysten/icons';
+import { cva, type VariantProps } from 'class-variance-authority';
+import clsx from 'clsx';
+
+import { ObjectModal } from '~/ui/Modal/ObjectModal';
+import { Image } from '~/ui/image/Image';
+
+const imageStyles = cva(['z-0 flex-shrink-0 relative'], {
+	variants: {
+		variant: {
+			xs: 'h-8 w-8',
+			small: 'h-16 w-16',
+			medium: 'md:h-31.5 md:w-31.5 h-16 w-16',
+			large: 'h-50 w-50',
+		},
+		disablePreview: {
+			true: '',
+			false: 'cursor-pointer',
+		},
+	},
+	defaultVariants: {
+		disablePreview: false,
+	},
+});
+
+type ImageStylesProps = VariantProps<typeof imageStyles>;
+
+interface Props extends ImageStylesProps {
+	title: string;
+	subtitle: string;
+	src: string;
+	open?: boolean;
+	setOpen?: (open: boolean) => void;
+	video?: string | null;
+	disablePreview?: boolean;
+}
+
+export function ObjectVideoImage({
+	title,
+	subtitle,
+	src,
+	video,
+	variant,
+	open,
+	setOpen,
+	disablePreview,
+}: Props) {
+	const close = () => {
+		if (disablePreview) {
+			return;
+		}
+
+		if (setOpen) {
+			setOpen(false);
+		}
+	};
+	const openPreview = () => {
+		if (disablePreview) {
+			return;
+		}
+
+		if (setOpen) {
+			setOpen(true);
+		}
+	};
+
+	return (
+		<>
+			<ObjectModal
+				open={!!open}
+				onClose={close}
+				title={title}
+				subtitle={subtitle}
+				src={src}
+				video={video}
+				alt={title}
+			/>
+			<div className={imageStyles({ variant, disablePreview })}>
+				<Image rounded="md" onClick={openPreview} alt={title} src={src} />
+				{video && (
+					<div className="pointer-events-none absolute bottom-2 right-2 z-10 flex items-center justify-center rounded-full opacity-80">
+						<MediaPlay16 className={clsx(variant === 'large' ? 'h-8 w-8' : 'h-5 w-5')} />
+					</div>
+				)}
+			</div>
+		</>
+	);
+}
---
+++ apps/wallet/src/ui/app/components/accounts/AccountsList.tsx
@@ -0,0 +1,85 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Filter16, Plus12 } from '@mysten/icons';
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import { useMemo } from 'react';
+import { AccountListItem } from './AccountListItem';
+import { FooterLink } from './FooterLink';
+import { useActiveAccount } from '../../hooks/useActiveAccount';
+import { useBackgroundClient } from '../../hooks/useBackgroundClient';
+import { Heading } from '../../shared/heading';
+
+import { ampli } from '_src/shared/analytics/ampli';
+import { useAccounts } from '_src/ui/app/hooks/useAccounts';
+import { Collapsible } from '_src/ui/app/shared/collapse';
+
+export function AccountsList() {
+	const { data: accounts } = useAccounts();
+	const activeAccount = useActiveAccount();
+	const backgroundClient = useBackgroundClient();
+
+	// todo: these will be grouped by account type
+	const otherAccounts = useMemo(
+		() => accounts?.filter((a) => a.id !== activeAccount?.id) || [],
+		[accounts, activeAccount?.id],
+	);
+
+	const handleSelectAccount = async (accountID: string) => {
+		const account = accounts?.find((a) => a.id === accountID);
+		if (!account) return;
+		if (accountID !== activeAccount?.id) {
+			ampli.switchedAccount({
+				toAccountType: account.type,
+			});
+			await backgroundClient.selectAccount(accountID);
+		}
+	};
+	if (!accounts || !activeAccount) return null;
+
+	return (
+		<div className="bg-gradients-graph-cards flex flex-col rounded-xl p-4 gap-5 border border-solid border-hero/10 w-full select-none">
+			<Heading variant="heading5" weight="semibold" color="steel-darker">
+				Accounts
+			</Heading>
+
+			<ToggleGroup.Root
+				asChild
+				value={activeAccount.id}
+				type="single"
+				onValueChange={handleSelectAccount}
+			>
+				<>
+					<Collapsible title="Current" defaultOpen shade="darker">
+						<ToggleGroup.Item asChild value={activeAccount.id}>
+							<div>
+								<AccountListItem account={activeAccount} />
+							</div>
+						</ToggleGroup.Item>
+					</Collapsible>
+
+					{otherAccounts.length ? (
+						<Collapsible title="Switch To" shade="darker">
+							<div className="flex flex-col gap-3">
+								{otherAccounts.map((account) => {
+									return (
+										<ToggleGroup.Item asChild key={account.id} value={account.id}>
+											<div>
+												<AccountListItem account={account} />
+											</div>
+										</ToggleGroup.Item>
+									);
+								})}
+							</div>
+						</Collapsible>
+					) : null}
+				</>
+			</ToggleGroup.Root>
+
+			<div className="flex justify-between">
+				<FooterLink color="steelDarker" icon={<Filter16 />} to="/accounts/manage" text="Manage" />
+				<FooterLink color="steelDarker" icon={<Plus12 />} to="/accounts/add-account" text="Add" />
+			</div>
+		</div>
+	);
+}
