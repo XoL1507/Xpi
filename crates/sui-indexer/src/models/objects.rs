2be781e2d6dbbe7b7eff03acf678b3225a5f1413
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:25:53 +0400

    Initialize andrey-137

---
+++ crates/sui-indexer/src/models/objects.rs
@@ -0,0 +1,59 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::models::owners::OwnerType;
+use crate::schema::objects;
+use diesel::prelude::*;
+use sui_json_rpc_types::SuiObjectData;
+use sui_types::object::Owner;
+
+#[derive(Queryable, Insertable, Debug, Identifiable, Clone)]
+#[diesel(primary_key(object_id))]
+pub struct Object {
+    #[diesel(deserialize_as = i64)]
+    pub id: Option<i64>,
+    pub object_id: String,
+    pub version: i64,
+    pub owner_type: OwnerType,
+    pub owner_address: Option<String>,
+    pub initial_shared_version: Option<i64>,
+    pub package_id: String,
+    pub transaction_module: String,
+    pub object_type: Option<String>,
+    pub object_status: String,
+}
+
+impl From<SuiObjectData> for Object {
+    fn from(o: SuiObjectData) -> Self {
+        let (owner_type, owner_address, initial_shared_version) =
+            owner_to_owner_info(&o.owner.expect("Expect the object type to be non-empty"));
+        Object {
+            id: None,
+            object_id: o.object_id.to_string(),
+            version: o.version.value() as i64,
+            owner_type,
+            owner_address,
+            initial_shared_version,
+            package_id: "".to_string(),
+            transaction_module: "".to_string(),
+            object_type: o.type_,
+            object_status: "".to_string(),
+        }
+    }
+}
+
+// return owner_type, owner_address and initial_shared_version
+pub fn owner_to_owner_info(owner: &Owner) -> (OwnerType, Option<String>, Option<i64>) {
+    match owner {
+        Owner::AddressOwner(address) => (OwnerType::AddressOwner, Some(address.to_string()), None),
+        Owner::ObjectOwner(address) => (OwnerType::ObjectOwner, Some(address.to_string()), None),
+        Owner::Shared {
+            initial_shared_version,
+        } => (
+            OwnerType::Shared,
+            None,
+            Some(initial_shared_version.value() as i64),
+        ),
+        Owner::Immutable => (OwnerType::Shared, None, None),
+    }
+}
---
+++ crates/sui-types/src/collection_types.rs
@@ -0,0 +1,91 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use crate::base_types::{ObjectID, SuiAddress};
+
+/// Rust version of the Move sui::vec_map::VecMap type
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, JsonSchema)]
+pub struct VecMap<K, V> {
+    pub contents: Vec<Entry<K, V>>,
+}
+
+/// Rust version of the Move sui::vec_map::Entry type
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, JsonSchema)]
+pub struct Entry<K, V> {
+    pub key: K,
+    pub value: V,
+}
+
+/// Rust version of the Move sui::vec_set::VecSet type
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, JsonSchema)]
+pub struct VecSet<T> {
+    contents: Vec<T>,
+}
+
+/// Rust version of the Move std::option::Option type.
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, JsonSchema)]
+pub struct MoveOption<T> {
+    pub vec: Vec<T>,
+}
+
+impl<T> MoveOption<T> {
+    pub fn empty() -> Self {
+        Self { vec: vec![] }
+    }
+}
+
+/// Rust version of the Move sui::table::Table type.
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, JsonSchema)]
+pub struct TableVec {
+    pub contents: Table,
+}
+
+impl Default for TableVec {
+    fn default() -> Self {
+        TableVec {
+            contents: Table {
+                id: ObjectID::from(SuiAddress::ZERO),
+                size: 0,
+            },
+        }
+    }
+}
+
+/// Rust version of the Move sui::table::Table type.
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, JsonSchema)]
+pub struct Table {
+    pub id: ObjectID,
+    pub size: u64,
+}
+
+impl Default for Table {
+    fn default() -> Self {
+        Table {
+            id: ObjectID::from(SuiAddress::ZERO),
+            size: 0,
+        }
+    }
+}
+
+/// Rust version of the Move sui::linked_table::LinkedTable type.
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, JsonSchema)]
+pub struct LinkedTable<K> {
+    pub id: ObjectID,
+    pub size: u64,
+    pub head: MoveOption<K>,
+    pub tail: MoveOption<K>,
+}
+
+impl<K> Default for LinkedTable<K> {
+    fn default() -> Self {
+        LinkedTable {
+            id: ObjectID::from(SuiAddress::ZERO),
+            size: 0,
+            head: MoveOption { vec: vec![] },
+            tail: MoveOption { vec: vec![] },
+        }
+    }
+}
