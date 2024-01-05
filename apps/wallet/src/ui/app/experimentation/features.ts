13a73b572910b9649f40db5226eaa30780f728d4
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:51:07 +0400

    Initialize mlogan-fix-indexing-squash

---
+++ apps/wallet/src/ui/app/experimentation/features.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * This is a list of feature keys that are used in
+ * in https://docs.growthbook.io/app/features#feature-keys
+ */
+export enum FEATURES {
+    DEPRECATE_GATEWAY = 'deprecate-gateway',
+    RPC_API_VERSION = 'rpc-api-version',
+    SUI_DENOMINATION = 'sui-denomination',
+}
