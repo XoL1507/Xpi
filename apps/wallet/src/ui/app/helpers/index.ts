85ad7ef40dee33b2dbcc8c4e6bf4a94a772b2608
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:14:26 +0400

    Initialize ignore-tx-and-disallow-obj-as-type-arg

---
+++ apps/wallet/src/ui/app/helpers/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+export { default as formatDate } from './formatDate';
+export { default as notEmpty } from './notEmptyCheck';
+export { parseAmount } from './parseAmount';
