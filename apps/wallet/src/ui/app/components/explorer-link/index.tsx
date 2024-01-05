49f402fee47021f551819cea03c57d49d4c08d0f
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:24:47 +0000

    Initialize oracle

---
+++ apps/wallet/src/ui/app/components/explorer-link/index.tsx
@@ -0,0 +1,43 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { ArrowUpRight16 } from '@mysten/icons';
+
+import { type ExplorerLinkConfig, useExplorerLink } from '../../hooks/useExplorerLink';
+import ExternalLink from '_components/external-link';
+
+import type { ReactNode } from 'react';
+
+import st from './ExplorerLink.module.scss';
+
+export type ExplorerLinkProps = ExplorerLinkConfig & {
+	track?: boolean;
+	children?: ReactNode;
+	className?: string;
+	title?: string;
+	showIcon?: boolean;
+};
+
+function ExplorerLink({
+	track,
+	children,
+	className,
+	title,
+	showIcon,
+	...linkConfig
+}: ExplorerLinkProps) {
+	const explorerHref = useExplorerLink(linkConfig);
+	if (!explorerHref) {
+		return null;
+	}
+
+	return (
+		<ExternalLink href={explorerHref} className={className} title={title}>
+			<>
+				{children} {showIcon && <ArrowUpRight16 className={st.explorerIcon} />}
+			</>
+		</ExternalLink>
+	);
+}
+
+export default ExplorerLink;
