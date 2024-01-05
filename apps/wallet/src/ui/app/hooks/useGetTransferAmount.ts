f17ee40aeca0c73cd75ad778911e1030091182a4
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:01:08 +0000

    Initialize tzakian/tto-v2-part1

---
+++ apps/wallet/src/ui/app/hooks/useGetTransferAmount.ts
@@ -0,0 +1,45 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { SUI_TYPE_ARG } from '@mysten/sui.js/utils';
+import { useMemo } from 'react';
+
+import { getAmount } from '_helpers';
+
+import type { SuiTransactionBlockResponse } from '@mysten/sui.js/client';
+
+export function useGetTransferAmount({
+	txn,
+	activeAddress,
+}: {
+	txn: SuiTransactionBlockResponse;
+	activeAddress: string;
+}) {
+	const { effects, events } = txn;
+	// const { coins } = getEventsSummary(events!, activeAddress);
+
+	const suiTransfer = useMemo(() => {
+		const txdetails = txn.transaction?.data.transaction!;
+		return getAmount(txdetails, effects!, events!)?.map(
+			({ amount, coinType, recipientAddress }) => {
+				return {
+					amount: amount || 0,
+					coinType: coinType || SUI_TYPE_ARG,
+					receiverAddress: recipientAddress,
+				};
+			},
+		);
+	}, [txn, effects, events]);
+
+	// MUSTFIX(chris)
+	// const transferAmount = useMemo(() => {
+	//     return suiTransfer?.length
+	//         ? suiTransfer
+	//         : coins.filter(
+	//               ({ receiverAddress }) => receiverAddress === activeAddress
+	//           );
+	// }, [suiTransfer, coins, activeAddress]);
+
+	// return suiTransfer ?? transferAmount;
+	return suiTransfer;
+}
