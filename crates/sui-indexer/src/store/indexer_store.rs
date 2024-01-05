f0d6adecc0a11746bd0a6a31dc3cec510160bb5a
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:39:41 +0400

    Initialize chris-gas-demo

---
+++ crates/sui-indexer/src/store/indexer_store.rs
@@ -0,0 +1,171 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::errors::IndexerError;
+use crate::models::addresses::Address;
+use crate::models::checkpoints::Checkpoint;
+use crate::models::events::Event;
+use crate::models::objects::{DeletedObject, Object, ObjectStatus};
+use crate::models::owners::ObjectOwner;
+use crate::models::packages::Package;
+use crate::models::transaction_index::{InputObject, MoveCall, Recipient};
+use crate::models::transactions::Transaction;
+use crate::types::SuiTransactionFullResponse;
+use async_trait::async_trait;
+use sui_json_rpc_types::{
+    Checkpoint as RpcCheckpoint, CheckpointId, EventFilter, EventPage, SuiObjectData,
+};
+use sui_types::base_types::{ObjectID, SequenceNumber};
+use sui_types::event::EventID;
+use sui_types::object::ObjectRead;
+
+#[async_trait]
+pub trait IndexerStore {
+    type ModuleCache;
+
+    fn get_latest_checkpoint_sequence_number(&self) -> Result<i64, IndexerError>;
+    fn get_checkpoint(&self, id: CheckpointId) -> Result<Checkpoint, IndexerError>;
+
+    fn get_event(&self, id: EventID) -> Result<Event, IndexerError>;
+    fn get_events(
+        &self,
+        query: EventFilter,
+        cursor: Option<EventID>,
+        limit: Option<usize>,
+        descending_order: bool,
+    ) -> Result<EventPage, IndexerError>;
+
+    fn get_object(
+        &self,
+        object_id: ObjectID,
+        version: Option<SequenceNumber>,
+    ) -> Result<ObjectRead, IndexerError>;
+
+    fn get_total_transaction_number(&self) -> Result<i64, IndexerError>;
+
+    // TODO: combine all get_transaction* methods
+    fn get_transaction_by_digest(&self, txn_digest: &str) -> Result<Transaction, IndexerError>;
+    fn multi_get_transactions_by_digests(
+        &self,
+        txn_digests: &[String],
+    ) -> Result<Vec<Transaction>, IndexerError>;
+
+    fn get_all_transaction_digest_page(
+        &self,
+        start_sequence: Option<i64>,
+        limit: usize,
+        is_descending: bool,
+    ) -> Result<Vec<String>, IndexerError>;
+
+    fn get_transaction_digest_page_by_mutated_object(
+        &self,
+        object_id: String,
+        start_sequence: Option<i64>,
+        limit: usize,
+        is_descending: bool,
+    ) -> Result<Vec<String>, IndexerError>;
+
+    fn get_transaction_digest_page_by_sender_address(
+        &self,
+        sender_address: String,
+        start_sequence: Option<i64>,
+        limit: usize,
+        is_descending: bool,
+    ) -> Result<Vec<String>, IndexerError>;
+
+    fn get_transaction_digest_page_by_recipient_address(
+        &self,
+        recipient_address: String,
+        start_sequence: Option<i64>,
+        limit: usize,
+        is_descending: bool,
+    ) -> Result<Vec<String>, IndexerError>;
+
+    fn get_transaction_digest_page_by_input_object(
+        &self,
+        object_id: String,
+        version: Option<i64>,
+        start_sequence: Option<i64>,
+        limit: usize,
+        is_descending: bool,
+    ) -> Result<Vec<String>, IndexerError>;
+
+    fn get_transaction_digest_page_by_move_call(
+        &self,
+        package: String,
+        module: Option<String>,
+        function: Option<String>,
+        start_sequence: Option<i64>,
+        limit: usize,
+        is_descending: bool,
+    ) -> Result<Vec<String>, IndexerError>;
+
+    fn get_transaction_sequence_by_digest(
+        &self,
+        txn_digest: Option<String>,
+        is_descending: bool,
+    ) -> Result<Option<i64>, IndexerError>;
+
+    fn get_move_call_sequence_by_digest(
+        &self,
+        txn_digest: Option<String>,
+        is_descending: bool,
+    ) -> Result<Option<i64>, IndexerError>;
+
+    fn get_input_object_sequence_by_digest(
+        &self,
+        txn_digest: Option<String>,
+        is_descending: bool,
+    ) -> Result<Option<i64>, IndexerError>;
+
+    fn get_recipient_sequence_by_digest(
+        &self,
+        txn_digest: Option<String>,
+        is_descending: bool,
+    ) -> Result<Option<i64>, IndexerError>;
+
+    fn read_transactions(
+        &self,
+        last_processed_id: i64,
+        limit: usize,
+    ) -> Result<Vec<Transaction>, IndexerError>;
+
+    fn get_total_address_number(&self) -> Result<u64, IndexerError>;
+
+    fn persist_checkpoint(&self, data: &TemporaryCheckpointStore) -> Result<usize, IndexerError>;
+    fn persist_epoch(&self, data: &TemporaryEpochStore) -> Result<(), IndexerError>;
+
+    fn module_cache(&self) -> &Self::ModuleCache;
+}
+
+#[derive(Clone, Debug)]
+pub struct CheckpointData {
+    pub checkpoint: RpcCheckpoint,
+    pub transactions: Vec<SuiTransactionFullResponse>,
+    pub changed_objects: Vec<(ObjectStatus, SuiObjectData)>,
+}
+
+// Per checkpoint indexing
+pub struct TemporaryCheckpointStore {
+    pub checkpoint: Checkpoint,
+    pub transactions: Vec<Transaction>,
+    pub events: Vec<Event>,
+    pub objects_changes: Vec<TransactionObjectChanges>,
+    pub addresses: Vec<Address>,
+    pub packages: Vec<Package>,
+    pub input_objects: Vec<InputObject>,
+    pub move_calls: Vec<MoveCall>,
+    pub recipients: Vec<Recipient>,
+}
+
+#[derive(Debug)]
+pub struct TransactionObjectChanges {
+    pub mutated_objects: Vec<Object>,
+    pub deleted_objects: Vec<DeletedObject>,
+}
+
+// Per epoch indexing
+pub struct TemporaryEpochStore {
+    pub owner_index: Vec<ObjectOwner>,
+    pub epoch_id: u64,
+}
---
+++ sdk/typescript/test/e2e/txn-builder.test.ts
@@ -0,0 +1,162 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { describe, it, expect, beforeAll, beforeEach } from 'vitest';
+import {
+  getExecutionStatusType,
+  getObjectId,
+  getSharedObjectInitialVersion,
+  getTransactionDigest,
+  ObjectId,
+  RawSigner,
+  SuiTransactionResponse,
+  SUI_SYSTEM_STATE_OBJECT_ID,
+  Transaction,
+  SuiObjectData,
+  getCreatedObjects,
+  SUI_CLOCK_OBJECT_ID,
+} from '../../src';
+import {
+  DEFAULT_RECIPIENT,
+  DEFAULT_GAS_BUDGET,
+  setup,
+  TestToolbox,
+  publishPackage,
+} from './utils/setup';
+
+describe('Transaction Builders', () => {
+  let toolbox: TestToolbox;
+  let packageId: ObjectId;
+  let publishTxn: SuiTransactionResponse;
+  let sharedObjectId: ObjectId;
+
+  beforeAll(async () => {
+    const packagePath = __dirname + '/./data/serializer';
+    ({ packageId, publishTxn } = await publishPackage(packagePath));
+    const sharedObject = getCreatedObjects(publishTxn)!.filter(
+      (o) => getSharedObjectInitialVersion(o.owner) !== undefined,
+    )[0];
+    sharedObjectId = getObjectId(sharedObject);
+  });
+
+  beforeEach(async () => {
+    toolbox = await setup();
+  });
+
+  it('SplitCoin + TransferObjects', async () => {
+    const coins = await toolbox.getGasObjectsOwnedByAddress();
+    const tx = new Transaction();
+    const coin_0 = coins[0].details as SuiObjectData;
+
+    const coin = tx.splitCoin(
+      tx.object(coin_0.objectId),
+      tx.pure(DEFAULT_GAS_BUDGET * 2),
+    );
+    tx.transferObjects([coin], tx.pure(toolbox.address()));
+    await validateTransaction(toolbox.signer, tx);
+  });
+
+  it('MergeCoins', async () => {
+    const coins = await toolbox.getGasObjectsOwnedByAddress();
+    const coin_0 = coins[0].details as SuiObjectData;
+    const coin_1 = coins[1].details as SuiObjectData;
+    const tx = new Transaction();
+    tx.mergeCoins(tx.object(coin_0.objectId), [tx.object(coin_1.objectId)]);
+    await validateTransaction(toolbox.signer, tx);
+  });
+
+  it('MoveCall', async () => {
+    const tx = new Transaction();
+    tx.moveCall({
+      target: '0x2::devnet_nft::mint',
+      arguments: [
+        tx.pure('Example NFT'),
+        tx.pure('An NFT created by the wallet Command Line Tool'),
+        tx.pure(
+          'ipfs://bafkreibngqhl3gaa7daob4i2vccziay2jjlp435cf66vhono7nrvww53ty',
+        ),
+      ],
+    });
+    tx.setGasBudget(1000);
+    await validateTransaction(toolbox.signer, tx);
+  });
+
+  it('MoveCall Shared Object', async () => {
+    const coins = await toolbox.getGasObjectsOwnedByAddress();
+    const coin_2 = coins[2].details as SuiObjectData;
+
+    const [{ suiAddress: validatorAddress }] =
+      await toolbox.getActiveValidators();
+
+    const tx = new Transaction();
+    tx.moveCall({
+      target: '0x2::sui_system::request_add_stake',
+      arguments: [
+        tx.object(SUI_SYSTEM_STATE_OBJECT_ID),
+        tx.object(coin_2.objectId),
+        tx.pure(validatorAddress),
+      ],
+    });
+
+    await validateTransaction(toolbox.signer, tx);
+  });
+
+  it('SplitCoin from gas object + TransferObjects', async () => {
+    const tx = new Transaction();
+    const coin = tx.splitCoin(tx.gas, tx.pure(1));
+    tx.transferObjects([coin], tx.pure(DEFAULT_RECIPIENT));
+    await validateTransaction(toolbox.signer, tx);
+  });
+
+  it('TransferObjects gas object', async () => {
+    const tx = new Transaction();
+    tx.transferObjects([tx.gas], tx.pure(DEFAULT_RECIPIENT));
+    await validateTransaction(toolbox.signer, tx);
+  });
+
+  it('TransferObject', async () => {
+    const coins = await toolbox.getGasObjectsOwnedByAddress();
+    const tx = new Transaction();
+    const coin_0 = coins[2].details as SuiObjectData;
+
+    tx.transferObjects(
+      [tx.object(coin_0.objectId)],
+      tx.pure(DEFAULT_RECIPIENT),
+    );
+    await validateTransaction(toolbox.signer, tx);
+  });
+
+  it('Move Shared Object Call with mixed usage of mutable and immutable references', async () => {
+    const tx = new Transaction();
+    tx.moveCall({
+      target: `${packageId}::serializer_tests::value`,
+      arguments: [tx.object(sharedObjectId)],
+    });
+    tx.moveCall({
+      target: `${packageId}::serializer_tests::set_value`,
+      arguments: [tx.object(sharedObjectId)],
+    });
+    await validateTransaction(toolbox.signer, tx);
+  });
+
+  it('immutable clock', async () => {
+    const tx = new Transaction();
+    tx.moveCall({
+      target: `${packageId}::serializer_tests::use_clock`,
+      arguments: [tx.object(SUI_CLOCK_OBJECT_ID)],
+    });
+    await validateTransaction(toolbox.signer, tx);
+  });
+});
+
+async function validateTransaction(signer: RawSigner, tx: Transaction) {
+  const localDigest = await signer.getTransactionDigest(tx);
+  const result = await signer.signAndExecuteTransaction({
+    transaction: tx,
+    options: {
+      showEffects: true,
+    },
+  });
+  expect(localDigest).toEqual(getTransactionDigest(result));
+  expect(getExecutionStatusType(result)).toEqual('success');
+}
