6cef6aac8aeaa329a148867a0a9a2d18327da319
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:48:50 +0400

    Initialize mlogan-udeps

---
+++ sdk/typescript/src/providers/provider.ts
@@ -0,0 +1,267 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { SignatureScheme } from '../cryptography/publickey';
+import {
+  GetObjectDataResponse,
+  SuiObjectInfo,
+  GatewayTxSeqNumber,
+  GetTxnDigestsResponse,
+  SuiTransactionResponse,
+  SuiObjectRef,
+  SuiMoveFunctionArgTypes,
+  SuiMoveNormalizedFunction,
+  SuiMoveNormalizedStruct,
+  SuiMoveNormalizedModule,
+  SuiMoveNormalizedModules,
+  SuiEventFilter,
+  SuiEventEnvelope,
+  SubscriptionId,
+  ExecuteTransactionRequestType,
+  SuiExecuteTransactionResponse,
+  TransactionDigest,
+  ObjectId,
+  SuiAddress,
+  ObjectOwner,
+  SuiEvents,
+} from '../types';
+
+///////////////////////////////
+// Exported Abstracts
+export abstract class Provider {
+  // Objects
+  /**
+   * Get all objects owned by an address
+   */
+  abstract getObjectsOwnedByAddress(
+    addressOrObjectId: string
+  ): Promise<SuiObjectInfo[]>;
+
+  /**
+   * Convenience method for getting all gas objects(SUI Tokens) owned by an address
+   */
+  abstract getGasObjectsOwnedByAddress(
+    _address: string
+  ): Promise<SuiObjectInfo[]>;
+
+  /**
+   * Convenience method for getting all coins objects owned by an address
+   * @param typeArg optional argument for filter by coin type
+   */
+  abstract getCoinBalancesOwnedByAddress(
+    address: string,
+    typeArg?: string
+  ): Promise<GetObjectDataResponse[]>;
+
+  /**
+   * Get details about an object
+   */
+  abstract getObject(objectId: string): Promise<GetObjectDataResponse>;
+
+  /**
+   * Get object reference(id, tx digest, version id)
+   * @param objectId
+   */
+  abstract getObjectRef(objectId: string): Promise<SuiObjectRef | undefined>;
+
+  // Transactions
+  /**
+   * Get transaction digests for a given range
+   *
+   * NOTE: this method may get deprecated after DevNet
+   */
+  abstract getTransactionDigestsInRange(
+    start: GatewayTxSeqNumber,
+    end: GatewayTxSeqNumber
+  ): Promise<GetTxnDigestsResponse>;
+
+  /**
+   * Get the latest `count` transactions
+   *
+   * NOTE: this method may get deprecated after DevNet
+   */
+  abstract getRecentTransactions(count: number): Promise<GetTxnDigestsResponse>;
+
+  /**
+   * Get total number of transactions
+   * NOTE: this method may get deprecated after DevNet
+   */
+  abstract getTotalTransactionNumber(): Promise<number>;
+
+  abstract executeTransaction(
+    txnBytes: string,
+    signatureScheme: SignatureScheme,
+    signature: string,
+    pubkey: string
+  ): Promise<SuiTransactionResponse>;
+
+  /**
+   * This is under development endpoint on Fullnode that will eventually
+   * replace the other `executeTransaction` that's only available on the
+   * Gateway
+   */
+  abstract executeTransactionWithRequestType(
+    txnBytes: string,
+    signatureScheme: SignatureScheme,
+    signature: string,
+    pubkey: string,
+    requestType: ExecuteTransactionRequestType
+  ): Promise<SuiExecuteTransactionResponse>;
+
+  // Move info
+  /**
+   * Get Move function argument types like read, write and full access
+   */
+  abstract getMoveFunctionArgTypes(
+    objectId: string,
+    moduleName: string,
+    functionName: string
+  ): Promise<SuiMoveFunctionArgTypes>;
+
+  /**
+   * Get a map from module name to
+   * structured representations of Move modules
+   */
+  abstract getNormalizedMoveModulesByPackage(
+    objectId: string
+  ): Promise<SuiMoveNormalizedModules>;
+
+  /**
+   * Get a structured representation of Move module
+   */
+  abstract getNormalizedMoveModule(
+    objectId: string,
+    moduleName: string
+  ): Promise<SuiMoveNormalizedModule>;
+
+  /**
+   * Get a structured representation of Move function
+   */
+  abstract getNormalizedMoveFunction(
+    objectId: string,
+    moduleName: string,
+    functionName: string
+  ): Promise<SuiMoveNormalizedFunction>;
+
+  /**
+   * Get a structured representation of Move struct
+   */
+  abstract getNormalizedMoveStruct(
+    objectId: string,
+    moduleName: string,
+    structName: string
+  ): Promise<SuiMoveNormalizedStruct>;
+
+  abstract syncAccountState(address: string): Promise<any>;
+
+  /**
+   * Get events for one transaction
+   * @param digest transaction digest to search by
+   * @param count max result count
+   */
+  abstract getEventsByTransaction(
+    digest: TransactionDigest,
+    count: number
+  ): Promise<SuiEvents>;
+
+  /**
+   * Get events emitted from within the specified Move module
+   * @param package_ Move package object ID
+   * @param module Move module name
+   * @param count max result count
+   * @param startTime start of time range
+   * @param endTime end of time range, exclusive
+   */
+  abstract getEventsByModule(
+    packageId: ObjectId,
+    module: string,
+    count: number,
+    startTime: number,
+    endTime: number
+  ): Promise<SuiEvents>;
+
+  /**
+   * Get events with a matching Move type name
+   * @param moveEventStructName Move struct type name
+   * @param count max result count
+   * @param startTime start of time range to search
+   * @param endTime end of time range
+   */
+  abstract getEventsByMoveEventStructName(
+    moveEventStructName: string,
+    count: number,
+    startTime: number,
+    endTime: number
+  ): Promise<SuiEvents>;
+
+  /**
+   * Get events from transactions sent by a specific address
+   * @param sender Sui address of the sender of the transaction that generated the event
+   * @param count max result count
+   * @param startTime start of time range to search
+   * @param endTime end of time range
+   */
+  abstract getEventsBySender(
+    sender: SuiAddress,
+    count: number,
+    startTime: number,
+    endTime: number
+  ): Promise<SuiEvents>;
+
+  /**
+   * Get events with a matching recipient
+   * @param recipient object owner that received the transaction that generated the event
+   * @param count max result count
+   * @param startTime start of time range to search
+   * @param endTime end of time range
+   */
+  abstract getEventsByRecipient(
+    recipient: ObjectOwner,
+    count: number,
+    startTime: number,
+    endTime: number
+  ): Promise<SuiEvents>;
+
+  /**
+   * Get events involving the given object
+   * @param object object id created, mutated, or deleted in events
+   * @param count max result count
+   * @param startTime start of time range to search
+   * @param endTime end of time range
+   */
+  abstract getEventsByObject(
+    object: ObjectId,
+    count: number,
+    startTime: number,
+    endTime: number
+  ): Promise<SuiEvents>;
+
+  /**
+   * Get all events within the given time span
+   * @param count max result count
+   * @param startTime start of time range to search
+   * @param endTime end of time range
+   */
+  abstract getEventsByTimeRange(
+    count: number,
+    startTime: number,
+    endTime: number
+  ): Promise<SuiEvents>;
+
+  /**
+   * Subscribe to get notifications whenever an event matching the filter occurs
+   * @param filter - filter describing the subset of events to follow
+   * @param onMessage - function to run when we receive a notification of a new event matching the filter
+   */
+  abstract subscribeEvent(
+    filter: SuiEventFilter,
+    onMessage: (event: SuiEventEnvelope) => void
+  ): Promise<SubscriptionId>;
+
+  /**
+   * Unsubscribe from an event subscription
+   * @param id - subscription id to unsubscribe from (previously received from subscribeEvent)
+   */
+  abstract unsubscribeEvent(id: SubscriptionId): Promise<boolean>;
+  // TODO: add more interface methods
+}
---
+++ sdk/typescript/src/signers/txn-data-serializers/local-txn-data-serializer.ts
@@ -0,0 +1,271 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Base64DataBuffer } from '../../serialization/base64';
+import {
+  bcs,
+  Coin,
+  COIN_JOIN_FUNC_NAME,
+  COIN_MODULE_NAME,
+  COIN_PACKAGE_ID,
+  COIN_SPLIT_VEC_FUNC_NAME,
+  SuiAddress,
+  Transaction,
+  TransactionData,
+  TypeTag,
+} from '../../types';
+import {
+  MoveCallTransaction,
+  MergeCoinTransaction,
+  SplitCoinTransaction,
+  TransferObjectTransaction,
+  TransferSuiTransaction,
+  PublishTransaction,
+  TxnDataSerializer,
+  PayTransaction,
+} from './txn-data-serializer';
+import { Provider } from '../../providers/provider';
+import { CallArgSerializer } from './call-arg-serializer';
+
+const TYPE_TAG = Array.from('TransactionData::').map((e) => e.charCodeAt(0));
+
+export class LocalTxnDataSerializer implements TxnDataSerializer {
+  /**
+   * Need a provider to fetch the latest object reference. Ideally the provider
+   * should cache the object reference locally
+   */
+  constructor(private provider: Provider) {}
+
+  async newTransferObject(
+    signerAddress: SuiAddress,
+    t: TransferObjectTransaction
+  ): Promise<Base64DataBuffer> {
+    try {
+      const objectRef = await this.provider.getObjectRef(t.objectId);
+      const tx = {
+        TransferObject: {
+          recipient: t.recipient,
+          object_ref: objectRef!,
+        },
+      };
+      return await this.constructTransactionData(
+        tx,
+        // TODO: make `gasPayment` a required field in `TransferObjectTransaction`
+        t.gasPayment!,
+        t.gasBudget,
+        signerAddress
+      );
+    } catch (err) {
+      throw new Error(
+        `Error constructing a TransferObject transaction: ${err} args ${JSON.stringify(
+          t
+        )}`
+      );
+    }
+  }
+
+  async newTransferSui(
+    signerAddress: SuiAddress,
+    t: TransferSuiTransaction
+  ): Promise<Base64DataBuffer> {
+    try {
+      const tx = {
+        TransferSui: {
+          recipient: t.recipient,
+          amount: t.amount == null ? { None: null } : { Some: t.amount },
+        },
+      };
+      return await this.constructTransactionData(
+        tx,
+        t.suiObjectId,
+        t.gasBudget,
+        signerAddress
+      );
+    } catch (err) {
+      throw new Error(
+        `Error constructing a TransferSui transaction: ${err} args ${JSON.stringify(
+          t
+        )}`
+      );
+    }
+  }
+
+  async newPay(
+    signerAddress: SuiAddress,
+    t: PayTransaction
+  ): Promise<Base64DataBuffer> {
+    try {
+      const inputCoinRefs = (
+        await Promise.all(
+          t.inputCoins.map((coin) => this.provider.getObjectRef(coin))
+        )
+      ).map((ref) => ref!);
+      const tx = {
+        Pay: {
+          coins: inputCoinRefs,
+          recipients: t.recipients,
+          amounts: t.amounts,
+        },
+      };
+      return await this.constructTransactionData(
+        tx,
+        t.gasPayment!,
+        t.gasBudget,
+        signerAddress
+      );
+    } catch (err) {
+      throw new Error(
+        `Error constructing a Pay transaction: ${err} args ${JSON.stringify(t)}`
+      );
+    }
+  }
+
+  async newMoveCall(
+    signerAddress: SuiAddress,
+    t: MoveCallTransaction
+  ): Promise<Base64DataBuffer> {
+    try {
+      const pkg = await this.provider.getObjectRef(t.packageObjectId);
+      const tx = {
+        Call: {
+          package: pkg!,
+          module: t.module,
+          function: t.function,
+          typeArguments: t.typeArguments as TypeTag[],
+          arguments: await new CallArgSerializer(
+            this.provider
+          ).serializeMoveCallArguments(t),
+        },
+      };
+
+      return await this.constructTransactionData(
+        tx,
+        // TODO: make `gasPayment` a required field in `MoveCallTransaction`
+        t.gasPayment!,
+        t.gasBudget,
+        signerAddress
+      );
+    } catch (err) {
+      throw new Error(
+        `Error constructing a move call: ${err} args ${JSON.stringify(t)}`
+      );
+    }
+  }
+
+  async newMergeCoin(
+    signerAddress: SuiAddress,
+    t: MergeCoinTransaction
+  ): Promise<Base64DataBuffer> {
+    try {
+      return await this.newMoveCall(signerAddress, {
+        packageObjectId: COIN_PACKAGE_ID,
+        module: COIN_MODULE_NAME,
+        function: COIN_JOIN_FUNC_NAME,
+        typeArguments: [await this.getCoinStructTag(t.coinToMerge)],
+        arguments: [t.primaryCoin, t.coinToMerge],
+        gasPayment: t.gasPayment,
+        gasBudget: t.gasBudget,
+      });
+    } catch (err) {
+      throw new Error(
+        `Error constructing a MergeCoin Transaction: ${err} args ${JSON.stringify(
+          t
+        )}`
+      );
+    }
+  }
+
+  async newSplitCoin(
+    signerAddress: SuiAddress,
+    t: SplitCoinTransaction
+  ): Promise<Base64DataBuffer> {
+    try {
+      return await this.newMoveCall(signerAddress, {
+        packageObjectId: COIN_PACKAGE_ID,
+        module: COIN_MODULE_NAME,
+        function: COIN_SPLIT_VEC_FUNC_NAME,
+        typeArguments: [await this.getCoinStructTag(t.coinObjectId)],
+        arguments: [t.coinObjectId, t.splitAmounts],
+        gasPayment: t.gasPayment,
+        gasBudget: t.gasBudget,
+      });
+    } catch (err) {
+      throw new Error(
+        `Error constructing a SplitCoin Transaction: ${err} args ${JSON.stringify(
+          t
+        )}`
+      );
+    }
+  }
+
+  async newPublish(
+    signerAddress: SuiAddress,
+    t: PublishTransaction
+  ): Promise<Base64DataBuffer> {
+    try {
+      const tx = {
+        Publish: {
+          modules: t.compiledModules as ArrayLike<ArrayLike<number>>,
+        },
+      };
+      return await this.constructTransactionData(
+        tx,
+        // TODO: make `gasPayment` a required field in `PublishTransaction`
+        t.gasPayment!,
+        t.gasBudget,
+        signerAddress
+      );
+    } catch (err) {
+      throw new Error(
+        `Error constructing a newPublish transaction: ${err} with args ${JSON.stringify(
+          t
+        )}`
+      );
+    }
+  }
+
+  private async getCoinStructTag(coinId: string): Promise<TypeTag> {
+    const coin = await this.provider.getObject(coinId);
+    const coinTypeArg = Coin.getCoinTypeArg(coin);
+    if (coinTypeArg == null) {
+      throw new Error(`Object ${coinId} is not a valid coin type`);
+    }
+    return { struct: Coin.getCoinStructTag(coinTypeArg) };
+  }
+
+  private async constructTransactionData(
+    tx: Transaction,
+    gasObjectId: string,
+    gasBudget: number,
+    signerAddress: SuiAddress
+  ): Promise<Base64DataBuffer> {
+    // TODO: mark gasPayment as required in `MoveCallTransaction`
+    const gasPayment = await this.provider.getObjectRef(gasObjectId);
+    const txData = {
+      kind: {
+        // TODO: support batch txns
+        Single: tx,
+      },
+      gasPayment: gasPayment!,
+      // Need to keep in sync with
+      // https://github.com/MystenLabs/sui/blob/f32877f2e40d35a008710c232e49b57aab886462/crates/sui-types/src/messages.rs#L338
+      gasPrice: 1,
+      gasBudget: gasBudget,
+      sender: signerAddress,
+    };
+
+    return this.serializeTransactionData(txData);
+  }
+
+  private serializeTransactionData(
+    tx: TransactionData,
+    // TODO: derive the buffer size automatically
+    size: number = 2048
+  ): Base64DataBuffer {
+    const dataBytes = bcs.ser('TransactionData', tx, size).toBytes();
+    const serialized = new Uint8Array(TYPE_TAG.length + dataBytes.length);
+    serialized.set(TYPE_TAG);
+    serialized.set(dataBytes, TYPE_TAG.length);
+    return new Base64DataBuffer(serialized);
+  }
+}
