b7a3d71a5b9afc319951b3ff6926044c85b60df0
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:15:16 +0000

    Initialize order-object-changes

---
+++ apps/wallet/src/ui/app/components/accounts/AccountsList.tsx
@@ -0,0 +1,92 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Filter16, Plus12 } from '@mysten/icons';
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import cn from 'classnames';
+import { useMemo } from 'react';
+import { AccountListItem } from './AccountListItem';
+import { FooterLink } from './FooterLink';
+import { getAccountBackgroundByType } from '../../helpers/accounts';
+import { useAccountGroups } from '../../hooks/useAccountGroups';
+import { useActiveAccount } from '../../hooks/useActiveAccount';
+import { useBackgroundClient } from '../../hooks/useBackgroundClient';
+import { Heading } from '../../shared/heading';
+
+import { ampli } from '_src/shared/analytics/ampli';
+import { Collapsible } from '_src/ui/app/shared/collapse';
+
+export function AccountsList() {
+	const accountGroups = useAccountGroups();
+	const accounts = accountGroups.list();
+	const activeAccount = useActiveAccount();
+	const backgroundClient = useBackgroundClient();
+
+	const otherAccounts = useMemo(
+		() => accounts.filter((a) => a.id !== activeAccount?.id) || [],
+		[accounts, activeAccount?.id],
+	);
+
+	const handleSelectAccount = async (accountID: string) => {
+		const account = accounts?.find((a) => a.id === accountID);
+		if (!account) return;
+		if (accountID !== activeAccount?.id) {
+			ampli.switchedAccount({
+				toAccountType: account.type,
+			});
+			await backgroundClient.selectAccount(accountID);
+		}
+	};
+	if (!accounts || !activeAccount) return null;
+
+	return (
+		<div
+			className={cn(
+				'flex flex-col rounded-xl p-4 gap-5 border border-solid border-hero/10 w-full select-none',
+				getAccountBackgroundByType(activeAccount),
+			)}
+		>
+			<Heading variant="heading5" weight="semibold" color="steel-darker">
+				Accounts
+			</Heading>
+
+			<ToggleGroup.Root
+				asChild
+				value={activeAccount.id}
+				type="single"
+				onValueChange={handleSelectAccount}
+			>
+				<>
+					<Collapsible title="Current" defaultOpen shade="darker">
+						<ToggleGroup.Item asChild value={activeAccount.id}>
+							<div>
+								<AccountListItem account={activeAccount} editable showLock />
+							</div>
+						</ToggleGroup.Item>
+					</Collapsible>
+
+					{otherAccounts.length ? (
+						<Collapsible title="Switch To" shade="darker">
+							<div className="flex flex-col gap-3">
+								{otherAccounts.map((account) => {
+									return (
+										<ToggleGroup.Item asChild key={account.id} value={account.id}>
+											<div>
+												<AccountListItem account={account} showLock />
+											</div>
+										</ToggleGroup.Item>
+									);
+								})}
+							</div>
+						</Collapsible>
+					) : null}
+				</>
+			</ToggleGroup.Root>
+
+			<div className="flex justify-between">
+				<FooterLink color="steelDarker" icon={<Filter16 />} to="/accounts/manage" text="Manage" />
+				<FooterLink color="steelDarker" icon={<Plus12 />} to="/accounts/add-account" text="Add" />
+			</div>
+		</div>
+	);
+}
---
+++ sdk/kiosk/src/tx/transfer-policy.ts
@@ -0,0 +1,114 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { TransactionBlock, TransactionArgument } from '@mysten/sui.js/transactions';
+import { objArg } from '../utils';
+import { ObjectArgument, TRANSFER_POLICY_MODULE, TRANSFER_POLICY_TYPE } from '../types';
+
+/**
+ * Call the `transfer_policy::new` function to create a new transfer policy.
+ * Returns `transferPolicyCap`
+ */
+export function createTransferPolicy(
+	tx: TransactionBlock,
+	itemType: string,
+	publisher: ObjectArgument,
+): TransactionArgument {
+	const [transferPolicy, transferPolicyCap] = createTransferPolicyWithoutSharing(
+		tx,
+		itemType,
+		publisher,
+	);
+
+	shareTransferPolicy(tx, itemType, transferPolicy);
+
+	return transferPolicyCap;
+}
+
+/**
+ * Creates a transfer Policy and returns both the Policy and the Cap.
+ * Used if we want to use the policy before making it a shared object.
+ */
+export function createTransferPolicyWithoutSharing(
+	tx: TransactionBlock,
+	itemType: string,
+	publisher: ObjectArgument,
+): [TransactionArgument, TransactionArgument] {
+	const [transferPolicy, transferPolicyCap] = tx.moveCall({
+		target: `${TRANSFER_POLICY_MODULE}::new`,
+		typeArguments: [itemType],
+		arguments: [objArg(tx, publisher)],
+	});
+
+	return [transferPolicy, transferPolicyCap];
+}
+/**
+ * Converts Transfer Policy to a shared object.
+ */
+export function shareTransferPolicy(
+	tx: TransactionBlock,
+	itemType: string,
+	transferPolicy: TransactionArgument,
+) {
+	tx.moveCall({
+		target: `0x2::transfer::public_share_object`,
+		typeArguments: [`${TRANSFER_POLICY_TYPE}<${itemType}>`],
+		arguments: [transferPolicy],
+	});
+}
+
+/**
+ * Call the `transfer_policy::withdraw` function to withdraw profits from a transfer policy.
+ */
+export function withdrawFromPolicy(
+	tx: TransactionBlock,
+	itemType: string,
+	policy: ObjectArgument,
+	policyCap: ObjectArgument,
+	amount?: string | bigint | null,
+): TransactionArgument {
+	const amountArg = tx.pure(amount ? { Some: amount } : { None: true }, 'Option<u64>');
+
+	const [profits] = tx.moveCall({
+		target: `${TRANSFER_POLICY_MODULE}::withdraw`,
+		typeArguments: [itemType],
+		arguments: [objArg(tx, policy), objArg(tx, policyCap), amountArg],
+	});
+
+	return profits;
+}
+
+/**
+ * Call the `transfer_policy::confirm_request` function to unblock the
+ * transaction.
+ */
+export function confirmRequest(
+	tx: TransactionBlock,
+	itemType: string,
+	policy: ObjectArgument,
+	request: TransactionArgument,
+): void {
+	tx.moveCall({
+		target: `${TRANSFER_POLICY_MODULE}::confirm_request`,
+		typeArguments: [itemType],
+		arguments: [objArg(tx, policy), request],
+	});
+}
+
+/**
+ * Calls the `transfer_policy::remove_rule` function to remove a Rule from the transfer policy's ruleset.
+ */
+export function removeTransferPolicyRule(
+	tx: TransactionBlock,
+	itemType: string,
+	ruleType: string,
+	configType: string,
+	policy: ObjectArgument,
+	policyCap: ObjectArgument,
+): void {
+	tx.moveCall({
+		target: `${TRANSFER_POLICY_MODULE}::remove_rule`,
+		typeArguments: [itemType, ruleType, configType],
+		arguments: [objArg(tx, policy), objArg(tx, policyCap)],
+	});
+}
