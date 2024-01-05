de4ef4113b39e1a0a6607a1526406f0d62f2070a
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:43:08 +0400

    Initialize jnaulty/test-multi-sig

---
+++ apps/wallet/src/shared/messaging/messages/payloads/transactions/ui/TransactionRequestResponse.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { type SuiSignMessageOutput } from '@mysten/wallet-standard';
+
+import { isBasePayload } from '_payloads';
+
+import type { SignedTransaction, SuiTransactionResponse } from '@mysten/sui.js';
+import type { BasePayload, Payload } from '_payloads';
+
+export interface TransactionRequestResponse extends BasePayload {
+    type: 'transaction-request-response';
+    txID: string;
+    approved: boolean;
+    txResult?: SuiTransactionResponse | SuiSignMessageOutput;
+    txResultError?: string;
+    txSigned?: SignedTransaction;
+}
+
+export function isTransactionRequestResponse(
+    payload: Payload
+): payload is TransactionRequestResponse {
+    return (
+        isBasePayload(payload) &&
+        payload.type === 'transaction-request-response'
+    );
+}
