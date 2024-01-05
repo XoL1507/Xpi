1086d27ac54adb7b16bc6574c1483b334cbfc1b3
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:41:03 +0400

    Initialize pc-wallet-ext-2959-home-ui-update

---
+++ explorer/client/src/pages/transaction-result/TransactionResultType.tsx
@@ -0,0 +1,26 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import type {
+    CertifiedTransaction,
+    ExecutionStatusType,
+    SuiObjectRef,
+} from '@mysten/sui.js';
+
+export type DataType = CertifiedTransaction & {
+    loadState: string;
+    txId: string;
+    status: ExecutionStatusType;
+    gasFee: number;
+    txError: string;
+    mutated: SuiObjectRef[];
+    created: SuiObjectRef[];
+    timestamp_ms: number | null;
+};
+
+export type Category =
+    | 'objects'
+    | 'transactions'
+    | 'addresses'
+    | 'ethAddress'
+    | 'unknown';
