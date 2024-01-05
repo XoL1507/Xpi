f414dcbd1fc93e12572ae010c505f78c1ecea8da
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:27:16 +0400

    Initialize andrey-138-strip-none

---
+++ sdk/typescript/test/e2e/coin-read.test.ts
@@ -0,0 +1,78 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { describe, it, expect, beforeAll } from 'vitest';
+import { RawSigner } from '../../src';
+
+import { publishPackage, setup, TestToolbox } from './utils/setup';
+
+describe('CoinRead API', () => {
+  let toolbox: TestToolbox;
+  let signer: RawSigner;
+  let packageId: string;
+  let testType: string;
+
+  beforeAll(async () => {
+    toolbox = await setup();
+    signer = new RawSigner(toolbox.keypair, toolbox.provider);
+    const packagePath = __dirname + '/./data/coin_metadata';
+    packageId = await publishPackage(signer, packagePath);
+    testType = packageId + '::test::TEST';
+  });
+
+  it('Get coins with/without type', async () => {
+    const suiCoins = await toolbox.provider.getCoins({
+      owner: toolbox.address(),
+    });
+    expect(suiCoins.data.length).toEqual(5);
+
+    const testCoins = await toolbox.provider.getCoins({
+      owner: toolbox.address(),
+      coinType: testType,
+    });
+    expect(testCoins.data.length).toEqual(2);
+
+    const allCoins = await toolbox.provider.getAllCoins({
+      owner: toolbox.address(),
+    });
+    expect(allCoins.data.length).toEqual(7);
+    expect(allCoins.nextCursor).toBeNull();
+
+    //test paging with limit
+    const someSuiCoins = await toolbox.provider.getCoins({
+      owner: toolbox.address(),
+      limit: 3,
+    });
+    expect(someSuiCoins.data.length).toEqual(3);
+    expect(someSuiCoins.nextCursor).toBeTruthy();
+  });
+
+  it('Get balance with/without type', async () => {
+    const suiBalance = await toolbox.provider.getBalance({
+      owner: toolbox.address(),
+    });
+    expect(suiBalance.coinType).toEqual('0x2::sui::SUI');
+    expect(suiBalance.coinObjectCount).toEqual(5);
+    expect(suiBalance.totalBalance).toBeGreaterThan(0);
+
+    const testBalance = await toolbox.provider.getBalance({
+      owner: toolbox.address(),
+      coinType: testType,
+    });
+    expect(testBalance.coinType).toEqual(testType);
+    expect(testBalance.coinObjectCount).toEqual(2);
+    expect(testBalance.totalBalance).toEqual(11);
+
+    const allBalances = await toolbox.provider.getAllBalances({
+      owner: toolbox.address(),
+    });
+    expect(allBalances.length).toEqual(2);
+  });
+
+  it('Get total supply', async () => {
+    const testSupply = await toolbox.provider.getTotalSupply({
+      coinType: testType,
+    });
+    expect(testSupply.value).toEqual(11);
+  });
+});
