a6ceb7f151983778009d86d1ba089621baa56be0
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:06:13 +0000

    Initialize APPS-objectVideoImgOrientation

---
+++ apps/explorer/src/components/transactions/ProgTxnBlockCard.tsx
@@ -0,0 +1,53 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import clsx from 'clsx';
+import { type ReactNode } from 'react';
+
+import { ExpandableList, ExpandableListControl, ExpandableListItems } from '~/ui/ExpandableList';
+import { TransactionBlockCard } from '~/ui/TransactionBlockCard';
+
+interface ProgrammableTxnBlockCardProps {
+	items: ReactNode[];
+	itemsLabel: string;
+	defaultItemsToShow: number;
+	noExpandableList?: boolean;
+	count?: number;
+	initialClose?: boolean;
+}
+
+export function ProgrammableTxnBlockCard({
+	items,
+	itemsLabel,
+	defaultItemsToShow,
+	noExpandableList,
+	count,
+	initialClose,
+}: ProgrammableTxnBlockCardProps) {
+	if (!items?.length) {
+		return null;
+	}
+
+	const cardTitle = count ? `${count} ${itemsLabel}` : itemsLabel;
+
+	return (
+		<TransactionBlockCard collapsible initialClose={initialClose} title={cardTitle}>
+			<ExpandableList items={items} defaultItemsToShow={defaultItemsToShow} itemsLabel={itemsLabel}>
+				<div
+					className={clsx(
+						'flex flex-col gap-6 overflow-y-auto',
+						!noExpandableList && 'max-h-[300px]',
+					)}
+				>
+					{noExpandableList ? <>{items}</> : <ExpandableListItems />}
+				</div>
+
+				{items.length > defaultItemsToShow && (
+					<div className="mt-6">
+						<ExpandableListControl />
+					</div>
+				)}
+			</ExpandableList>
+		</TransactionBlockCard>
+	);
+}
