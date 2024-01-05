73b5886241d2fc203f7adecbcd0f975dcc19e42c
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 20:17:02 +0000

    Initialize rosetta-gas-bug-fixes

---
+++ apps/wallet/src/shared/api-env.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+export enum API_ENV {
+    local = 'local',
+    devNet = 'devNet',
+    testNet = 'testNet',
+    mainnet = 'mainnet',
+    customRPC = 'customRPC',
+}
