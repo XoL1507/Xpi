78aadbee472b66f5b68e81bc5e3ca3aad6dff5ce
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:33:52 +0000

    Initialize releases/sui-v1.4.0-release

---
+++ apps/wallet/src/ui/app/shared/faucet/FaucetRequestButton.tsx
@@ -0,0 +1,50 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { FaucetRateLimitError } from '@mysten/sui.js';
+import { toast } from 'react-hot-toast';
+
+import FaucetMessageInfo from './FaucetMessageInfo';
+import { useFaucetMutation } from './useFaucetMutation';
+import { useFaucetRateLimiter } from './useFaucetRateLimiter';
+import { API_ENV_TO_INFO } from '_app/ApiProvider';
+import { Button, type ButtonProps } from '_app/shared/ButtonUI';
+import { useAppSelector } from '_hooks';
+
+export type FaucetRequestButtonProps = {
+	variant?: ButtonProps['variant'];
+	size?: ButtonProps['size'];
+};
+
+function FaucetRequestButton({ variant = 'primary', size = 'narrow' }: FaucetRequestButtonProps) {
+	const network = useAppSelector(({ app }) => app.apiEnv);
+	const networkName = API_ENV_TO_INFO[network].name.replace(/sui\s*/gi, '');
+	const [isRateLimited, rateLimit] = useFaucetRateLimiter();
+	const mutation = useFaucetMutation({
+		onError: (error) => {
+			if (error instanceof FaucetRateLimitError) {
+				rateLimit();
+			}
+		},
+	});
+
+	return mutation.enabled ? (
+		<Button
+			data-testid="faucet-request-button"
+			variant={variant}
+			size={size}
+			disabled={isRateLimited}
+			onClick={() => {
+				toast.promise(mutation.mutateAsync(), {
+					loading: <FaucetMessageInfo loading />,
+					success: (totalReceived) => <FaucetMessageInfo totalReceived={totalReceived} />,
+					error: (error) => <FaucetMessageInfo error={error.message} />,
+				});
+			}}
+			loading={mutation.isMutating}
+			text={`Request ${networkName} SUI Tokens`}
+		/>
+	) : null;
+}
+
+export default FaucetRequestButton;
