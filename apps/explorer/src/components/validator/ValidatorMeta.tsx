1914d49fe53a87bde207ccb5e0aa964657861126
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:18:44 +0400

    Initialize chris-test-ci

---
+++ apps/explorer/src/components/validator/ValidatorMeta.tsx
@@ -0,0 +1,105 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { ArrowUpRight12 } from '@mysten/icons';
+import { toB64, type MoveActiveValidator } from '@mysten/sui.js';
+import { useMemo } from 'react';
+
+import { StakeButton } from './StakeButton';
+
+import { DescriptionList, DescriptionItem } from '~/ui/DescriptionList';
+import { Heading } from '~/ui/Heading';
+import { ImageIcon } from '~/ui/ImageIcon';
+import { AddressLink } from '~/ui/InternalLink';
+import { Text } from '~/ui/Text';
+import { getName } from '~/utils/getName';
+
+type ValidatorMetaProps = {
+    validatorData: MoveActiveValidator;
+};
+
+export function ValidatorMeta({ validatorData }: ValidatorMetaProps) {
+    const metadata = validatorData.fields.metadata.fields;
+
+    const validatorName = useMemo(() => getName(metadata.name), [metadata]);
+
+    const validatorPublicKey = useMemo(
+        () => toB64(new Uint8Array(metadata.pubkey_bytes)),
+        [metadata]
+    );
+
+    // NOTE: We only support the string-encoded metadata fields, which will become the only encoding soon:
+    const logo =
+        !metadata.image_url || typeof metadata.image_url !== 'string'
+            ? null
+            : metadata.image_url;
+
+    const description =
+        !metadata.description || typeof metadata.description !== 'string'
+            ? null
+            : metadata.description;
+
+    const projectUrl =
+        !metadata.project_url || typeof metadata.project_url !== 'string'
+            ? null
+            : metadata.project_url;
+
+    return (
+        <>
+            <div className="flex basis-full gap-5 border-r border-transparent border-r-gray-45 md:mr-7.5 md:basis-1/4">
+                <ImageIcon
+                    src={logo}
+                    label={validatorName}
+                    fallback={validatorName}
+                    size="xl"
+                />
+                <div className="mt-1.5 flex flex-col">
+                    <Heading as="h1" variant="heading2/bold" color="gray-90">
+                        {validatorName}
+                    </Heading>
+                    {projectUrl && (
+                        <a
+                            href={projectUrl}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            className="mt-2.5 inline-flex items-center gap-1.5 text-body font-medium text-sui-dark no-underline"
+                        >
+                            {projectUrl}
+                            <ArrowUpRight12 className="text-steel" />
+                        </a>
+                    )}
+                    <div className="mt-3.5">
+                        <StakeButton />
+                    </div>
+                </div>
+            </div>
+            <div className="min-w-0 basis-full break-words md:basis-2/3">
+                <DescriptionList>
+                    <DescriptionItem title="Description">
+                        <Text variant="p1/medium" color="gray-90">
+                            {description || '--'}
+                        </Text>
+                    </DescriptionItem>
+                    <DescriptionItem title="Location">
+                        <Text variant="p1/medium" color="gray-90">
+                            --
+                        </Text>
+                    </DescriptionItem>
+                    <DescriptionItem title="Address">
+                        <AddressLink
+                            address={
+                                validatorData.fields.metadata.fields.sui_address
+                            }
+                            noTruncate
+                        />
+                    </DescriptionItem>
+                    <DescriptionItem title="Public Key">
+                        <Text variant="p1/medium" color="gray-90">
+                            {validatorPublicKey}
+                        </Text>
+                    </DescriptionItem>
+                </DescriptionList>
+            </div>
+        </>
+    );
+}
---
+++ narwhal/network/src/admin.rs
@@ -0,0 +1,125 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use axum::routing::post;
+use axum::{extract::Extension, http::StatusCode, routing::get, Json, Router};
+use mysten_metrics::{spawn_logged_monitored_task, spawn_monitored_task};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
+use std::time::Duration;
+use tokio::task::JoinHandle;
+use tokio::time::sleep;
+use tracing::{error, info};
+use types::metered_channel::Sender;
+use types::{ConditionalBroadcastReceiver, ReconfigureNotification};
+
+pub fn start_admin_server(
+    port: u16,
+    network: anemo::Network,
+    mut tr_shutdown: ConditionalBroadcastReceiver,
+    tx_state_handler: Option<Sender<ReconfigureNotification>>,
+) -> Vec<JoinHandle<()>> {
+    let mut router = Router::new()
+        .route("/peers", get(get_peers))
+        .route("/known_peers", get(get_known_peers));
+
+    // Primaries will have this service enabled
+    if let Some(tx_state_handler) = tx_state_handler {
+        let r = Router::new()
+            .route("/reconfigure", post(reconfigure))
+            .layer(Extension(tx_state_handler));
+        router = router.merge(r);
+    }
+
+    router = router.layer(Extension(network));
+
+    let socket_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), port);
+    info!(
+        address =% socket_address,
+        "starting admin server"
+    );
+
+    let handle = axum_server::Handle::new();
+    let shutdown_handle = handle.clone();
+
+    let mut handles = Vec::new();
+    // Spawn a task to shutdown server.
+    handles.push(spawn_monitored_task!(async move {
+        _ = tr_shutdown.receiver.recv().await;
+        handle.clone().shutdown();
+    }));
+
+    handles.push(spawn_logged_monitored_task!(
+        async move {
+            // retry a few times before quitting
+            let mut total_retries = 10;
+
+            loop {
+                total_retries -= 1;
+
+                match TcpListener::bind(socket_address) {
+                    Ok(listener) => {
+                        axum_server::from_tcp(listener)
+                            .handle(shutdown_handle)
+                            .serve(router.into_make_service())
+                            .await
+                            .unwrap_or_else(|err| {
+                                panic!("Failed to boot admin {}: {err}", socket_address)
+                            });
+
+                        return;
+                    }
+                    Err(err) => {
+                        if total_retries == 0 {
+                            error!("{}", err);
+                            panic!("Failed to boot admin {}: {}", socket_address, err);
+                        }
+
+                        error!("{}", err);
+
+                        // just sleep for a bit before retrying in case the port
+                        // has not been de-allocated
+                        sleep(Duration::from_secs(1)).await;
+
+                        continue;
+                    }
+                }
+            }
+        },
+        "AdminServerTask"
+    ));
+
+    handles
+}
+
+async fn get_peers(
+    Extension(network): Extension<anemo::Network>,
+) -> (StatusCode, Json<Vec<String>>) {
+    (
+        StatusCode::OK,
+        Json(network.peers().iter().map(|x| x.to_string()).collect()),
+    )
+}
+
+async fn get_known_peers(
+    Extension(network): Extension<anemo::Network>,
+) -> (StatusCode, Json<Vec<String>>) {
+    (
+        StatusCode::OK,
+        Json(
+            network
+                .known_peers()
+                .get_all()
+                .iter()
+                .map(|x| format!("{}: {:?}", x.peer_id, x.address))
+                .collect(),
+        ),
+    )
+}
+
+async fn reconfigure(
+    Extension(tx_state_handler): Extension<Sender<ReconfigureNotification>>,
+    Json(reconfigure_notification): Json<ReconfigureNotification>,
+) -> StatusCode {
+    let _ = tx_state_handler.send(reconfigure_notification).await;
+    StatusCode::OK
+}
