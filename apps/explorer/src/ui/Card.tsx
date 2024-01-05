e970564c824149d9a84e7685e2da8b7b81a4124c
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 20:12:37 +0000

    Initialize nm/port-suins-fe

---
+++ apps/explorer/src/ui/Card.tsx
@@ -0,0 +1,40 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { cva, type VariantProps } from 'class-variance-authority';
+import { type ReactNode } from 'react';
+
+const cardStyles = cva(null, {
+    variants: {
+        bg: {
+            default: 'bg-gray-40',
+            highlight: 'bg-success-light',
+            lightBlue: 'bg-sui/10',
+        },
+        rounded: {
+            lg: 'rounded-lg',
+            xl: 'rounded-xl',
+        },
+        spacing: {
+            none: '',
+            sm: 'px-5 py-4',
+            md: 'p-5',
+            lg: 'sm:p-8 p-4',
+        },
+    },
+    defaultVariants: {
+        bg: 'default',
+        spacing: 'md',
+        rounded: 'lg',
+    },
+});
+
+export interface CardProps extends VariantProps<typeof cardStyles> {
+    children?: ReactNode;
+}
+
+export function Card({ spacing, rounded, bg, children }: CardProps) {
+    return (
+        <div className={cardStyles({ spacing, rounded, bg })}>{children}</div>
+    );
+}
---
+++ apps/wallet/src/ui/app/components/logo/index.tsx
@@ -0,0 +1,38 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Sui, SuiLogoTxt } from '@mysten/icons';
+
+import { Text } from '../../shared/text';
+import { API_ENV } from '_src/shared/api-env';
+
+const networkNames: Record<API_ENV, string> = {
+    [API_ENV.local]: 'Local',
+    [API_ENV.testNet]: 'Testnet',
+    [API_ENV.devNet]: 'Devnet',
+    [API_ENV.customRPC]: 'Custom RPC',
+};
+
+type LogoProps = {
+    networkName?: API_ENV;
+};
+
+const Logo = ({ networkName }: LogoProps) => {
+    return (
+        <div className="inline-flex flex-nowrap items-center gap-0.5 text-gray-90">
+            <Sui className="h-[26px] w-5" />
+            <div className="flex flex-col">
+                <SuiLogoTxt className="w-5 h-[13px]" />
+                {networkName && (
+                    <div className="whitespace-nowrap">
+                        <Text variant="subtitleSmallExtra">
+                            {networkNames[networkName]}
+                        </Text>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Logo;
