253de869024cc7153507e854a947f725241063b6
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:35:50 +0400

    Initialize validator-tool-squashed

---
+++ sdk/typescript/test/e2e/object-display-standard.test.ts
@@ -0,0 +1,56 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { describe, it, expect, beforeAll } from 'vitest';
+import { ObjectId, getObjectDisplay } from '../../src';
+import { publishPackage, setup, TestToolbox } from './utils/setup';
+
+describe('Test Object Display Standard', () => {
+  let toolbox: TestToolbox;
+  let packageId: ObjectId;
+
+  beforeAll(async () => {
+    toolbox = await setup();
+    const packagePath = __dirname + '/./data/display_test';
+    ({ packageId } = await publishPackage(packagePath, toolbox));
+  });
+
+  it('Test getting Display fields', async () => {
+    const boarId = (
+      await toolbox.provider.getOwnedObjects({
+        owner: toolbox.address(),
+        typeFilter: `${packageId}::boars::Boar`,
+        options: { showDisplay: true, showType: true },
+      })
+    )[0].objectId;
+    const display = getObjectDisplay(
+      await toolbox.provider.getObject({
+        id: boarId,
+        options: { showDisplay: true },
+      }),
+    );
+    expect(display).toEqual({
+      age: '10',
+      buyer: `0x${toolbox.address()}`,
+      creator: 'Chris',
+      description: `Unique Boar from the Boars collection with First Boar and ${boarId}`,
+      img_url: 'https://get-a-boar.com/first.png',
+      name: 'First Boar',
+      price: '',
+      project_url: 'https://get-a-boar.com/',
+      full_url: 'https://get-a-boar.fullurl.com/',
+      escape_syntax: '{name}',
+    });
+  });
+
+  it('Test getting Display fields for object that has no display object', async () => {
+    const coinId = (await toolbox.getGasObjectsOwnedByAddress())[0].objectId;
+    const display = getObjectDisplay(
+      await toolbox.provider.getObject({
+        id: coinId,
+        options: { showDisplay: true },
+      }),
+    );
+    expect(display).toEqual(undefined);
+  });
+});
