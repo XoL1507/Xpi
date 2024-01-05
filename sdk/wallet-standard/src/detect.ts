6e0e1e0359dca056aeda15b1829c8ff98bcee8ff
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:58:07 +0000

    Initialize wrobertson/better_types_for_add_features

---
+++ sdk/wallet-standard/src/detect.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Wallet } from '@wallet-standard/core';
+import { WalletWithSuiFeatures } from './features';
+
+// These features are absolutely required for wallets to function in the Sui ecosystem.
+// Eventually, as wallets have more consistent support of features, we may want to extend this list.
+const REQUIRED_BASE_FEATURES = ['standard:connect', 'standard:events'] as const;
+
+type WalletFeatureName = keyof WalletWithSuiFeatures['features'];
+type DefaultRequiredWalletFeatureName = (typeof REQUIRED_BASE_FEATURES)[number];
+
+export type AdditionallyRequiredWalletFeatureName = Exclude<
+	WalletFeatureName,
+	DefaultRequiredWalletFeatureName
+>;
+
+export function isWalletWithSuiFeatures(
+	wallet: Wallet,
+	/** Extra features that are required to be present, in addition to the expected feature set. */
+	additionalFeatures: AdditionallyRequiredWalletFeatureName[] = [],
+): wallet is WalletWithSuiFeatures {
+	return [...REQUIRED_BASE_FEATURES, ...additionalFeatures].every(
+		(feature) => feature in wallet.features,
+	);
+}
