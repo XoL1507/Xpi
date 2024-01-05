7e295c4b447001bfbccce3006b294a1d1c169447
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:40:12 +0400

    Initialize aggregate-quorum-side

---
+++ wallet/src/ui/app/redux/RootReducer.ts
@@ -0,0 +1,22 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { combineReducers } from '@reduxjs/toolkit';
+
+import account from './slices/account';
+import app from './slices/app';
+import suiObjects from './slices/sui-objects';
+import transactions from './slices/transactions';
+import txresults from './slices/txresults';
+
+const rootReducer = combineReducers({
+    account,
+    app,
+    suiObjects,
+    transactions,
+    txresults,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default rootReducer;
