b743dcb4b48e18fe25d3885d3d41643c3b60fcb6
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:57:19 +0400

    Initialize not-pin-user-sig-in-tx-hash

---
+++ sdk/typescript/src/types/version.ts
@@ -0,0 +1,16 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { parse } from '@suchipi/femver';
+
+export type RpcApiVersion = {
+  major: number;
+  minor: number;
+  patch: number;
+};
+
+export function parseVersionFromString(
+  version: string
+): RpcApiVersion | undefined {
+  return parse(version);
+}
