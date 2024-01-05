4272dc155cce7464b0fe5b62abf3d9bf6b3df0d2
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:31:23 +0400

    Initialize chris-fixci

---
+++ sdk/typescript/test/e2e/objects.test.ts
@@ -0,0 +1,59 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { describe, it, expect, beforeAll } from 'vitest';
+import { getObjectType } from '../../src';
+import { setup, TestToolbox } from './utils/setup';
+
+describe('Object Reading API', () => {
+  let toolbox: TestToolbox;
+
+  beforeAll(async () => {
+    toolbox = await setup();
+  });
+
+  it('Get Owned Objects', async () => {
+    const gasObjects = await toolbox.provider.getObjectsOwnedByAddress({
+      owner: toolbox.address(),
+    });
+    expect(gasObjects.length).to.greaterThan(0);
+  });
+
+  it('Get Object', async () => {
+    const gasObjects = await toolbox.getGasObjectsOwnedByAddress();
+    expect(gasObjects.length).to.greaterThan(0);
+    const objectInfos = await Promise.all(
+      gasObjects.map((gasObject) =>
+        toolbox.provider.getObject({
+          id: gasObject['objectId'],
+          options: { showType: true },
+        }),
+      ),
+    );
+    objectInfos.forEach((objectInfo) =>
+      expect(getObjectType(objectInfo)).to.equal(
+        '0x2::coin::Coin<0x2::sui::SUI>',
+      ),
+    );
+  });
+
+  it('Get Objects', async () => {
+    const gasObjects = await toolbox.getGasObjectsOwnedByAddress();
+    expect(gasObjects.length).to.greaterThan(0);
+    const gasObjectIds = gasObjects.map((gasObject) => gasObject['objectId']);
+    const objectInfos = await toolbox.provider.multiGetObjects({
+      ids: gasObjectIds,
+      options: {
+        showType: true,
+      },
+    });
+
+    expect(gasObjects.length).to.equal(objectInfos.length);
+
+    objectInfos.forEach((objectInfo) =>
+      expect(getObjectType(objectInfo)).to.equal(
+        '0x2::coin::Coin<0x2::sui::SUI>',
+      ),
+    );
+  });
+});
