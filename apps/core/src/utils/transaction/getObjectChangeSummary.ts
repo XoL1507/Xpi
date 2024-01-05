e44b2bf54a2a31c6d916cecd1f6c0123a834f31b
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 20:19:20 +0000

    Initialize node-stream

---
+++ apps/core/src/utils/transaction/getObjectChangeSummary.ts
@@ -0,0 +1,45 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import {
+    type SuiTransactionBlockResponse,
+    type SuiAddress,
+    type DryRunTransactionBlockResponse,
+    SuiObjectChangeTransferred,
+    SuiObjectChangeCreated,
+    SuiObjectChangeMutated,
+} from '@mysten/sui.js';
+
+export type ObjectChangeSummary = {
+    mutated: SuiObjectChangeMutated[];
+    created: SuiObjectChangeCreated[];
+    transferred: SuiObjectChangeTransferred[];
+};
+
+export const getObjectChangeSummary = (
+    transaction: DryRunTransactionBlockResponse | SuiTransactionBlockResponse,
+    currentAddress?: SuiAddress | null
+) => {
+    const { objectChanges } = transaction;
+    if (!objectChanges) return null;
+
+    const mutated = objectChanges.filter(
+        (change) => change.type === 'mutated'
+    ) as SuiObjectChangeMutated[];
+
+    const created = objectChanges.filter(
+        (change) =>
+            change.type === 'created' &&
+            (typeof currentAddress === 'undefined' ||
+                change.sender === currentAddress)
+    ) as SuiObjectChangeCreated[];
+
+    const transferred = objectChanges.filter(
+        (change) => change.type === 'transferred'
+    ) as SuiObjectChangeTransferred[];
+
+    return {
+        mutated,
+        created,
+        transferred,
+    };
+};
