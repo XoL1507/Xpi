3a2a6b7bfc7cf3dfaea1be6f91941ba580c3236e
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:37:23 +0400

    Initialize jordan--Rework_splitCoin_to_splitCoins

---
+++ crates/sui-json-rpc/src/api/mod.rs
@@ -0,0 +1,61 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+mod coin;
+mod event;
+mod governance;
+mod read;
+mod transaction_builder;
+mod write;
+
+pub use coin::CoinReadApiClient;
+pub use coin::CoinReadApiOpenRpc;
+pub use coin::CoinReadApiServer;
+
+pub use event::EventReadApiClient;
+pub use event::EventReadApiOpenRpc;
+pub use event::EventReadApiServer;
+
+pub use write::WriteApiClient;
+pub use write::WriteApiOpenRpc;
+pub use write::WriteApiServer;
+
+pub use governance::GovernanceReadApiClient;
+pub use governance::GovernanceReadApiOpenRpc;
+pub use governance::GovernanceReadApiServer;
+
+pub use read::ReadApiClient;
+pub use read::ReadApiOpenRpc;
+pub use read::ReadApiServer;
+
+// use anyhow::anyhow;
+pub use transaction_builder::TransactionBuilderClient;
+pub use transaction_builder::TransactionBuilderOpenRpc;
+pub use transaction_builder::TransactionBuilderServer;
+
+/// Maximum number of events returned in an event query.
+/// This is equivalent to EVENT_QUERY_MAX_LIMIT in `sui-storage` crate.
+/// To avoid unnecessary dependency on that crate, we have a reference here
+/// for document purposes.
+pub const QUERY_MAX_RESULT_LIMIT: usize = 1000;
+
+pub const MAX_GET_OWNED_OBJECT_LIMIT: usize = 256;
+
+pub fn cap_page_limit(limit: Option<usize>) -> usize {
+    let limit = limit.unwrap_or_default();
+    if limit > QUERY_MAX_RESULT_LIMIT || limit == 0 {
+        QUERY_MAX_RESULT_LIMIT
+    } else {
+        limit
+    }
+}
+
+pub fn cap_page_objects_limit(limit: Option<usize>) -> Result<usize, anyhow::Error> {
+    let limit = limit.unwrap_or(MAX_GET_OWNED_OBJECT_LIMIT);
+    if limit > MAX_GET_OWNED_OBJECT_LIMIT || limit == 0 {
+        Ok(MAX_GET_OWNED_OBJECT_LIMIT)
+        // MUSTFIXD(jian): implement this error: Err(anyhow!("limit is greater than max get owned object size"))
+    } else {
+        Ok(limit)
+    }
+}
---
+++ sdk/wallet-adapter/example/src/App.tsx
@@ -0,0 +1,74 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import "./App.css";
+import { ConnectButton, useWalletKit } from "@mysten/wallet-kit";
+import { Transaction } from "@mysten/sui.js";
+import { useEffect } from "react";
+
+const transaction = new Transaction();
+transaction.moveCall({
+  target: `0x2::devnet_nft::mint`,
+  arguments: [
+    transaction.pure("foo"),
+    transaction.pure("bar"),
+    transaction.pure("baz"),
+  ],
+});
+
+function App() {
+  const {
+    currentWallet,
+    signTransaction,
+    signAndExecuteTransaction,
+    signMessage,
+  } = useWalletKit();
+
+  useEffect(() => {
+    // You can do something with `currentWallet` here.
+  }, [currentWallet]);
+
+  return (
+    <div className="App">
+      <ConnectButton />
+      <div>
+        <button
+          onClick={async () => {
+            console.log(await signTransaction({ transaction }));
+          }}
+        >
+          Sign Transaction
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={async () => {
+            console.log(
+              await signAndExecuteTransaction({
+                transaction,
+                options: { contentOptions: { showEffects: true } },
+              })
+            );
+          }}
+        >
+          Sign + Execute Transaction
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={async () => {
+            console.log(
+              await signMessage({
+                message: new TextEncoder().encode("Message to sign"),
+              })
+            );
+          }}
+        >
+          Sign message
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
