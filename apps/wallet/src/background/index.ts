7e65aa43944898c46c467556e3997c4fc6a6cc24
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:53:32 +0400

    Initialize lu/executed-sequence

---
+++ apps/wallet/src/background/index.ts
@@ -0,0 +1,60 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { lte } from 'semver';
+import Browser from 'webextension-polyfill';
+
+import { LOCK_ALARM_NAME } from './Alarms';
+import Keyring from './Keyring';
+import Permissions from './Permissions';
+import { Connections } from './connections';
+import { openInNewTab } from '_shared/utils';
+import { MSG_CONNECT } from '_src/content-script/keep-bg-alive';
+
+Browser.runtime.onInstalled.addListener(async ({ reason, previousVersion }) => {
+    if (reason === 'install') {
+        openInNewTab();
+    } else if (
+        reason === 'update' &&
+        previousVersion &&
+        lte(previousVersion, '0.1.1')
+    ) {
+        // clear everything in the storage
+        // mainly done to clear the mnemonic that was stored
+        // as plain text
+        await Browser.storage.local.clear();
+    }
+});
+
+const connections = new Connections();
+
+Permissions.permissionReply.subscribe((permission) => {
+    if (permission) {
+        connections.notifyForPermissionReply(permission);
+    }
+});
+
+Keyring.on('lockedStatusUpdate', (isLocked: boolean) => {
+    connections.notifyForLockedStatusUpdate(isLocked);
+});
+
+Browser.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === LOCK_ALARM_NAME) {
+        Keyring.lock();
+    }
+});
+
+Keyring.on('lockedStatusUpdate', async (isLocked) => {
+    if (!isLocked) {
+        const allTabs = await Browser.tabs.query({});
+        for (const aTab of allTabs) {
+            if (aTab.id) {
+                try {
+                    await Browser.tabs.sendMessage(aTab.id, MSG_CONNECT);
+                } catch (e) {
+                    // not all tabs have the cs installed
+                }
+            }
+        }
+    }
+});
