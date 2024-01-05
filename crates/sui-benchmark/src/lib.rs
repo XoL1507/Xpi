128b6fc721cff1f26044996457000ac72f14f0af
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:05:13 +0400

    Initialize remove-locs-consensus-handler

---
+++ crates/sui-benchmark/src/lib.rs
@@ -0,0 +1,278 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+use anyhow::bail;
+use async_trait::async_trait;
+use std::{collections::BTreeMap, sync::Arc};
+use sui_core::{
+    authority_aggregator::AuthorityAggregator,
+    authority_client::NetworkAuthorityClient,
+    quorum_driver::{QuorumDriver, QuorumDriverHandler, QuorumDriverMetrics},
+};
+use sui_json_rpc_types::{SuiCertifiedTransaction, SuiObjectRead, SuiTransactionEffects};
+use sui_network::default_mysten_network_config;
+use sui_sdk::SuiClient;
+use sui_types::{
+    base_types::ObjectID,
+    committee::{Committee, EpochId},
+    messages::{CertifiedTransactionEffects, QuorumDriverResponse, Transaction},
+    object::{Object, ObjectRead},
+};
+use sui_types::{
+    base_types::ObjectRef, crypto::AuthorityStrongQuorumSignInfo,
+    messages::ExecuteTransactionRequestType, object::Owner,
+};
+use tracing::{error, info};
+
+pub mod drivers;
+pub mod util;
+pub mod workloads;
+
+/// A wrapper on execution results to accommodate different types of
+/// responses from LocalValidatorAggregatorProxy and FullNodeProxy
+#[allow(clippy::large_enum_variant)]
+pub enum ExecutionEffects {
+    CertifiedTransactionEffects(CertifiedTransactionEffects),
+    SuiTransactionEffects(SuiTransactionEffects),
+}
+
+impl ExecutionEffects {
+    pub fn mutated(&self) -> Vec<(ObjectRef, Owner)> {
+        match self {
+            ExecutionEffects::CertifiedTransactionEffects(certified_effects) => {
+                certified_effects.data().mutated.clone()
+            }
+            ExecutionEffects::SuiTransactionEffects(sui_tx_effects) => sui_tx_effects
+                .mutated
+                .clone()
+                .into_iter()
+                .map(|refe| (refe.reference.to_object_ref(), refe.owner))
+                .collect(),
+        }
+    }
+
+    pub fn created(&self) -> Vec<(ObjectRef, Owner)> {
+        match self {
+            ExecutionEffects::CertifiedTransactionEffects(certified_effects) => {
+                certified_effects.data().created.clone()
+            }
+            ExecutionEffects::SuiTransactionEffects(sui_tx_effects) => sui_tx_effects
+                .created
+                .clone()
+                .into_iter()
+                .map(|refe| (refe.reference.to_object_ref(), refe.owner))
+                .collect(),
+        }
+    }
+
+    pub fn quorum_sig(&self) -> Option<&AuthorityStrongQuorumSignInfo> {
+        match self {
+            ExecutionEffects::CertifiedTransactionEffects(certified_effects) => {
+                Some(certified_effects.auth_sig())
+            }
+            ExecutionEffects::SuiTransactionEffects(_) => None,
+        }
+    }
+
+    pub fn gas_object(&self) -> (ObjectRef, Owner) {
+        match self {
+            ExecutionEffects::CertifiedTransactionEffects(certified_effects) => {
+                certified_effects.data().gas_object
+            }
+            ExecutionEffects::SuiTransactionEffects(sui_tx_effects) => {
+                let refe = &sui_tx_effects.gas_object;
+                (refe.reference.to_object_ref(), refe.owner)
+            }
+        }
+    }
+}
+
+#[async_trait]
+pub trait ValidatorProxy {
+    async fn get_object(&self, object_id: ObjectID) -> Result<Object, anyhow::Error>;
+
+    async fn execute_transaction(
+        &self,
+        tx: Transaction,
+    ) -> anyhow::Result<(SuiCertifiedTransaction, ExecutionEffects)>;
+
+    async fn reconfig(&self);
+
+    fn clone_committee(&self) -> Committee;
+
+    fn get_current_epoch(&self) -> EpochId;
+
+    fn clone_new(&self) -> Box<dyn ValidatorProxy + Send + Sync>;
+}
+
+pub struct LocalValidatorAggregatorProxy {
+    _qd_handler: QuorumDriverHandler<NetworkAuthorityClient>,
+    qd: Arc<QuorumDriver<NetworkAuthorityClient>>,
+}
+
+impl LocalValidatorAggregatorProxy {
+    pub fn from_auth_agg(agg: Arc<AuthorityAggregator<NetworkAuthorityClient>>) -> Self {
+        let qd_handler =
+            QuorumDriverHandler::new(agg, Arc::new(QuorumDriverMetrics::new_for_tests()));
+        let qd = qd_handler.clone_quorum_driver();
+        Self {
+            _qd_handler: qd_handler,
+            qd,
+        }
+    }
+}
+
+#[async_trait]
+impl ValidatorProxy for LocalValidatorAggregatorProxy {
+    async fn get_object(&self, object_id: ObjectID) -> Result<Object, anyhow::Error> {
+        let auth_agg = self.qd.authority_aggregator().load();
+        match auth_agg.get_object_info_execute(object_id).await? {
+            ObjectRead::Exists(_, object, _) => Ok(object),
+            other => bail!("object {object_id} does not exist: {:?}", other),
+        }
+    }
+
+    async fn execute_transaction(
+        &self,
+        tx: Transaction,
+    ) -> anyhow::Result<(SuiCertifiedTransaction, ExecutionEffects)> {
+        let ticket = self.qd.submit_transaction(tx.verify()?).await?;
+        let QuorumDriverResponse {
+            tx_cert,
+            effects_cert,
+        } = ticket.await?;
+        Ok((
+            tx_cert.try_into().unwrap(),
+            ExecutionEffects::CertifiedTransactionEffects(effects_cert.into()),
+        ))
+    }
+
+    async fn reconfig(&self) {
+        let auth_agg = self.qd.authority_aggregator().load();
+        match auth_agg
+            .get_committee_with_net_addresses(self.qd.current_epoch())
+            .await
+        {
+            Err(err) => {
+                error!(
+                    "Reconfiguration - Failed to get committee with network address: {}",
+                    err
+                )
+            }
+            Ok(committee_info) => {
+                let network_config = default_mysten_network_config();
+                let new_epoch = committee_info.committee.epoch;
+                // Check if we already advanced.
+                let cur_epoch = self.qd.current_epoch();
+                if new_epoch <= cur_epoch {
+                    return;
+                }
+                info!("Reconfiguration - Observed a new epoch {new_epoch}, attempting to reconfig from current epoch: {cur_epoch}");
+                match auth_agg.recreate_with_net_addresses(committee_info, &network_config) {
+                    Err(err) => error!(
+                        "Reconfiguration - Error when cloning authority aggregator with committee: {}",
+                        err
+                    ),
+                    Ok(auth_agg) => {
+                        self.qd.update_validators(Arc::new(auth_agg)).await;
+                        info!("Reconfiguration - Reconfiguration to epoch {new_epoch} is done");
+                    }
+                }
+            }
+        }
+    }
+
+    fn clone_committee(&self) -> Committee {
+        self.qd.clone_committee()
+    }
+
+    fn get_current_epoch(&self) -> EpochId {
+        self.qd.current_epoch()
+    }
+
+    fn clone_new(&self) -> Box<dyn ValidatorProxy + Send + Sync> {
+        let qdh = self._qd_handler.clone_new();
+        let qd = qdh.clone_quorum_driver();
+        Box::new(Self {
+            _qd_handler: qdh,
+            qd,
+        })
+    }
+}
+
+pub struct FullNodeProxy {
+    sui_client: SuiClient,
+    committee: Committee,
+}
+
+impl FullNodeProxy {
+    pub async fn from_url(http_url: &str) -> Result<Self, anyhow::Error> {
+        let sui_client = SuiClient::new(http_url, None, None).await?;
+
+        let resp = sui_client.read_api().get_committee_info(None).await?;
+        let epoch = resp.epoch;
+        let committee = if let Some(committee_vec) = resp.committee_info {
+            let committee_map =
+                BTreeMap::from_iter(committee_vec.into_iter().map(|(name, stake)| (name, stake)));
+            Committee::new(epoch, committee_map)?
+        } else {
+            bail!(
+                "Get empty committee info from fullnode for epoch {:?}",
+                epoch
+            )
+        };
+
+        Ok(Self {
+            sui_client,
+            committee,
+        })
+    }
+}
+
+#[async_trait]
+impl ValidatorProxy for FullNodeProxy {
+    async fn get_object(&self, object_id: ObjectID) -> Result<Object, anyhow::Error> {
+        match self.sui_client.read_api().get_object(object_id).await? {
+            SuiObjectRead::Exists(sui_obj) => sui_obj.try_into(),
+            other => bail!("object {object_id} does not exist: {:?}", other),
+        }
+    }
+
+    async fn execute_transaction(
+        &self,
+        tx: Transaction,
+    ) -> anyhow::Result<(SuiCertifiedTransaction, ExecutionEffects)> {
+        let result = self
+            .sui_client
+            .quorum_driver()
+            // We need to use WaitForLocalExecution to make sure objects are updated on FN
+            .execute_transaction(
+                tx.verify()?,
+                Some(ExecuteTransactionRequestType::WaitForLocalExecution),
+            )
+            .await?;
+        let tx_cert = result.tx_cert.unwrap();
+        let effects = ExecutionEffects::SuiTransactionEffects(result.effects.unwrap());
+        Ok((tx_cert, effects))
+    }
+
+    async fn reconfig(&self) {
+        // TODO poll FN until it has proceeds to next epoch
+        // and update self.committee
+        return;
+    }
+
+    fn clone_committee(&self) -> Committee {
+        self.committee.clone()
+    }
+
+    fn get_current_epoch(&self) -> EpochId {
+        self.committee.epoch
+    }
+
+    fn clone_new(&self) -> Box<dyn ValidatorProxy + Send + Sync> {
+        Box::new(Self {
+            sui_client: self.sui_client.clone(),
+            committee: self.clone_committee(),
+        })
+    }
+}
---
+++ crates/sui-rosetta/src/operations.rs
@@ -0,0 +1,369 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::collections::HashMap;
+use std::ops::Neg;
+use std::str::FromStr;
+
+use serde::Deserialize;
+use serde::Serialize;
+use serde_json::{json, Value};
+use sui_sdk::rpc_types::{SuiEvent, SuiExecutionStatus, SuiTransactionData, SuiTransactionKind};
+
+use sui_types::base_types::{ObjectRef, SuiAddress};
+use sui_types::event::BalanceChangeType;
+use sui_types::gas_coin::GAS;
+use sui_types::messages::TransactionData;
+use sui_types::object::Owner;
+
+use crate::types::{
+    AccountIdentifier, Amount, CoinAction, CoinChange, CoinIdentifier, ConstructionMetadata,
+    IndexCounter, OperationIdentifier, OperationStatus, OperationType, SignedValue,
+};
+use crate::Error;
+
+#[cfg(test)]
+#[path = "unit_tests/operations_tests.rs"]
+mod operations_tests;
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct Operation {
+    pub operation_identifier: OperationIdentifier,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub related_operations: Vec<OperationIdentifier>,
+    #[serde(rename = "type")]
+    pub type_: OperationType,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub status: Option<OperationStatus>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub account: Option<AccountIdentifier>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub amount: Option<Amount>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub coin_change: Option<CoinChange>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub metadata: Option<Value>,
+}
+
+impl Operation {
+    pub fn from_data(data: &SuiTransactionData) -> Result<Vec<Operation>, anyhow::Error> {
+        let sender = data.sender;
+        let mut counter = IndexCounter::default();
+        let mut ops = data
+            .transactions
+            .iter()
+            .flat_map(|tx| parse_operations(tx, sender, &mut counter, None))
+            .flatten()
+            .collect::<Vec<_>>();
+        let gas = Operation::gas_budget(
+            &mut counter,
+            None,
+            data.gas_payment.to_object_ref(),
+            data.gas_budget,
+            sender,
+        );
+        ops.push(gas);
+        Ok(ops)
+    }
+
+    pub fn from_data_and_events(
+        data: &SuiTransactionData,
+        status: &SuiExecutionStatus,
+        events: &[SuiEvent],
+    ) -> Result<Vec<Operation>, anyhow::Error> {
+        let sender = data.sender;
+        let mut counter = IndexCounter::default();
+        let status = Some((status).into());
+        let mut ops = data
+            .transactions
+            .iter()
+            .flat_map(|tx| parse_operations(tx, sender, &mut counter, status))
+            .flatten()
+            .collect::<Vec<_>>();
+        let gas = Operation::gas_budget(
+            &mut counter,
+            status,
+            data.gas_payment.to_object_ref(),
+            data.gas_budget,
+            sender,
+        );
+        ops.push(gas);
+
+        // We will need to subtract the PaySui operation amounts from the actual balance
+        // change amount extracted from event to prevent double counting.
+        let mut pay_sui_balance_to_subtract = HashMap::new();
+
+        let pay_sui_ops = ops
+            .iter()
+            .filter_map(|op| match (op.type_, &op.account, &op.amount) {
+                (OperationType::PaySui, Some(acc), Some(amount)) => {
+                    let amount = if amount.value.is_negative() {
+                        // Safe to downcast, total supply of SUI is way less then i128::MAX
+                        amount.value.abs() as i128
+                    } else {
+                        (amount.value.abs() as i128).neg()
+                    };
+                    Some((acc.address, amount))
+                }
+                _ => None,
+            });
+
+        for (addr, amount) in pay_sui_ops {
+            *pay_sui_balance_to_subtract.entry(addr).or_default() += amount
+        }
+
+        // Extract coin change operations from events
+        let coin_change_operations = Operation::get_coin_operation_from_events(
+            events,
+            status,
+            pay_sui_balance_to_subtract,
+            &mut counter,
+        );
+        ops.extend(coin_change_operations);
+
+        Ok(ops)
+    }
+
+    pub fn get_coin_operation_from_events(
+        events: &[SuiEvent],
+        status: Option<OperationStatus>,
+        balance_to_subtract: HashMap<SuiAddress, i128>,
+        counter: &mut IndexCounter,
+    ) -> Vec<Operation> {
+        // Aggregate balance changes by address, rosetta don't care about coins.
+        let mut balance_change = balance_to_subtract;
+        let mut gas: HashMap<SuiAddress, i128> = HashMap::new();
+        for (type_, address, amount) in events.iter().flat_map(Self::get_balance_change_from_event)
+        {
+            if type_ == OperationType::SuiBalanceChange {
+                let sum = balance_change.entry(address).or_default();
+                *sum += amount;
+            } else if type_ == OperationType::GasSpent {
+                let sum = gas.entry(address).or_default();
+                *sum += amount;
+            }
+        }
+
+        let mut ops = balance_change
+            .into_iter()
+            .filter_map(|(addr, amount)| {
+                if amount != 0 {
+                    Some(Operation {
+                        operation_identifier: counter.next_idx().into(),
+                        related_operations: vec![],
+                        type_: OperationType::SuiBalanceChange,
+                        status,
+                        account: Some(addr.into()),
+                        amount: Some(Amount::new(amount.into())),
+                        coin_change: None,
+                        metadata: None,
+                    })
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+
+        ops.extend(
+            gas.into_iter()
+                .map(|(addr, amount)| Operation {
+                    operation_identifier: counter.next_idx().into(),
+                    related_operations: vec![],
+                    type_: OperationType::GasSpent,
+                    status: Some(OperationStatus::Success),
+                    account: Some(addr.into()),
+                    amount: Some(Amount::new(amount.into())),
+                    coin_change: None,
+                    metadata: None,
+                })
+                .collect::<Vec<_>>(),
+        );
+        ops
+    }
+
+    fn get_balance_change_from_event(
+        event: &SuiEvent,
+    ) -> Option<(OperationType, SuiAddress, i128)> {
+        if let SuiEvent::CoinBalanceChange {
+            owner: Owner::AddressOwner(owner),
+            coin_type,
+            amount,
+            change_type,
+            ..
+        } = event
+        {
+            // We only interested in SUI coins and account addresses
+            if coin_type == &GAS::type_().to_string() {
+                let type_ = if change_type == &BalanceChangeType::Gas {
+                    // We always charge gas
+                    OperationType::GasSpent
+                } else {
+                    OperationType::SuiBalanceChange
+                };
+                return Some((type_, *owner, *amount));
+            }
+        }
+        None
+    }
+
+    /// Parse operation input from rosetta to Sui transaction
+    pub async fn create_data(
+        operations: Vec<Operation>,
+        metadata: ConstructionMetadata,
+    ) -> Result<TransactionData, Error> {
+        let mut type_ = None;
+        let mut recipients = vec![];
+        let mut amounts = vec![];
+        let mut sender = None;
+        let mut budget = None;
+        for op in operations {
+            // Currently only PaySui is support,
+            if op.type_ != OperationType::PaySui && op.type_ != OperationType::GasBudget {
+                return Err(Error::UnsupportedOperation(op.type_));
+            }
+            if type_.is_none() && op.type_ != OperationType::GasBudget {
+                type_ = Some(op.type_)
+            }
+            if op.type_ == OperationType::GasBudget {
+                let budget_value = op
+                    .metadata
+                    .clone()
+                    .and_then(|v| v.pointer("/budget").cloned())
+                    .ok_or_else(|| Error::MissingInput("gas budget".to_string()))?;
+                budget = Some(
+                    budget_value
+                        .as_u64()
+                        .or_else(|| budget_value.as_str().and_then(|s| u64::from_str(s).ok()))
+                        .ok_or_else(|| Error::InvalidInput(format!("{budget_value}")))?,
+                );
+            } else if op.type_ == OperationType::PaySui {
+                if let (Some(amount), Some(account)) = (op.amount, op.account) {
+                    if amount.value.is_negative() {
+                        sender = Some(account.address)
+                    } else {
+                        recipients.push(account.address);
+                        let amount = amount.value.abs();
+                        if amount > u64::MAX as u128 {
+                            return Err(Error::InvalidInput(
+                                "Input amount exceed u64::MAX".to_string(),
+                            ));
+                        }
+                        amounts.push(amount as u64)
+                    }
+                }
+            }
+        }
+
+        let address = sender.ok_or_else(|| Error::MissingInput("Sender address".to_string()))?;
+        let gas = metadata.sender_coins[0];
+        let budget = budget.ok_or_else(|| Error::MissingInput("gas budget".to_string()))?;
+
+        Ok(TransactionData::new_pay_sui(
+            address,
+            metadata.sender_coins,
+            recipients,
+            amounts,
+            gas,
+            budget,
+        ))
+    }
+
+    pub fn gas_budget(
+        counter: &mut IndexCounter,
+        status: Option<OperationStatus>,
+        gas: ObjectRef,
+        budget: u64,
+        sender: SuiAddress,
+    ) -> Self {
+        Self {
+            operation_identifier: counter.next_idx().into(),
+            related_operations: vec![],
+            type_: OperationType::GasBudget,
+            status,
+            account: Some(AccountIdentifier { address: sender }),
+            amount: None,
+            coin_change: Some(CoinChange {
+                coin_identifier: CoinIdentifier {
+                    identifier: gas.into(),
+                },
+                coin_action: CoinAction::CoinSpent,
+            }),
+            metadata: Some(json!({ "budget": budget })),
+        }
+    }
+}
+
+fn parse_operations(
+    tx: &SuiTransactionKind,
+    sender: SuiAddress,
+    counter: &mut IndexCounter,
+    status: Option<OperationStatus>,
+) -> Result<Vec<Operation>, anyhow::Error> {
+    let operations = if let SuiTransactionKind::PaySui(tx) = tx {
+        let recipients = tx.recipients.iter().zip(&tx.amounts);
+        let mut aggregated_recipients: HashMap<SuiAddress, u64> = HashMap::new();
+
+        for (recipient, amount) in recipients {
+            *aggregated_recipients.entry(*recipient).or_default() += *amount
+        }
+
+        let mut pay_operations = aggregated_recipients
+            .into_iter()
+            .map(|(recipient, amount)| Operation {
+                operation_identifier: counter.next_idx().into(),
+                related_operations: vec![],
+                type_: OperationType::PaySui,
+                status,
+                account: Some(recipient.into()),
+                amount: Some(Amount::new(amount.into())),
+                coin_change: None,
+                metadata: None,
+            })
+            .collect::<Vec<_>>();
+        let total_paid = tx.amounts.iter().sum::<u64>();
+        pay_operations.push(Operation {
+            operation_identifier: counter.next_idx().into(),
+            related_operations: vec![],
+            type_: OperationType::PaySui,
+            status,
+            account: Some(sender.into()),
+            amount: Some(Amount::new(SignedValue::neg(total_paid as u128))),
+            coin_change: None,
+            metadata: None,
+        });
+        pay_operations
+    } else {
+        let (type_, metadata) = match tx {
+            SuiTransactionKind::TransferObject(tx) => (OperationType::TransferObject, json!(tx)),
+            SuiTransactionKind::Publish(tx) => (OperationType::Publish, json!(tx.disassembled)),
+            SuiTransactionKind::Call(tx) => (OperationType::MoveCall, json!(tx)),
+            SuiTransactionKind::TransferSui(tx) => (OperationType::TransferSUI, json!(tx)),
+            SuiTransactionKind::Pay(tx) => (OperationType::Pay, json!(tx)),
+            SuiTransactionKind::PayAllSui(tx) => (OperationType::PayAllSui, json!(tx)),
+            SuiTransactionKind::ChangeEpoch(tx) => (OperationType::EpochChange, json!(tx)),
+            SuiTransactionKind::Genesis(tx) => (OperationType::Genesis, json!(tx)),
+            SuiTransactionKind::PaySui(_) => unreachable!(),
+        };
+        generic_operation(counter, type_, status, sender, metadata)
+    };
+    Ok(operations)
+}
+
+fn generic_operation(
+    counter: &mut IndexCounter,
+    type_: OperationType,
+    status: Option<OperationStatus>,
+    sender: SuiAddress,
+    metadata: Value,
+) -> Vec<Operation> {
+    vec![Operation {
+        operation_identifier: counter.next_idx().into(),
+        related_operations: vec![],
+        type_,
+        status,
+        account: Some(AccountIdentifier { address: sender }),
+        amount: None,
+        coin_change: None,
+        metadata: Some(metadata),
+    }]
+}
