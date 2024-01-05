4b992232618b3a31deca7cb1acf66e9d79f9dccf
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:28:19 +0400

    Initialize chris-disable-positional

---
+++ apps/wallet/src/ui/app/staking/stake/StakingCard.tsx
@@ -0,0 +1,358 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useCoinDecimals } from '@mysten/core';
+import {
+    getTransactionDigest,
+    SUI_TYPE_ARG,
+    type SuiAddress,
+} from '@mysten/sui.js';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import { Formik } from 'formik';
+import { useCallback, useMemo } from 'react';
+import { toast } from 'react-hot-toast';
+import { Navigate, useNavigate, useSearchParams } from 'react-router-dom';
+
+import Alert from '../../components/alert';
+import { getDelegationDataByStakeId } from '../getDelegationByStakeId';
+import { getStakeSuiBySuiId } from '../getStakeSuiBySuiId';
+import { useGetDelegatedStake } from '../useGetDelegatedStake';
+import { useSystemState } from '../useSystemState';
+import { DelegationState, STATE_TO_COPY } from './../home/DelegationCard';
+import StakeForm from './StakeForm';
+import { UnStakeForm } from './UnstakeForm';
+import { ValidatorFormDetail } from './ValidatorFormDetail';
+import { createValidationSchema } from './validation';
+import { useActiveAddress } from '_app/hooks/useActiveAddress';
+import BottomMenuLayout, {
+    Content,
+    Menu,
+} from '_app/shared/bottom-menu-layout';
+import Button from '_app/shared/button';
+import { Collapse } from '_app/shared/collapse';
+import Icon, { SuiIcons } from '_components/icon';
+import Loading from '_components/loading';
+import LoadingIndicator from '_components/loading/LoadingIndicator';
+import { parseAmount } from '_helpers';
+import { useSigner, useGetCoinBalance } from '_hooks';
+import { Coin } from '_redux/slices/sui-objects/Coin';
+import { trackEvent } from '_src/shared/plausible';
+import { Text } from '_src/ui/app/shared/text';
+
+import type { FormikHelpers } from 'formik';
+
+const initialValues = {
+    amount: '',
+    gasBudget: '',
+};
+
+export type FormValues = typeof initialValues;
+
+function StakingCard() {
+    const coinType = SUI_TYPE_ARG;
+    const accountAddress = useActiveAddress();
+    const { data: suiBalance, isLoading: loadingSuiBalances } =
+        useGetCoinBalance(coinType, accountAddress);
+    const coinBalance = BigInt(suiBalance?.totalBalance || 0);
+    const [searchParams] = useSearchParams();
+    const validatorAddress = searchParams.get('address');
+    const stakeSuiIdParams = searchParams.get('staked');
+    const unstake = searchParams.get('unstake') === 'true';
+    const { data: allDelegation, isLoading } = useGetDelegatedStake(
+        accountAddress || ''
+    );
+
+    const { data: system, isLoading: validatorsIsloading } = useSystemState();
+
+    const totalTokenBalance = useMemo(() => {
+        if (!allDelegation) return 0n;
+        // return only the total amount of tokens staked for a specific stakeId
+        return getStakeSuiBySuiId(allDelegation, stakeSuiIdParams);
+    }, [allDelegation, stakeSuiIdParams]);
+
+    const stakeData = useMemo(() => {
+        if (!allDelegation || !stakeSuiIdParams) return null;
+        // return delegation data for a specific stakeId
+        return getDelegationDataByStakeId(allDelegation, stakeSuiIdParams);
+    }, [allDelegation, stakeSuiIdParams]);
+
+    const coinSymbol = useMemo(
+        () => (coinType && Coin.getCoinSymbol(coinType)) || '',
+        [coinType]
+    );
+
+    const suiEarned = stakeData?.estimatedReward || 0;
+
+    const [coinDecimals] = useCoinDecimals(coinType);
+
+    const validationSchema = useMemo(
+        () =>
+            createValidationSchema(
+                coinBalance,
+                coinSymbol,
+                coinDecimals,
+                unstake
+            ),
+        [coinBalance, coinSymbol, coinDecimals, unstake]
+    );
+
+    const queryClient = useQueryClient();
+    const delegationId = useMemo(() => {
+        if (!stakeData || stakeData.status === 'Pending') return null;
+        return stakeData.stakedSuiId;
+    }, [stakeData]);
+
+    const navigate = useNavigate();
+    const signer = useSigner();
+
+    const stakeToken = useMutation({
+        mutationFn: async ({
+            tokenTypeArg,
+            amount,
+            validatorAddress,
+        }: {
+            tokenTypeArg: string;
+            amount: bigint;
+            validatorAddress: SuiAddress;
+        }) => {
+            if (!validatorAddress || !amount || !tokenTypeArg || !signer) {
+                throw new Error('Failed, missing required field');
+            }
+            trackEvent('Stake', {
+                props: { validator: validatorAddress },
+            });
+            return Coin.stakeCoin(signer, amount, validatorAddress);
+        },
+    });
+    const unStakeToken = useMutation({
+        mutationFn: async ({
+            delegationId,
+            stakeSuId,
+        }: {
+            delegationId: string;
+            stakeSuId: string;
+        }) => {
+            if (!delegationId || !stakeSuId || !signer) {
+                throw new Error(
+                    'Failed, missing required field (!principalWithdrawAmount | delegationId | stakeSuId).'
+                );
+            }
+
+            trackEvent('Unstake');
+
+            return Coin.unStakeCoin(signer, delegationId, stakeSuId);
+        },
+    });
+
+    const onHandleSubmit = useCallback(
+        async (
+            { amount }: FormValues,
+            { resetForm }: FormikHelpers<FormValues>
+        ) => {
+            if (coinType === null || validatorAddress === null) {
+                return;
+            }
+            try {
+                const bigIntAmount = parseAmount(amount, coinDecimals);
+                let response;
+                let txDigest;
+                if (unstake) {
+                    // check for delegation data
+                    if (
+                        !stakeData ||
+                        !stakeSuiIdParams ||
+                        stakeData.status === 'Pending'
+                    ) {
+                        return;
+                    }
+                    response = await unStakeToken.mutateAsync({
+                        delegationId: stakeData.stakedSuiId,
+                        stakeSuId: stakeSuiIdParams,
+                    });
+
+                    txDigest = getTransactionDigest(response);
+                } else {
+                    response = await stakeToken.mutateAsync({
+                        amount: bigIntAmount,
+                        tokenTypeArg: coinType,
+                        validatorAddress: validatorAddress,
+                    });
+                    txDigest = getTransactionDigest(response);
+                }
+
+                // Invalidate the react query for system state and validator
+                Promise.all([
+                    queryClient.invalidateQueries({
+                        queryKey: ['system', 'state'],
+                    }),
+                    queryClient.invalidateQueries({
+                        queryKey: ['validator'],
+                    }),
+                ]);
+                resetForm();
+
+                navigate(
+                    `/receipt?${new URLSearchParams({
+                        txdigest: txDigest,
+                        from: 'stake',
+                    }).toString()}`
+                );
+            } catch (e) {
+                const msg = (e as Error)?.message;
+                toast.error(
+                    <div className="max-w-xs overflow-hidden flex flex-col">
+                        <strong>{unstake ? 'Unstake' : 'Stake'} failed</strong>
+                        {msg ? (
+                            <small className="text-ellipsis overflow-hidden">
+                                {msg}
+                            </small>
+                        ) : null}
+                    </div>
+                );
+            }
+        },
+        [
+            coinType,
+            validatorAddress,
+            coinDecimals,
+            unstake,
+            queryClient,
+            navigate,
+            stakeData,
+            stakeSuiIdParams,
+            unStakeToken,
+            stakeToken,
+        ]
+    );
+
+    if (!coinType || !validatorAddress || (!validatorsIsloading && !system)) {
+        return <Navigate to="/" replace={true} />;
+    }
+    return (
+        <div className="flex flex-col flex-nowrap flex-grow w-full">
+            <Loading
+                loading={isLoading || validatorsIsloading || loadingSuiBalances}
+            >
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={onHandleSubmit}
+                >
+                    {({
+                        isSubmitting,
+                        isValid,
+                        submitForm,
+                        errors,
+                        touched,
+                    }) => (
+                        <BottomMenuLayout>
+                            <Content>
+                                <div className="mb-4">
+                                    <ValidatorFormDetail
+                                        validatorAddress={validatorAddress}
+                                        unstake={unstake}
+                                    />
+                                </div>
+
+                                {unstake ? (
+                                    <UnStakeForm
+                                        coinBalance={totalTokenBalance}
+                                        coinType={coinType}
+                                        stakingReward={suiEarned}
+                                    />
+                                ) : (
+                                    <StakeForm
+                                        coinBalance={coinBalance}
+                                        coinType={coinType}
+                                        epoch={system?.epoch}
+                                    />
+                                )}
+
+                                {(unstake || touched.amount) &&
+                                (errors.amount || errors.gasBudget) ? (
+                                    <div className="mt-2 flex flex-col flex-nowrap">
+                                        <Alert
+                                            mode="warning"
+                                            className="text-body"
+                                        >
+                                            {errors.amount || errors.gasBudget}
+                                        </Alert>
+                                    </div>
+                                ) : null}
+
+                                {!unstake && (
+                                    <div className="flex-1 mt-7.5">
+                                        <Collapse
+                                            title={
+                                                STATE_TO_COPY[
+                                                    stakeData?.status ===
+                                                    'Pending'
+                                                        ? DelegationState.WARM_UP
+                                                        : DelegationState.EARNING
+                                                ]
+                                            }
+                                            initialIsOpen
+                                        >
+                                            <Text
+                                                variant="p3"
+                                                color="steel-dark"
+                                                weight="normal"
+                                            >
+                                                The staked SUI starts earning
+                                                reward at the end of the Epoch
+                                                in which it was staked. The
+                                                rewards will become available at
+                                                the end of one full Epoch of
+                                                staking.
+                                            </Text>
+                                        </Collapse>
+                                    </div>
+                                )}
+                            </Content>
+
+                            <Menu
+                                stuckClass="staked-cta"
+                                className="w-full px-0 pb-0 mx-0"
+                            >
+                                <Button
+                                    size="large"
+                                    mode="neutral"
+                                    href="/stake"
+                                    disabled={isSubmitting}
+                                    className="!text-steel-darker w-1/2"
+                                >
+                                    <Icon
+                                        icon={SuiIcons.ArrowLeft}
+                                        className="text-body text-gray-65 font-normal"
+                                    />
+                                    Back
+                                </Button>
+                                <Button
+                                    size="large"
+                                    mode="primary"
+                                    onClick={submitForm}
+                                    className="w-1/2"
+                                    disabled={
+                                        !isValid ||
+                                        isSubmitting ||
+                                        (unstake && !delegationId)
+                                    }
+                                >
+                                    {isSubmitting ? (
+                                        <LoadingIndicator color="inherit" />
+                                    ) : unstake ? (
+                                        'Unstake Now'
+                                    ) : (
+                                        'Stake Now'
+                                    )}
+                                </Button>
+                            </Menu>
+                        </BottomMenuLayout>
+                    )}
+                </Formik>
+            </Loading>
+        </div>
+    );
+}
+
+export default StakingCard;
---
+++ crates/sui-types/src/governance.rs
@@ -0,0 +1,73 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use move_core_types::ident_str;
+use move_core_types::identifier::IdentStr;
+use move_core_types::language_storage::StructTag;
+
+use crate::balance::Balance;
+use crate::base_types::{ObjectID, SuiAddress};
+use crate::committee::EpochId;
+use crate::id::{ID, UID};
+use crate::SUI_FRAMEWORK_ADDRESS;
+use serde::Deserialize;
+use serde::Serialize;
+
+/// Minimum amount of stake required for a validator to be in the validator set
+pub const MINIMUM_VALIDATOR_STAKE_SUI: u64 = 25_000_000;
+
+pub const STAKING_POOL_MODULE_NAME: &IdentStr = ident_str!("staking_pool");
+pub const STAKED_SUI_STRUCT_NAME: &IdentStr = ident_str!("StakedSui");
+pub const DELEGATION_STRUCT_NAME: &IdentStr = ident_str!("Delegation");
+
+pub const ADD_DELEGATION_MUL_COIN_FUN_NAME: &IdentStr =
+    ident_str!("request_add_delegation_mul_coin");
+pub const ADD_DELEGATION_FUN_NAME: &IdentStr = ident_str!("request_add_delegation_mul_coin");
+pub const ADD_DELEGATION_LOCKED_COIN_FUN_NAME: &IdentStr =
+    ident_str!("request_add_delegation_mul_locked_coin");
+pub const WITHDRAW_DELEGATION_FUN_NAME: &IdentStr = ident_str!("request_withdraw_delegation");
+
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
+pub struct StakedSui {
+    id: UID,
+    pool_id: ID,
+    validator_address: SuiAddress,
+    delegation_request_epoch: u64,
+    principal: Balance,
+    sui_token_lock: Option<EpochId>,
+}
+
+impl StakedSui {
+    pub fn type_() -> StructTag {
+        StructTag {
+            address: SUI_FRAMEWORK_ADDRESS,
+            module: STAKING_POOL_MODULE_NAME.to_owned(),
+            name: STAKED_SUI_STRUCT_NAME.to_owned(),
+            type_params: vec![],
+        }
+    }
+
+    pub fn id(&self) -> ObjectID {
+        self.id.id.bytes
+    }
+
+    pub fn pool_id(&self) -> ObjectID {
+        self.pool_id.bytes
+    }
+
+    pub fn request_epoch(&self) -> EpochId {
+        self.delegation_request_epoch
+    }
+
+    pub fn principal(&self) -> u64 {
+        self.principal.value()
+    }
+
+    pub fn validator_address(&self) -> SuiAddress {
+        self.validator_address
+    }
+
+    pub fn sui_token_lock(&self) -> Option<EpochId> {
+        self.sui_token_lock
+    }
+}
