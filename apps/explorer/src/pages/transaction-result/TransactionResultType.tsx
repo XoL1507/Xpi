35e253b32777a69e600e21dd2e3fb912d7c13b89
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:59:05 +0400

    Initialize sui-62-add-ObjectType-to-transaction-effect

---
+++ apps/explorer/src/pages/transaction-result/TransactionResultType.tsx
@@ -0,0 +1,25 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import type {
+    CertifiedTransaction,
+    ExecutionStatusType,
+    SuiObjectRef,
+    SuiEvent,
+    SuiTransactionResponse,
+} from '@mysten/sui.js';
+
+export type DataType = CertifiedTransaction & {
+    transaction: SuiTransactionResponse | null;
+    loadState: string;
+    txId: string;
+    status: ExecutionStatusType;
+    gasFee: number;
+    txError: string;
+    mutated: SuiObjectRef[];
+    created: SuiObjectRef[];
+    events?: SuiEvent[];
+    timestamp_ms: number | null;
+};
+
+export type Category = 'object' | 'transaction' | 'address' | 'unknown';
