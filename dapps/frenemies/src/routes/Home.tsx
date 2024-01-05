9e8c57d00551a40d90c9cd6af07db4d966075da6
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:14:54 +0400

    Initialize aw/borrow-native

---
+++ dapps/frenemies/src/routes/Home.tsx
@@ -0,0 +1,124 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Round } from "../components/round/Round";
+import { Card } from "../components/Card";
+import { Stat } from "../components/Stat";
+import { Validators } from "../components/Validators";
+import { useScorecard } from "../network/queries/scorecard";
+import { formatGoal, formatTimeRemaining } from "../utils/format";
+import { useWalletKit } from "@mysten/wallet-kit";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Scoreboard } from "../components/Scoreboard";
+import { useEpoch } from "../network/queries/epoch";
+import { Goal, LEADERBOARD, Leaderboard } from "../network/types";
+import { Assignment } from "../components/Assignment";
+import { useSuiSystem } from "../network/queries/sui-system";
+import { Logo } from "../components/Validators/Logo";
+import { useRawObject } from "../network/queries/use-raw";
+import { config } from "../config";
+
+/**
+ * The Home page.
+ */
+export function Home() {
+  const navigate = useNavigate();
+  const { data: epoch } = useEpoch();
+  const { currentAccount } = useWalletKit();
+  const { data: leaderboard } = useRawObject<Leaderboard>(config.VITE_LEADERBOARD, LEADERBOARD);
+  const { data: scorecard, isSuccess } = useScorecard(currentAccount);
+  const { data: system } = useSuiSystem();
+
+  const { goal, validator } = scorecard?.data.assignment || {
+    goal: Goal.Neutral,
+    validator: "not_assigned",
+  };
+
+  const assignedValidator = system?.validators.fields.active_validators.find(
+    (v) => v.fields.metadata.fields.sui_address.replace("0x", "") === validator
+  );
+
+  useEffect(() => {
+    if (!currentAccount) {
+      navigate("/connect", { replace: true });
+    }
+  }, [currentAccount]);
+
+  useEffect(() => {
+    if (isSuccess && !scorecard) {
+      navigate("/setup", { replace: true });
+    }
+  }, [scorecard, isSuccess]);
+
+  const [timer, setTime] = useState(() => getTime(epoch?.timestamp, epoch?.prevTimestamp));
+
+  useEffect(() => {
+    if (!epoch) return;
+
+    const interval = setInterval(
+      () => setTime(getTime(epoch.timestamp, epoch.prevTimestamp)),
+      1000
+    );
+    return () => clearInterval(interval);
+  }, [epoch]);
+
+  const hasAssignment = !!scorecard
+    && !!epoch
+    && scorecard.data.assignment.epoch == epoch.data.epoch;
+
+  return (
+    <>
+      <Scoreboard />
+      <Round />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <Card spacing="sm">
+          <Stat label="Your Role">{hasAssignment ? formatGoal(goal) : 'Not Assigned'}</Stat>
+        </Card>
+        <Card spacing="sm">
+          <Stat label="Assigned Validator">
+            {assignedValidator && hasAssignment ? (
+              <div className="flex items-center gap-2">
+                <Logo
+                  src={
+                    assignedValidator.fields.metadata.fields.image_url as string
+                  }
+                  size="md"
+                  label={
+                    assignedValidator.fields.metadata.fields.name as string
+                  }
+                  circle
+                />
+                <div>{assignedValidator.fields.metadata.fields.name}</div>
+              </div>
+            ) : (
+              "--"
+            )}
+          </Stat>
+        </Card>
+        <Card spacing="sm">
+          <Stat label="Time Remaining">
+            <div className="text-steel-dark font-light">
+              {formatTimeRemaining(timer || 0)}
+            </div>
+          </Stat>
+        </Card>
+      </div>
+      {hasAssignment && <Assignment />}
+      <Validators hasAssignment={hasAssignment} />
+    </>
+  );
+}
+
+/**
+ * Calculate time left until the next epoch based on the last two timestamps
+ * for epoch changes (`timestamp` and `prevTimestamp`).
+ */
+function getTime(timestamp?: number, prevTimestamp?: number): number | null {
+  if (!timestamp || !prevTimestamp) return null;
+
+  const prevEpochLength = timestamp - prevTimestamp;
+  const timePassed = Date.now() - timestamp;
+  const timeLeft = prevEpochLength - timePassed;
+  return timeLeft <= 0 ? 0 : timeLeft;
+};
---
+++ dapps/frenemies/src/routes/Setup.tsx
@@ -0,0 +1,192 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+  LocalTxnDataSerializer,
+  UnserializedSignableTransaction,
+} from "@mysten/sui.js";
+import { useWalletKit } from "@mysten/wallet-kit";
+import { useMutation } from "@tanstack/react-query";
+import { FormEvent, useEffect, useId } from "react";
+import { useNavigate } from "react-router-dom";
+import { Card } from "../components/Card";
+import { config } from "../config";
+import { useEpoch } from "../network/queries/epoch";
+import { useScorecard } from "../network/queries/scorecard";
+import { SUI_SYSTEM_ID } from "../network/queries/sui-system";
+import { useMyType } from "../network/queries/use-raw";
+import { Coin, SUI_COIN } from "../network/types";
+import { getGas } from "../utils/coins";
+import provider from "../network/provider";
+
+const GAS_BUDGET = 10000n;
+
+export function Setup() {
+  const id = useId();
+  const navigate = useNavigate();
+  const { currentAccount, signAndExecuteTransaction } = useWalletKit();
+  const { data: scorecard, isSuccess } = useScorecard(currentAccount);
+  const { data: coins } = useMyType<Coin>(SUI_COIN, currentAccount);
+  const { data: epoch } = useEpoch();
+
+  const createScorecard = useMutation(
+    ["create-scorecard"],
+    async (username: string) => {
+      if (!currentAccount || !coins || !coins.length) {
+        throw new Error("No Coins found, please request some from faucet");
+      }
+
+      const gasPrice = epoch?.data.referenceGasPrice || 1n;
+      const checkTx: UnserializedSignableTransaction = {
+        kind: "moveCall",
+        data: {
+          packageObjectId: config.VITE_PKG,
+          module: "registry",
+          function: "is_registered",
+          typeArguments: [],
+          arguments: [config.VITE_REGISTRY, username],
+        },
+      };
+
+      const inspectRes = await provider.devInspectTransaction(
+        currentAccount,
+        checkTx,
+        Number(gasPrice)
+      );
+
+      if ("Err" in inspectRes.results) {
+        throw new Error(
+          `Error happened while checking for uniqueness: ${inspectRes.results.Err}`
+        );
+      }
+
+      const {
+        Ok: [
+          [,{ // @ts-ignore // not cool
+              returnValues: [[[ exists ]]],
+            },
+          ],
+        ],
+      } = inspectRes.results;
+
+      // Add a warning saying that the name is already taken.
+      // Depending on the the `exists` result: 0 or 1;
+      if (exists == 1) {
+        throw new Error(`Name: '${username}' is already taken`);
+      }
+
+      const gasSearch = GAS_BUDGET * gasPrice;
+      const { gas } = getGas(coins, gasSearch);
+      if (!gas) {
+        throw new Error(
+          `Gas object with at least '${gasSearch}' MIST not found`
+        );
+      }
+
+      const submitTx: UnserializedSignableTransaction = {
+        kind: "moveCall",
+        data: {
+          packageObjectId: config.VITE_PKG,
+          module: "frenemies",
+          function: "register",
+          arguments: [username, config.VITE_REGISTRY, SUI_SYSTEM_ID],
+          typeArguments: [],
+          gasPayment: gas.reference.objectId,
+
+          // TODO: Fix in sui.js - add option to use bigint...
+          gasBudget: Number(GAS_BUDGET),
+          gasPrice: Number(gasPrice),
+        },
+      };
+
+      const serializer = new LocalTxnDataSerializer(provider);
+      const serializedTx = await serializer.serializeToBytes(
+        currentAccount,
+        submitTx
+      );
+      const dryRunRes = await provider.dryRunTransaction(
+        serializedTx.toString()
+      );
+
+      if (dryRunRes.status.status == "failure") {
+        throw new Error(
+          `Transaction would've failed with a reason '${dryRunRes.status.error}'`
+        );
+      }
+
+      await signAndExecuteTransaction(submitTx);
+    },
+    {
+      onSuccess() {
+        navigate("/", { replace: true });
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (!currentAccount) {
+      navigate("/connect", { replace: true });
+    }
+  }, [currentAccount]);
+
+  useEffect(() => {
+    if (isSuccess && scorecard) {
+      navigate("/", { replace: true });
+    }
+  }, [scorecard, isSuccess]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    createScorecard.mutate(formData.get("username") as string);
+  };
+
+  // TODO: Loading UI:
+  if (!isSuccess || scorecard) {
+    return null;
+  }
+
+  return (
+    <div className="max-w-4xl w-full mx-auto text-center">
+      <Card spacing="xl">
+        <h1 className="text-steel-darker text-2xl leading-tight font-semibold mb-5">
+          Woo hoo! Just one more step before we begin.
+        </h1>
+        <img src="/capy_singing.svg" className="mb-5 h-64 w-64 mx-auto" />
+        <form onSubmit={handleSubmit}>
+          <label
+            htmlFor={id}
+            className="text-steel-darker leading-tight mb-3 block"
+          >
+            What shall we call you in the game?
+          </label>
+          <input
+            id={id}
+            name="username"
+            className="text-sm text-center w-56 mx-auto rounded-lg p-3 bg-white border border-gray-45 shadow-button leading-none"
+            placeholder="Enter a player name"
+            disabled={!isSuccess || createScorecard.isLoading}
+            required
+          />
+
+          {/* TODO: Nice error handling, this is just for debugging: */}
+          {createScorecard.isError && (
+            <div className="mt-4 text-issue-dark text-left text-xs bg-issue-light rounded p-4">
+              {String(createScorecard.error)}
+            </div>
+          )}
+
+          <div className="mt-16">
+            <button
+              type="submit"
+              className="shadow-notification bg-frenemies rounded-lg text-white disabled:text-white/50 px-5 py-3 w-56 leading-none"
+              disabled={!isSuccess || createScorecard.isLoading}
+            >
+              Continue
+            </button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+}
