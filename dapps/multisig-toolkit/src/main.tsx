747b6b47bd866671ceef65da9a34dc1404635adb
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:15:46 +0000

    Initialize separate-tx-indexing-metrics

---
+++ dapps/multisig-toolkit/src/main.tsx
@@ -0,0 +1,25 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import './index.css';
+import '@fontsource-variable/inter';
+import '@fontsource-variable/red-hat-mono';
+
+import { WalletKitProvider } from '@mysten/wallet-kit';
+import { QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+
+import { queryClient } from './lib/queryClient';
+import { router } from './routes';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<WalletKitProvider enableUnsafeBurner disableAutoConnect>
+				<RouterProvider router={router} />
+			</WalletKitProvider>
+		</QueryClientProvider>
+	</React.StrictMode>,
+);
