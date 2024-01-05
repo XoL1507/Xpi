32382249772c3a6e4302c8271fb214181159c219
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 20:11:51 +0000

    Initialize ak/testnet-stress-0427

---
+++ apps/icons/src/ChevronLeft12.tsx
@@ -0,0 +1,22 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import { SVGProps } from "react";
+const SvgChevronLeft12 = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 12 12"
+    {...props}
+  >
+    <path
+      d="M7 3L4 6L7 9"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default SvgChevronLeft12;
