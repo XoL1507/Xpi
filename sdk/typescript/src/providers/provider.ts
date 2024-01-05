f2b14eb648bcfaa519445515983b91e0c1a6e7ae
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:43:48 +0400

    Initialize tharbert/testnet_validator

---
+++ sdk/typescript/src/providers/provider.ts
@@ -0,0 +1,76 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { SignatureScheme } from '../cryptography/publickey';
+import {
+  GetObjectDataResponse,
+  SuiObjectInfo,
+  GatewayTxSeqNumber,
+  GetTxnDigestsResponse,
+  SuiTransactionResponse,
+  SuiObjectRef,
+} from '../types';
+
+///////////////////////////////
+// Exported Abstracts
+export abstract class Provider {
+  // Objects
+  /**
+   * Get all objects owned by an address
+   */
+  abstract getObjectsOwnedByAddress(
+    addressOrObjectId: string
+  ): Promise<SuiObjectInfo[]>;
+
+  /**
+   * Convenience method for getting all gas objects(SUI Tokens) owned by an address
+   */
+  abstract getGasObjectsOwnedByAddress(
+    _address: string
+  ): Promise<SuiObjectInfo[]>;
+
+  /**
+   * Get details about an object
+   */
+  abstract getObject(objectId: string): Promise<GetObjectDataResponse>;
+
+  /**
+   * Get object reference(id, tx digest, version id)
+   * @param objectId
+   */
+  abstract getObjectRef(objectId: string): Promise<SuiObjectRef | undefined>;
+
+  // Transactions
+  /**
+   * Get transaction digests for a given range
+   *
+   * NOTE: this method may get deprecated after DevNet
+   */
+  abstract getTransactionDigestsInRange(
+    start: GatewayTxSeqNumber,
+    end: GatewayTxSeqNumber
+  ): Promise<GetTxnDigestsResponse>;
+
+  /**
+   * Get the latest `count` transactions
+   *
+   * NOTE: this method may get deprecated after DevNet
+   */
+  abstract getRecentTransactions(count: number): Promise<GetTxnDigestsResponse>;
+
+  /**
+   * Get total number of transactions
+   * NOTE: this method may get deprecated after DevNet
+   */
+  abstract getTotalTransactionNumber(): Promise<number>;
+
+  abstract executeTransaction(
+    txnBytes: string,
+    signatureScheme: SignatureScheme,
+    signature: string,
+    pubkey: string
+  ): Promise<SuiTransactionResponse>;
+
+  abstract syncAccountState(address: string): Promise<any>;
+  // TODO: add more interface methods
+}
