e675b2a868454948e6a036c7e7418ae9c0b8e697
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:03:32 +0400

    Initialize load-kp

---
+++ sdk/typescript/test/e2e/read-transactions.test.ts
@@ -0,0 +1,73 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { describe, it, expect, beforeAll } from 'vitest';
+import { setup, TestToolbox } from './utils/setup';
+
+describe('Transaction Reading API', () => {
+  let toolbox: TestToolbox;
+
+  beforeAll(async () => {
+    toolbox = await setup();
+  });
+
+  it('Get Total Transactions', async () => {
+    const numTransactions = await toolbox.provider.getTotalTransactionNumber();
+    expect(numTransactions).to.greaterThan(0);
+  });
+
+  it('Get Transaction', async () => {
+    const resp = await toolbox.provider.getTransactions(
+      'All',
+      null,
+      1,
+    );
+    const digest = resp.data[0];
+    const txn = await toolbox.provider.getTransactionWithEffects(digest);
+    expect(txn.certificate.transactionDigest).toEqual(digest);
+  });
+
+  it('Get Transaction Auth Signers', async () => {
+    const version = await toolbox.provider.getRpcApiVersion();
+    // This endpoint is only available in 0.18 and above
+    if (version?.major === 0 && version?.minor < 18) { 
+      return;
+    }
+    
+    const resp = await toolbox.provider.getTransactions(
+      'All',
+      null,
+      1,
+    );
+    const digest = resp.data[0];
+    const res = await toolbox.provider.getTransactionAuthSigners(digest);
+    expect(res.signers.length).greaterThan(0);
+  });
+
+  it('Get Transactions', async () => {
+    const resp = await toolbox.provider.getTransactionsForAddress(
+      toolbox.address(),
+      false
+    );
+    expect(resp.length).to.greaterThan(0);
+
+    const allTransactions = await toolbox.provider.getTransactions(
+      'All',
+      null,
+      10,
+    );
+    expect(allTransactions.data.length).to.greaterThan(0);
+
+    const resp2 = await toolbox.provider.getTransactions(
+      { ToAddress: toolbox.address() },
+      null,
+      null,
+    );
+    const resp3 = await toolbox.provider.getTransactions(
+      { FromAddress: toolbox.address() },
+      null,
+      null,
+    );
+    expect([...resp2.data, ...resp3.data]).toEqual(resp);
+  });
+});
