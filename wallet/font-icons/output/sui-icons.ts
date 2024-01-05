deedb2818ea633d8b22322f4810a8a1c3c860f9e
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:45:16 +0400

    Initialize revert-4221-andrey-3

---
+++ wallet/font-icons/output/sui-icons.ts
@@ -0,0 +1,35 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+export enum SuiIcons {
+  Buy = "sui-icons-Buy",
+  Download = "sui-icons-Download",
+  Info = "sui-icons-Info",
+  Plus = "sui-icons-Plus",
+  Preview = "sui-icons-Preview",
+  ThumbsUp = "sui-icons-ThumbsUp",
+  Apps = "sui-icons-apps",
+  ArrowLeft = "sui-icons-arrow-left",
+  ArrowRight = "sui-icons-arrow-right",
+  Checkmark = "sui-icons-checkmark",
+  Clipboard = "sui-icons-clipboard",
+  Close = "sui-icons-close",
+  Coins = "sui-icons-coins",
+  Globe = "sui-icons-globe",
+  HandCoins = "sui-icons-hand-coins",
+  History = "sui-icons-history",
+  Logout = "sui-icons-logout",
+  Nfts = "sui-icons-nfts",
+  PercentagePolygon = "sui-icons-percentage-polygon",
+  Person = "sui-icons-person",
+  Search = "sui-icons-search",
+  SuiChevronRight = "sui-icons-sui-chevron-right",
+  SuiLogoIcon = "sui-icons-sui-logo-icon",
+  SuiLogoTxt = "sui-icons-sui-logo-txt",
+  Tokens = "sui-icons-tokens",
+  VersionIcon = "sui-icons-version-icon"
+}
+
+export type SuiIconsClassname = "sui-icons-Buy" | "sui-icons-Download" | "sui-icons-Info" | "sui-icons-Plus" | "sui-icons-Preview" | "sui-icons-ThumbsUp" | "sui-icons-apps" | "sui-icons-arrow-left" | "sui-icons-arrow-right" | "sui-icons-checkmark" | "sui-icons-clipboard" | "sui-icons-close" | "sui-icons-coins" | "sui-icons-globe" | "sui-icons-hand-coins" | "sui-icons-history" | "sui-icons-logout" | "sui-icons-nfts" | "sui-icons-percentage-polygon" | "sui-icons-person" | "sui-icons-search" | "sui-icons-sui-chevron-right" | "sui-icons-sui-logo-icon" | "sui-icons-sui-logo-txt" | "sui-icons-tokens" | "sui-icons-version-icon"
+export type SuiIconsIcon = "Buy" | "Download" | "Info" | "Plus" | "Preview" | "ThumbsUp" | "apps" | "arrow-left" | "arrow-right" | "checkmark" | "clipboard" | "close" | "coins" | "globe" | "hand-coins" | "history" | "logout" | "nfts" | "percentage-polygon" | "person" | "search" | "sui-chevron-right" | "sui-logo-icon" | "sui-logo-txt" | "tokens" | "version-icon"
+export const SuiIconsPrefix = "sui-icons-"
