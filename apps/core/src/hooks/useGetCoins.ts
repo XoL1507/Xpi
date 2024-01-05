6a8cc85a9988eaf8c709169e497c96351f4666e1
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:08:48 +0000

    Initialize cache-proof

---
+++ apps/core/src/hooks/useGetCoins.ts
@@ -0,0 +1,35 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useSuiClient } from '@mysten/dapp-kit';
+import { PaginatedCoins } from '@mysten/sui.js/client';
+import { UseInfiniteQueryResult, useInfiniteQuery } from '@tanstack/react-query';
+
+const MAX_COINS_PER_REQUEST = 10;
+
+export function useGetCoins(
+	coinType: string,
+	address?: string | null,
+	maxCoinsPerRequest = MAX_COINS_PER_REQUEST,
+): UseInfiniteQueryResult<PaginatedCoins> {
+	const client = useSuiClient();
+	return useInfiniteQuery(
+		['get-coins', address, coinType, maxCoinsPerRequest],
+		({ pageParam }) =>
+			client.getCoins({
+				owner: address!,
+				coinType,
+				cursor: pageParam ? pageParam.cursor : null,
+				limit: maxCoinsPerRequest,
+			}),
+		{
+			getNextPageParam: ({ hasNextPage, nextCursor }) =>
+				hasNextPage
+					? {
+							cursor: nextCursor,
+					  }
+					: false,
+			enabled: !!address,
+		},
+	);
+}
