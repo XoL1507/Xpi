11f176f5f219836278102ca8249cb3bb16a05c75
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:25:31 +0400

    Initialize mlogan-message-evolution

---
+++ crates/sui-indexer/src/models/mod.rs
@@ -0,0 +1,11 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod addresses;
+pub mod checkpoints;
+pub mod error_logs;
+pub mod events;
+pub mod objects;
+pub mod owners;
+pub mod packages;
+pub mod transactions;
