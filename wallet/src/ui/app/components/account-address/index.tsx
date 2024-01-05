61e393ca3a25d4e4e44f79ded0248191e7980fc7
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:39:54 +0400

    Initialize pc-wallet-ext-dapp-interface

---
+++ wallet/src/ui/app/components/account-address/index.tsx
@@ -0,0 +1,33 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import CopyToClipboard from '_components/copy-to-clipboard';
+import ExplorerLink from '_components/explorer-link';
+import { ExplorerLinkType } from '_components/explorer-link/ExplorerLinkType';
+import { useAppSelector, useMiddleEllipsis } from '_hooks';
+
+import st from './AccountAddress.module.scss';
+
+function AccountAddress() {
+    const address = useAppSelector(
+        ({ account: { address } }) => address && `0x${address}`
+    );
+    const shortenAddress = useMiddleEllipsis(address || '', 20);
+    return address ? (
+        <span className={st['address-container']}>
+            <CopyToClipboard txt={address}>
+                <span className={st.address} title={address}>
+                    {shortenAddress}
+                </span>
+            </CopyToClipboard>
+            <ExplorerLink
+                type={ExplorerLinkType.address}
+                useActiveAddress={true}
+                title="View account on Sui Explorer"
+                className={st.explorerLink}
+            />
+        </span>
+    ) : null;
+}
+
+export default AccountAddress;
