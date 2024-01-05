b2e051293cfaff3c1cb0d073257686e8a4c3a1cc
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:08:24 +0400

    Initialize mlogan-emma-bug

---
+++ apps/explorer/src/ui/InternalLink.tsx
@@ -0,0 +1,34 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { formatAddress } from '../utils/stringUtils';
+
+import { Link, type LinkProps } from '~/ui/Link';
+
+interface BaseInternalLinkProps extends LinkProps {
+    noTruncate?: boolean;
+}
+
+function createInternalLink<T extends string>(base: string, propName: T) {
+    return ({
+        [propName]: id,
+        noTruncate,
+        ...props
+    }: BaseInternalLinkProps & Record<T, string>) => {
+        const truncatedAddress = noTruncate ? id : formatAddress(id);
+        return (
+            <Link
+                variant="mono"
+                to={`/${base}/${encodeURIComponent(id)}`}
+                {...props}
+            >
+                {truncatedAddress}
+            </Link>
+        );
+    };
+}
+
+export const AddressLink = createInternalLink('address', 'address');
+export const ObjectLink = createInternalLink('object', 'objectId');
+export const TransactionLink = createInternalLink('transaction', 'digest');
+export const ValidatorLink = createInternalLink('validator', 'address');
