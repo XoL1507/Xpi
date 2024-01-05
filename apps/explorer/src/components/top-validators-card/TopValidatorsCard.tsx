5e268bd6ebc3e74308679d70c540534690f89028
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:06:21 +0400

    Initialize jordan--tsup-to-vite

---
+++ apps/explorer/src/components/top-validators-card/TopValidatorsCard.tsx
@@ -0,0 +1,266 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Base64DataBuffer, is, SuiObject, SUI_TYPE_ARG } from '@mysten/sui.js';
+import BigNumber from 'bignumber.js';
+import { useMemo } from 'react';
+
+import { ReactComponent as ArrowRight } from '../../assets/SVGIcons/12px/ArrowRight.svg';
+
+import { useFormatCoin } from '~/hooks/useFormatCoin';
+import { useGetObject } from '~/hooks/useGetObject';
+import { Banner } from '~/ui/Banner';
+import { AddressLink } from '~/ui/InternalLink';
+import { Link } from '~/ui/Link';
+import { PlaceholderTable } from '~/ui/PlaceholderTable';
+import { TableCard } from '~/ui/TableCard';
+import { Text } from '~/ui/Text';
+
+const VALIDATORS_OBJECT_ID = '0x05';
+const NUMBER_OF_VALIDATORS = 10;
+
+const VALDIATOR_NAME = /^[A-Z-_.\s0-9]+$/i;
+
+export type ValidatorMetadata = {
+    type: '0x2::validator::ValidatorMetadata';
+    fields: {
+        name: string | number[];
+        net_address: string;
+        next_epoch_stake: number;
+        pubkey_bytes: string;
+        sui_address: string;
+    };
+};
+
+export type Validator = {
+    type: '0x2::validator::Validator';
+    fields: {
+        delegation: bigint;
+        delegation_count: number;
+        metadata: ValidatorMetadata;
+        pending_delegation: bigint;
+        pending_delegation_withdraw: bigint;
+        pending_delegator_count: number;
+        pending_delegator_withdraw_count: number;
+        pending_stake: {
+            type: '0x1::option::Option<0x2::balance::Balance<0x2::sui::SUI>>';
+            fields: any;
+        };
+        pending_withdraw: bigint;
+        stake_amount: bigint;
+    };
+};
+
+export const STATE_DEFAULT: ValidatorState = {
+    delegation_reward: 0,
+    epoch: 0,
+    id: { id: '', version: 0 },
+    parameters: {
+        type: '0x2::sui_system::SystemParameters',
+        fields: {
+            max_validator_candidate_count: 0,
+            min_validator_stake: BigInt(0),
+        },
+    },
+    storage_fund: 0,
+    treasury_cap: {
+        type: '',
+        fields: {},
+    },
+    validators: {
+        type: '0x2::validator_set::ValidatorSet',
+        fields: {
+            delegation_stake: BigInt(0),
+            active_validators: [],
+            next_epoch_validators: [],
+            pending_removals: '',
+            pending_validators: '',
+            quorum_stake_threshold: BigInt(0),
+            total_validator_stake: BigInt(0),
+        },
+    },
+};
+
+const textDecoder = new TextDecoder();
+
+export type ObjFields = {
+    type: string;
+    fields: any;
+};
+
+export type SystemParams = {
+    type: '0x2::sui_system::SystemParameters';
+    fields: {
+        max_validator_candidate_count: number;
+        min_validator_stake: bigint;
+    };
+};
+
+export type ValidatorState = {
+    delegation_reward: number;
+    epoch: number;
+    id: { id: string; version: number };
+    parameters: SystemParams;
+    storage_fund: number;
+    treasury_cap: ObjFields;
+    validators: {
+        type: '0x2::validator_set::ValidatorSet';
+        fields: {
+            delegation_stake: bigint;
+            active_validators: Validator[];
+            next_epoch_validators: Validator[];
+            pending_removals: string;
+            pending_validators: string;
+            quorum_stake_threshold: bigint;
+            total_validator_stake: bigint;
+        };
+    };
+};
+
+function StakeColumn({ stake }: { stake: bigint }) {
+    const [amount, symbol] = useFormatCoin(stake, SUI_TYPE_ARG);
+    return (
+        <div className="flex items-end gap-0.5">
+            <Text variant="bodySmall/medium" color="steel-darker">
+                {amount}
+            </Text>
+            <Text variant="captionSmall/medium" color="steel-dark">
+                {symbol}
+            </Text>
+        </div>
+    );
+}
+
+export function processValidators(set: Validator[], totalStake: bigint) {
+    return set.map((av) => {
+        let name: string;
+        const rawName = av.fields.metadata.fields.name;
+        if (Array.isArray(rawName)) {
+            name = String.fromCharCode(...rawName);
+        } else {
+            name = textDecoder.decode(new Base64DataBuffer(rawName).getData());
+            if (!VALDIATOR_NAME.test(name)) {
+                name = rawName;
+            }
+        }
+        return {
+            name,
+            address: av.fields.metadata.fields.sui_address,
+            stake: av.fields.stake_amount,
+            stakePercent: getStakePercent(av.fields.stake_amount, totalStake),
+            delegation_count: av.fields.delegation_count || 0,
+        };
+    });
+}
+
+export const getStakePercent = (stake: bigint, total: bigint): number => {
+    const bnStake = new BigNumber(stake.toString());
+    const bnTotal = new BigNumber(total.toString());
+    return bnStake.div(bnTotal).multipliedBy(100).toNumber();
+};
+
+const validatorsTable = (validatorsData: ValidatorState, limit?: number) => {
+    const totalStake = validatorsData.validators.fields.total_validator_stake;
+
+    const validators = processValidators(
+        validatorsData.validators.fields.active_validators,
+        totalStake
+    ).sort((a, b) => (a.name > b.name ? 1 : -1));
+
+    const validatorsItems = limit ? validators.splice(0, limit) : validators;
+
+    return {
+        data: validatorsItems.map((validator) => {
+            return {
+                name: (
+                    <Text variant="bodySmall/medium" color="steel-darker">
+                        {validator.name}
+                    </Text>
+                ),
+                stake: <StakeColumn stake={validator.stake} />,
+                delegation: (
+                    <Text variant="bodySmall/medium" color="steel-darker">
+                        {validator.stake.toString()}
+                    </Text>
+                ),
+                address: (
+                    <AddressLink
+                        address={validator.address}
+                        noTruncate={!limit}
+                    />
+                ),
+            };
+        }),
+        columns: [
+            {
+                headerLabel: 'Name',
+                accessorKey: 'name',
+            },
+            {
+                headerLabel: 'Address',
+                accessorKey: 'address',
+            },
+            {
+                headerLabel: 'Stake',
+                accessorKey: 'stake',
+            },
+        ],
+    };
+};
+
+export function TopValidatorsCard({ limit }: { limit?: number }) {
+    const { data, isLoading, isSuccess, isError } =
+        useGetObject(VALIDATORS_OBJECT_ID);
+
+    const validatorData =
+        data &&
+        is(data.details, SuiObject) &&
+        data.details.data.dataType === 'moveObject'
+            ? (data.details.data.fields as ValidatorState)
+            : null;
+
+    const tableData = useMemo(
+        () => (validatorData ? validatorsTable(validatorData, limit) : null),
+        [validatorData, limit]
+    );
+
+    if (isError || (!isLoading && !tableData?.data.length)) {
+        return (
+            <Banner variant="error" fullWidth>
+                Validator data could not be loaded
+            </Banner>
+        );
+    }
+
+    return (
+        <>
+            {isLoading && (
+                <PlaceholderTable
+                    rowCount={limit || NUMBER_OF_VALIDATORS}
+                    rowHeight="13px"
+                    colHeadings={['Name', 'Address', 'Stake']}
+                    colWidths={['220px', '220px', '220px']}
+                />
+            )}
+
+            {isSuccess && tableData && (
+                <>
+                    <TableCard
+                        data={tableData.data}
+                        columns={tableData.columns}
+                    />
+                    {limit && (
+                        <div className="mt-3">
+                            <Link to="/validators">
+                                <div className="flex items-center gap-2">
+                                    More Validators
+                                    <ArrowRight fill="currentColor" />
+                                </div>
+                            </Link>
+                        </div>
+                    )}
+                </>
+            )}
+        </>
+    );
+}
---
+++ crates/sui-rosetta/src/state.rs
@@ -0,0 +1,461 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::operations::{Operation, Operations};
+use crate::types::{
+    Block, BlockHash, BlockHeight, BlockIdentifier, BlockResponse, OperationType, Transaction,
+    TransactionIdentifier,
+};
+use crate::Error;
+use anyhow::anyhow;
+use async_trait::async_trait;
+use mysten_metrics::spawn_monitored_task;
+use rocksdb::Options;
+use serde::Deserialize;
+use serde::Serialize;
+use std::collections::HashMap;
+use std::path::Path;
+use std::str::FromStr;
+use std::sync::Arc;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use sui_config::genesis::Genesis;
+use sui_sdk::SuiClient;
+use sui_storage::default_db_options;
+use sui_types::base_types::{SuiAddress, TransactionDigest, TRANSACTION_DIGEST_LENGTH};
+use sui_types::gas_coin::GasCoin;
+use sui_types::query::TransactionQuery;
+use tracing::{debug, error, info};
+use typed_store::rocks::{DBMap, DBOptions};
+use typed_store::traits::TableSummary;
+use typed_store::traits::TypedStoreDebug;
+use typed_store::Map;
+use typed_store_derive::DBMapUtils;
+
+#[cfg(test)]
+#[path = "unit_tests/balance_changing_tx_tests.rs"]
+mod balance_changing_tx_tests;
+
+#[derive(Clone)]
+pub struct OnlineServerContext {
+    pub client: SuiClient,
+    block_provider: Arc<dyn BlockProvider + Send + Sync>,
+}
+
+impl OnlineServerContext {
+    pub fn new(client: SuiClient, block_provider: Arc<dyn BlockProvider + Send + Sync>) -> Self {
+        Self {
+            client,
+            block_provider,
+        }
+    }
+
+    pub fn blocks(&self) -> &(dyn BlockProvider + Sync + Send) {
+        &*self.block_provider
+    }
+}
+
+#[async_trait]
+pub trait BlockProvider {
+    async fn get_block_by_index(&self, index: u64) -> Result<BlockResponse, Error>;
+    async fn get_block_by_hash(&self, hash: BlockHash) -> Result<BlockResponse, Error>;
+    async fn current_block(&self) -> Result<BlockResponse, Error>;
+    fn genesis_block_identifier(&self) -> BlockIdentifier;
+    async fn oldest_block_identifier(&self) -> Result<BlockIdentifier, Error>;
+    async fn current_block_identifier(&self) -> Result<BlockIdentifier, Error>;
+    async fn get_balance_at_block(
+        &self,
+        addr: SuiAddress,
+        block_height: u64,
+    ) -> Result<u128, Error>;
+}
+
+#[derive(Clone)]
+pub struct PseudoBlockProvider {
+    database: Arc<BlockProviderTables>,
+    client: SuiClient,
+    genesis: Genesis,
+}
+
+#[async_trait]
+impl BlockProvider for PseudoBlockProvider {
+    async fn get_block_by_index(&self, index: u64) -> Result<BlockResponse, Error> {
+        let (block_id, parent, timestamp) =
+            &self
+                .database
+                .blocks
+                .get(&index)?
+                .ok_or(Error::BlockNotFound {
+                    index: Some(index),
+                    hash: None,
+                })?;
+
+        Ok(self
+            .create_block_response(*block_id, *parent, *timestamp)
+            .await?)
+    }
+
+    async fn get_block_by_hash(&self, hash: BlockHash) -> Result<BlockResponse, Error> {
+        let height = self
+            .database
+            .block_heights
+            .get(&hash)?
+            .ok_or(Error::BlockNotFound {
+                index: None,
+                hash: Some(hash),
+            })?;
+        self.get_block_by_index(height).await
+    }
+
+    async fn current_block(&self) -> Result<BlockResponse, Error> {
+        let (_, (block_id, parent, timestamp)) = self
+            .database
+            .blocks
+            .iter()
+            .skip_prior_to(&BlockHeight::MAX)?
+            .next()
+            .ok_or(Error::BlockNotFound {
+                index: None,
+                hash: None,
+            })?;
+        Ok(self
+            .create_block_response(block_id, parent, timestamp)
+            .await?)
+    }
+
+    fn genesis_block_identifier(&self) -> BlockIdentifier {
+        BlockIdentifier {
+            index: 0,
+            hash: BlockHash::new([0u8; TRANSACTION_DIGEST_LENGTH]),
+        }
+    }
+
+    async fn oldest_block_identifier(&self) -> Result<BlockIdentifier, Error> {
+        self.database
+            .blocks
+            .iter()
+            .next()
+            .map(|(_, (id, _, _))| id)
+            .ok_or(Error::BlockNotFound {
+                index: None,
+                hash: None,
+            })
+    }
+
+    async fn current_block_identifier(&self) -> Result<BlockIdentifier, Error> {
+        self.current_block().await.map(|b| b.block.block_identifier)
+    }
+
+    async fn get_balance_at_block(
+        &self,
+        addr: SuiAddress,
+        block_height: u64,
+    ) -> Result<u128, Error> {
+        Ok(self
+            .database
+            .balances
+            .iter()
+            .skip_prior_to(&(addr, block_height))?
+            .next()
+            .and_then(|((address, _), balance)| {
+                if address == addr {
+                    Some(balance.balance)
+                } else {
+                    None
+                }
+            })
+            .unwrap_or_default())
+    }
+}
+
+impl PseudoBlockProvider {
+    pub fn spawn(client: SuiClient, genesis: Genesis, db_path: &Path) -> Self {
+        let blocks = Self {
+            database: Arc::new(BlockProviderTables::open(db_path, None)),
+            client: client.clone(),
+            genesis,
+        };
+
+        let block_interval = option_env!("SUI_BLOCK_INTERVAL")
+            .map(|i| u64::from_str(i).ok())
+            .flatten()
+            .unwrap_or(2000);
+        let block_interval = Duration::from_millis(block_interval);
+
+        let f = blocks.clone();
+        spawn_monitored_task!(async move {
+            if f.database.is_empty() {
+                info!("Database is empty, indexing genesis block...");
+                let genesis = genesis_block(&f.genesis);
+                let genesis_txs = genesis
+                    .block
+                    .transactions
+                    .iter()
+                    .flat_map(|tx| tx.operations.clone())
+                    .collect();
+
+                f.add_block_index(
+                    &genesis.block.block_identifier,
+                    &genesis.block.parent_block_identifier,
+                )
+                .unwrap();
+                if let Err(e) = f.update_balance(0, genesis_txs).await {
+                    error!("Error updating balance, cause: {e:?}")
+                }
+            } else {
+                let current_block = f.current_block_identifier().await.unwrap();
+                info!("Resuming from block {}", current_block.index);
+            };
+            loop {
+                if let Err(e) = f.create_next_block(&client).await {
+                    error!("Error creating block, cause: {e:?}")
+                }
+                tokio::time::sleep(block_interval).await;
+            }
+        });
+
+        blocks
+    }
+
+    async fn create_next_block(&self, client: &SuiClient) -> Result<(), Error> {
+        let current_block = self.current_block_identifier().await?;
+        let total_tx = client.read_api().get_total_transaction_number().await?;
+        if total_tx == 0 {
+            return Ok(());
+        }
+        if current_block.index < total_tx {
+            let tx_digests = if current_block.index == 0 {
+                client
+                    .read_api()
+                    .get_transactions(TransactionQuery::All, None, None, false)
+                    .await?
+                    .data
+            } else {
+                let cursor = current_block.hash;
+                let mut tx_digests = client
+                    .read_api()
+                    .get_transactions(TransactionQuery::All, Some(cursor), None, false)
+                    .await?
+                    .data;
+                if tx_digests.remove(0) != cursor {
+                    return Err(Error::DataError(
+                        "Incorrect transaction data returned from Sui.".to_string(),
+                    ));
+                }
+                tx_digests
+            };
+
+            let mut index = current_block.index;
+            let mut parent_block_identifier = current_block;
+
+            for digest in tx_digests {
+                index += 1;
+                let block_identifier = BlockIdentifier {
+                    index,
+                    hash: digest,
+                };
+
+                // update balance
+                let response = client.read_api().get_transaction(digest).await?;
+
+                let operations = response.try_into()?;
+
+                self.add_block_index(&block_identifier, &parent_block_identifier)?;
+                self.update_balance(index, operations)
+                    .await
+                    .map_err(|e| anyhow!("Failed to update balance, cause : {e}",))?;
+                parent_block_identifier = block_identifier
+            }
+        } else {
+            debug!("No new transactions.")
+        };
+
+        Ok(())
+    }
+
+    fn add_block_index(
+        &self,
+        block_id: &BlockIdentifier,
+        parent: &BlockIdentifier,
+    ) -> Result<(), Error> {
+        let index = &block_id.index;
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u64;
+        self.database.block_heights.insert(&block_id.hash, index)?;
+        self.database
+            .blocks
+            .insert(index, &(*block_id, *parent, timestamp))?;
+        Ok(())
+    }
+
+    async fn update_balance(
+        &self,
+        block_height: u64,
+        ops: Operations,
+    ) -> Result<(), anyhow::Error> {
+        for (addr, value) in extract_balance_changes_from_ops(ops)? {
+            let current_balance = self.get_balance_at_block(addr, block_height).await? as i128;
+            let new_balance = if value.is_negative() {
+                if current_balance < value.abs() {
+                    // This can happen due to missing transactions data due to unstable validators, causing balance to
+                    // fall below zero temporarily. The problem should go away when we start using checkpoints for event and indexing
+                    return Err(anyhow!(
+                        "Account gas value fall below 0 at block {}, address: [{}]",
+                        block_height,
+                        addr
+                    ));
+                }
+                current_balance - value.abs()
+            } else {
+                current_balance + value.abs()
+            };
+
+            self.database.balances.insert(
+                &(addr, block_height),
+                &HistoricBalance {
+                    block_height,
+                    balance: new_balance as u128,
+                },
+            )?;
+        }
+        Ok(())
+    }
+
+    async fn create_block_response(
+        &self,
+        block_identifier: BlockIdentifier,
+        parent_block_identifier: BlockIdentifier,
+        timestamp: u64,
+    ) -> Result<BlockResponse, Error> {
+        if block_identifier.index == 0 {
+            return Ok(genesis_block(&self.genesis));
+        }
+
+        let tx = self
+            .client
+            .read_api()
+            .get_transaction(block_identifier.hash)
+            .await?;
+
+        let digest = tx.certificate.transaction_digest;
+        let operations = tx.try_into()?;
+
+        let transaction = Transaction {
+            transaction_identifier: TransactionIdentifier { hash: digest },
+            operations,
+            related_transactions: vec![],
+            metadata: None,
+        };
+
+        Ok(BlockResponse {
+            block: Block {
+                block_identifier,
+                parent_block_identifier,
+                timestamp,
+                transactions: vec![transaction],
+                metadata: None,
+            },
+            other_transactions: vec![],
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct HistoricBalance {
+    block_height: u64,
+    balance: u128,
+}
+
+fn extract_balance_changes_from_ops(
+    ops: Operations,
+) -> Result<HashMap<SuiAddress, i128>, anyhow::Error> {
+    ops.into_iter()
+        .try_fold(HashMap::<SuiAddress, i128>::new(), |mut changes, op| {
+            match op.type_ {
+                OperationType::SuiBalanceChange
+                | OperationType::Gas
+                | OperationType::Genesis
+                | OperationType::PaySui => {
+                    let addr = op
+                        .account
+                        .ok_or_else(|| {
+                            anyhow!("Account address cannot be null for {:?}", op.type_)
+                        })?
+                        .address;
+                    let amount = op
+                        .amount
+                        .ok_or_else(|| anyhow!("Amount cannot be null for {:?}", op.type_))?;
+                    *changes.entry(addr).or_default() += amount.value
+                }
+                _ => {}
+            };
+            Ok(changes)
+        })
+}
+
+fn genesis_block(genesis: &Genesis) -> BlockResponse {
+    let id = BlockIdentifier {
+        index: 0,
+        hash: BlockHash::new([0u8; TRANSACTION_DIGEST_LENGTH]),
+    };
+
+    let operations = genesis
+        .objects()
+        .iter()
+        .flat_map(|o| {
+            GasCoin::try_from(o)
+                .ok()
+                .and_then(|coin| o.owner.get_owner_address().ok().map(|addr| (addr, coin)))
+        })
+        .enumerate()
+        .map(|(index, (address, coin))| Operation::genesis(index as u64, address, coin))
+        .collect();
+
+    let transaction = Transaction {
+        transaction_identifier: TransactionIdentifier {
+            hash: TransactionDigest::new([0; 32]),
+        },
+        operations,
+        related_transactions: vec![],
+        metadata: None,
+    };
+
+    BlockResponse {
+        block: Block {
+            block_identifier: id,
+            parent_block_identifier: id,
+            timestamp: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_millis() as u64,
+            transactions: vec![transaction],
+            metadata: None,
+        },
+        other_transactions: vec![],
+    }
+}
+
+#[derive(DBMapUtils)]
+pub struct BlockProviderTables {
+    #[default_options_override_fn = "default_config"]
+    blocks: DBMap<BlockHeight, (BlockIdentifier, BlockIdentifier, u64)>,
+    #[default_options_override_fn = "default_config"]
+    block_heights: DBMap<BlockHash, BlockHeight>,
+    #[default_options_override_fn = "default_config"]
+    balances: DBMap<(SuiAddress, u64), HistoricBalance>,
+}
+
+impl BlockProviderTables {
+    pub fn open(db_dir: &Path, db_options: Option<Options>) -> Self {
+        Self::open_tables_read_write(db_dir.to_path_buf(), db_options, None)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.blocks.is_empty()
+    }
+}
+
+fn default_config() -> DBOptions {
+    default_db_options(None, None).1
+}
