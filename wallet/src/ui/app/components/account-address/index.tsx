b4e459f3fc9950ff155cd758e7c001a9ecdb8d28
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:41:39 +0400

    Initialize issue_91_npm_token_add_project_file

---
+++ wallet/src/ui/app/components/account-address/index.tsx
@@ -0,0 +1,45 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import cl from 'classnames';
+
+import CopyToClipboard from '_components/copy-to-clipboard';
+import ExplorerLink from '_components/explorer-link';
+import { ExplorerLinkType } from '_components/explorer-link/ExplorerLinkType';
+import { useAppSelector, useMiddleEllipsis } from '_hooks';
+
+import st from './AccountAddress.module.scss';
+
+type AccountAddressProps = {
+    className?: string;
+    showLink?: boolean;
+    shorten?: boolean;
+};
+
+function AccountAddress({
+    className,
+    showLink = true,
+    shorten = true,
+}: AccountAddressProps) {
+    const address = useAppSelector(({ account: { address } }) => address);
+    const shortenAddress = useMiddleEllipsis(address || '', 20);
+    return address ? (
+        <span className={cl(st.addressContainer, className)}>
+            <CopyToClipboard txt={address}>
+                <span className={st.address} title={address}>
+                    {shorten ? shortenAddress : address}
+                </span>
+            </CopyToClipboard>
+            {showLink ? (
+                <ExplorerLink
+                    type={ExplorerLinkType.address}
+                    useActiveAddress={true}
+                    title="View account on Sui Explorer"
+                    className={st.explorerLink}
+                />
+            ) : null}
+        </span>
+    ) : null;
+}
+
+export default AccountAddress;
