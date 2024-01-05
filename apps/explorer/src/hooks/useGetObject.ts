f6980eed64969bf3311972339013ea06e281075e
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:31:57 +0400

    Initialize andrey-140

---
+++ apps/explorer/src/hooks/useGetObject.ts
@@ -0,0 +1,35 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useRpcClient } from '@mysten/core';
+import { type SuiObjectResponse, normalizeSuiAddress } from '@mysten/sui.js';
+import { useQuery, type UseQueryResult } from '@tanstack/react-query';
+
+export function useGetSystemObject() {
+    const rpc = useRpcClient();
+    return useQuery(['system', 'state'], () => rpc.getLatestSuiSystemState());
+}
+
+export function useGetObject(
+    objectId: string
+): UseQueryResult<SuiObjectResponse, unknown> {
+    const rpc = useRpcClient();
+    const normalizedObjId = normalizeSuiAddress(objectId);
+    const response = useQuery(
+        ['object', normalizedObjId],
+        async () =>
+            rpc.getObject({
+                id: normalizedObjId,
+                options: {
+                    showType: true,
+                    showContent: true,
+                    showOwner: true,
+                    showPreviousTransaction: true,
+                    showStorageRebate: true,
+                },
+            }),
+        { enabled: !!objectId }
+    );
+
+    return response;
+}
---
+++ sdk/typescript/test/e2e/entry-point-string.test.ts
@@ -0,0 +1,62 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { describe, it, expect, beforeAll } from 'vitest';
+import { getExecutionStatusType, ObjectId, Transaction } from '../../src';
+import {
+  DEFAULT_GAS_BUDGET,
+  publishPackage,
+  setup,
+  TestToolbox,
+} from './utils/setup';
+
+describe('Test Move call with strings', () => {
+  let toolbox: TestToolbox;
+  let packageId: ObjectId;
+
+  async function callWithString(
+    str: string | string[],
+    len: number,
+    funcName: string,
+  ) {
+    const tx = new Transaction();
+    tx.setGasBudget(DEFAULT_GAS_BUDGET);
+    tx.moveCall({
+      target: `${packageId}::entry_point_string::${funcName}`,
+      arguments: [tx.pure(str), tx.pure(len)],
+    });
+    const result = await toolbox.signer.signAndExecuteTransaction({
+      transaction: tx,
+      options: {
+        showEffects: true,
+      },
+    });
+    expect(getExecutionStatusType(result)).toEqual('success');
+  }
+
+  beforeAll(async () => {
+    toolbox = await setup();
+    const packagePath =
+      __dirname +
+      '/../../../../crates/sui-core/src/unit_tests/data/entry_point_string';
+    ({ packageId } = await publishPackage(packagePath));
+  });
+
+  it('Test ascii', async () => {
+    const s = 'SomeString';
+    await callWithString(s, s.length, 'ascii_arg');
+  });
+
+  it('Test utf8', async () => {
+    const s = 'çå∞≠¢õß∂ƒ∫';
+    const byte_len = 24;
+    await callWithString(s, byte_len, 'utf8_arg');
+  });
+
+  it('Test string vec', async () => {
+    const s1 = 'çå∞≠¢';
+    const s2 = 'õß∂ƒ∫';
+    const byte_len = 24;
+    await callWithString([s1, s2], byte_len, 'utf8_vec_arg');
+  });
+});
