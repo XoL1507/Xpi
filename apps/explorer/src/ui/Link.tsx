0ee0dea4efb9faaf250aa98f98e3f9eff8ee3d9b
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:34:43 +0400

    Initialize perf-09-15

---
+++ apps/explorer/src/ui/Link.tsx
@@ -0,0 +1,52 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { cva, type VariantProps } from 'class-variance-authority';
+import { type ReactNode } from 'react';
+
+import { ButtonOrLink, type ButtonOrLinkProps } from './utils/ButtonOrLink';
+
+const linkStyles = cva([], {
+    variants: {
+        variant: {
+            text: 'text-body font-semibold text-steel-dark hover:text-steel-darker active:text-steel disabled:text-gray-60',
+            mono: 'font-mono text-bodySmall font-medium text-sui-dark break-all',
+        },
+        uppercase: {
+            true: 'uppercase',
+        },
+        size: {
+            md: '!text-body',
+            sm: '!text-bodySmall',
+        },
+    },
+    defaultVariants: {
+        variant: 'text',
+    },
+});
+
+export interface LinkProps
+    extends ButtonOrLinkProps,
+        VariantProps<typeof linkStyles> {
+    before?: ReactNode;
+    after?: ReactNode;
+}
+
+export function Link({
+    variant,
+    size,
+    before,
+    after,
+    children,
+    ...props
+}: LinkProps) {
+    return (
+        <ButtonOrLink className={linkStyles({ variant, size })} {...props}>
+            <div className="inline-flex flex-nowrap gap-2">
+                {before}
+                {children}
+                {after}
+            </div>
+        </ButtonOrLink>
+    );
+}
