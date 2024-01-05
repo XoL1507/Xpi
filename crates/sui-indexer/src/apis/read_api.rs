ae5e8bf2dc40d641bad172dff0792ef65c6daf58
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:25:11 +0400

    Initialize epoch-flatten-start-config

---
+++ crates/sui-indexer/src/apis/read_api.rs
@@ -0,0 +1,387 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::errors::IndexerError;
+use crate::store::IndexerStore;
+use async_trait::async_trait;
+use jsonrpsee::core::RpcResult;
+use jsonrpsee::http_client::HttpClient;
+use jsonrpsee::RpcModule;
+use std::collections::BTreeMap;
+use sui_json_rpc::api::{cap_page_limit, ReadApiClient, ReadApiServer};
+use sui_json_rpc::SuiRpcModule;
+use sui_json_rpc_types::{
+    Checkpoint, CheckpointId, DynamicFieldPage, MoveFunctionArgType, Page,
+    SuiMoveNormalizedFunction, SuiMoveNormalizedModule, SuiMoveNormalizedStruct,
+    SuiObjectDataOptions, SuiObjectInfo, SuiObjectResponse, SuiPastObjectResponse,
+    SuiTransactionResponse, TransactionsPage,
+};
+use sui_open_rpc::Module;
+use sui_types::base_types::{ObjectID, SequenceNumber, SuiAddress, TxSequenceNumber};
+use sui_types::digests::{CheckpointContentsDigest, CheckpointDigest, TransactionDigest};
+use sui_types::dynamic_field::DynamicFieldName;
+use sui_types::messages_checkpoint::{
+    CheckpointContents, CheckpointSequenceNumber, CheckpointSummary,
+};
+use sui_types::query::TransactionQuery;
+
+pub(crate) struct ReadApi<S> {
+    fullnode: HttpClient,
+    state: S,
+    method_to_be_forwarded: Vec<String>,
+}
+
+impl<S: IndexerStore> ReadApi<S> {
+    pub fn new(state: S, fullnode_client: HttpClient) -> Self {
+        Self {
+            state,
+            fullnode: fullnode_client,
+            // TODO: read from config or env file
+            method_to_be_forwarded: vec![],
+        }
+    }
+
+    async fn get_total_transaction_number(&self) -> RpcResult<u64> {
+        let total_tx_number = self.state.get_total_transaction_number()?;
+        Ok(total_tx_number as u64)
+    }
+
+    async fn get_transaction(
+        &self,
+        digest: TransactionDigest,
+    ) -> RpcResult<SuiTransactionResponse> {
+        let txn_resp: SuiTransactionResponse = self
+            .state
+            .get_transaction_by_digest(digest.to_string())?
+            .try_into()?;
+        Ok(txn_resp)
+    }
+
+    async fn get_transactions(
+        &self,
+        query: TransactionQuery,
+        cursor: Option<TransactionDigest>,
+        limit: Option<usize>,
+        descending_order: Option<bool>,
+    ) -> RpcResult<TransactionsPage> {
+        let limit = cap_page_limit(limit);
+        let indexer_seq_number = self.state.get_transaction_sequence_by_digest(
+            cursor.map(|digest| digest.to_string()),
+            descending_order.unwrap_or_default(),
+        )?;
+
+        let digests_from_db = match query {
+            TransactionQuery::All => self.state.get_all_transaction_digest_page(
+                indexer_seq_number,
+                limit,
+                descending_order.unwrap_or_default(),
+            ),
+            // TODO(gegaowp): implement Move call query handling.
+            TransactionQuery::MoveFunction {
+                package: _,
+                module: _,
+                function: _,
+            } => Ok(vec![]),
+            // TODO(gegaowp): input objects are tricky to retrive from
+            // SuiTransactionResponse, instead we should store the BCS
+            // serialized transaction and retrive from there.
+            // This is now blocked by the endpoint on FN side.
+            TransactionQuery::InputObject(_input_obj_id) => Ok(vec![]),
+            TransactionQuery::MutatedObject(mutated_obj_id) => {
+                self.state.get_transaction_digest_page_by_mutated_object(
+                    mutated_obj_id.to_string(),
+                    indexer_seq_number,
+                    limit + 1,
+                    descending_order.unwrap_or_default(),
+                )
+            }
+            TransactionQuery::FromAddress(sender_address) => {
+                self.state.get_transaction_digest_page_by_sender_address(
+                    sender_address.to_string(),
+                    indexer_seq_number,
+                    limit + 1,
+                    descending_order.unwrap_or_default(),
+                )
+            }
+            TransactionQuery::ToAddress(recipient_address) => {
+                self.state.get_transaction_digest_page_by_recipient_address(
+                    recipient_address.to_string(),
+                    indexer_seq_number,
+                    limit + 1,
+                    descending_order.unwrap_or_default(),
+                )
+            }
+        }?;
+
+        // digests here are of size (limit + 1), where the last one is the cursor for the next page
+        let mut txn_digests = digests_from_db
+            .iter()
+            .map(|digest| {
+                let txn_digest: Result<TransactionDigest, _> = digest.clone().parse();
+                txn_digest.map_err(|e| {
+                    IndexerError::JsonSerdeError(format!(
+                        "Failed to deserialize transaction digest: {:?} with error {:?}",
+                        digest, e
+                    ))
+                })
+            })
+            .collect::<Result<Vec<TransactionDigest>, IndexerError>>()?;
+
+        let next_cursor = txn_digests.get(limit).cloned();
+        txn_digests.truncate(limit);
+
+        Ok(Page {
+            data: txn_digests,
+            next_cursor,
+        })
+    }
+
+    async fn get_latest_checkpoint_sequence_number(&self) -> Result<i64, IndexerError> {
+        self.state.get_latest_checkpoint_sequence_number()
+    }
+
+    async fn get_checkpoint(&self, id: CheckpointId) -> Result<Checkpoint, IndexerError> {
+        let checkpoint = self.state.get_checkpoint(id)?;
+        checkpoint.try_into()
+    }
+}
+
+#[async_trait]
+impl<S> ReadApiServer for ReadApi<S>
+where
+    S: IndexerStore + Sync + Send + 'static,
+{
+    async fn get_objects_owned_by_address(
+        &self,
+        address: SuiAddress,
+    ) -> RpcResult<Vec<SuiObjectInfo>> {
+        self.fullnode.get_objects_owned_by_address(address).await
+    }
+
+    async fn get_dynamic_fields(
+        &self,
+        parent_object_id: ObjectID,
+        cursor: Option<ObjectID>,
+        limit: Option<usize>,
+    ) -> RpcResult<DynamicFieldPage> {
+        self.fullnode
+            .get_dynamic_fields(parent_object_id, cursor, limit)
+            .await
+    }
+
+    async fn get_object_with_options(
+        &self,
+        object_id: ObjectID,
+        options: Option<SuiObjectDataOptions>,
+    ) -> RpcResult<SuiObjectResponse> {
+        self.fullnode
+            .get_object_with_options(object_id, options)
+            .await
+    }
+
+    async fn get_dynamic_field_object(
+        &self,
+        parent_object_id: ObjectID,
+        name: DynamicFieldName,
+    ) -> RpcResult<SuiObjectResponse> {
+        self.fullnode
+            .get_dynamic_field_object(parent_object_id, name)
+            .await
+    }
+
+    async fn get_total_transaction_number(&self) -> RpcResult<u64> {
+        if self
+            .method_to_be_forwarded
+            .contains(&"get_total_transaction_number".to_string())
+        {
+            return self.fullnode.get_total_transaction_number().await;
+        }
+        self.get_total_transaction_number().await
+    }
+
+    async fn get_transactions(
+        &self,
+        query: TransactionQuery,
+        cursor: Option<TransactionDigest>,
+        limit: Option<usize>,
+        descending_order: Option<bool>,
+    ) -> RpcResult<TransactionsPage> {
+        if self
+            .method_to_be_forwarded
+            .contains(&"get_transactions".to_string())
+        {
+            return self
+                .fullnode
+                .get_transactions(query, cursor, limit, descending_order)
+                .await;
+        }
+        self.get_transactions(query, cursor, limit, descending_order)
+            .await
+    }
+
+    async fn get_transactions_in_range(
+        &self,
+        start: TxSequenceNumber,
+        end: TxSequenceNumber,
+    ) -> RpcResult<Vec<TransactionDigest>> {
+        self.fullnode.get_transactions_in_range(start, end).await
+    }
+
+    async fn get_transaction(
+        &self,
+        digest: TransactionDigest,
+    ) -> RpcResult<SuiTransactionResponse> {
+        if self
+            .method_to_be_forwarded
+            .contains(&"get_transaction".to_string())
+        {
+            return self.fullnode.get_transaction(digest).await;
+        }
+        self.get_transaction(digest).await
+    }
+
+    async fn multi_get_transactions(
+        &self,
+        digests: Vec<TransactionDigest>,
+    ) -> RpcResult<Vec<SuiTransactionResponse>> {
+        if self
+            .method_to_be_forwarded
+            .contains(&"muti_get_transactions".to_string())
+        {
+            return self.fullnode.multi_get_transactions(digests).await;
+        }
+        self.multi_get_transactions(digests).await
+    }
+
+    async fn get_normalized_move_modules_by_package(
+        &self,
+        package: ObjectID,
+    ) -> RpcResult<BTreeMap<String, SuiMoveNormalizedModule>> {
+        self.fullnode
+            .get_normalized_move_modules_by_package(package)
+            .await
+    }
+
+    async fn get_normalized_move_module(
+        &self,
+        package: ObjectID,
+        module_name: String,
+    ) -> RpcResult<SuiMoveNormalizedModule> {
+        self.fullnode
+            .get_normalized_move_module(package, module_name)
+            .await
+    }
+
+    async fn get_normalized_move_struct(
+        &self,
+        package: ObjectID,
+        module_name: String,
+        struct_name: String,
+    ) -> RpcResult<SuiMoveNormalizedStruct> {
+        self.fullnode
+            .get_normalized_move_struct(package, module_name, struct_name)
+            .await
+    }
+
+    async fn get_normalized_move_function(
+        &self,
+        package: ObjectID,
+        module_name: String,
+        function_name: String,
+    ) -> RpcResult<SuiMoveNormalizedFunction> {
+        self.fullnode
+            .get_normalized_move_function(package, module_name, function_name)
+            .await
+    }
+
+    async fn get_move_function_arg_types(
+        &self,
+        package: ObjectID,
+        module: String,
+        function: String,
+    ) -> RpcResult<Vec<MoveFunctionArgType>> {
+        self.fullnode
+            .get_move_function_arg_types(package, module, function)
+            .await
+    }
+
+    async fn try_get_past_object(
+        &self,
+        object_id: ObjectID,
+        version: SequenceNumber,
+    ) -> RpcResult<SuiPastObjectResponse> {
+        self.fullnode.try_get_past_object(object_id, version).await
+    }
+
+    async fn get_latest_checkpoint_sequence_number(&self) -> RpcResult<CheckpointSequenceNumber> {
+        if self
+            .method_to_be_forwarded
+            .contains(&"get_latest_checkpoint_sequence_number".to_string())
+        {
+            return self.fullnode.get_latest_checkpoint_sequence_number().await;
+        }
+        Ok(self.get_latest_checkpoint_sequence_number().await? as u64)
+    }
+
+    async fn get_checkpoint(&self, id: CheckpointId) -> RpcResult<Checkpoint> {
+        if self
+            .method_to_be_forwarded
+            .contains(&"get_checkpoint".to_string())
+        {
+            return self.fullnode.get_checkpoint(id).await;
+        }
+        Ok(self.get_checkpoint(id).await?)
+    }
+
+    // NOTE: checkpoint APIs below will be deprecated,
+    // thus skipping them regarding indexer native implementations.
+    async fn get_checkpoint_summary_by_digest(
+        &self,
+        digest: CheckpointDigest,
+    ) -> RpcResult<CheckpointSummary> {
+        self.fullnode.get_checkpoint_summary_by_digest(digest).await
+    }
+
+    async fn get_checkpoint_summary(
+        &self,
+        sequence_number: CheckpointSequenceNumber,
+    ) -> RpcResult<CheckpointSummary> {
+        self.fullnode.get_checkpoint_summary(sequence_number).await
+    }
+
+    async fn get_checkpoint_contents_by_digest(
+        &self,
+        digest: CheckpointContentsDigest,
+    ) -> RpcResult<CheckpointContents> {
+        self.fullnode
+            .get_checkpoint_contents_by_digest(digest)
+            .await
+    }
+
+    async fn get_checkpoint_contents(
+        &self,
+        sequence_number: CheckpointSequenceNumber,
+    ) -> RpcResult<CheckpointContents> {
+        self.fullnode.get_checkpoint_contents(sequence_number).await
+    }
+
+    async fn get_display_deprecated(
+        &self,
+        object_id: ObjectID,
+    ) -> RpcResult<BTreeMap<String, String>> {
+        self.fullnode.get_display_deprecated(object_id).await
+    }
+}
+
+impl<S> SuiRpcModule for ReadApi<S>
+where
+    S: IndexerStore + Sync + Send + 'static,
+{
+    fn rpc(self) -> RpcModule<Self> {
+        self.into_rpc()
+    }
+
+    fn rpc_doc_module() -> Module {
+        sui_json_rpc::api::ReadApiOpenRpc::module_doc()
+    }
+}
---
+++ sdk/typescript/src/types/objects.ts
@@ -0,0 +1,337 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+  any,
+  array,
+  assign,
+  boolean,
+  Infer,
+  literal,
+  map,
+  number,
+  object,
+  optional,
+  record,
+  string,
+  union,
+} from 'superstruct';
+import {
+  ObjectId,
+  ObjectOwner,
+  SequenceNumber,
+  TransactionDigest,
+} from './common';
+
+export const ObjectType = union([string(), literal('package')]);
+export type ObjectType = Infer<typeof ObjectType>;
+
+export const SuiObjectRef = object({
+  /** Base64 string representing the object digest */
+  digest: TransactionDigest,
+  /** Hex code as string representing the object id */
+  objectId: string(),
+  /** Object version */
+  version: number(),
+});
+export type SuiObjectRef = Infer<typeof SuiObjectRef>;
+
+export const SuiGasData = object({
+  payment: array(SuiObjectRef),
+  /** Gas Object's owner */
+  owner: string(),
+  price: number(),
+  budget: number(),
+});
+export type SuiGasData = Infer<typeof SuiGasData>;
+
+export const SuiObjectInfo = assign(
+  SuiObjectRef,
+  object({
+    type: string(),
+    owner: ObjectOwner,
+    previousTransaction: TransactionDigest,
+  }),
+);
+export type SuiObjectInfo = Infer<typeof SuiObjectInfo>;
+
+export const ObjectContentFields = record(string(), any());
+export type ObjectContentFields = Infer<typeof ObjectContentFields>;
+
+export const MovePackageContent = record(string(), string());
+export type MovePackageContent = Infer<typeof MovePackageContent>;
+
+export const SuiMoveObject = object({
+  /** Move type (e.g., "0x2::coin::Coin<0x2::sui::SUI>") */
+  type: string(),
+  /** Fields and values stored inside the Move object */
+  fields: ObjectContentFields,
+  hasPublicTransfer: boolean(),
+});
+export type SuiMoveObject = Infer<typeof SuiMoveObject>;
+
+export const SuiMovePackage = object({
+  /** A mapping from module name to disassembled Move bytecode */
+  disassembled: MovePackageContent,
+});
+export type SuiMovePackage = Infer<typeof SuiMovePackage>;
+
+export const SuiParsedData = union([
+  assign(SuiMoveObject, object({ dataType: literal('moveObject') })),
+  assign(SuiMovePackage, object({ dataType: literal('package') })),
+]);
+export type SuiParsedData = Infer<typeof SuiParsedData>;
+
+export const SuiRawMoveObject = object({
+  /** Move type (e.g., "0x2::coin::Coin<0x2::sui::SUI>") */
+  type: string(),
+  hasPublicTransfer: boolean(),
+  version: SequenceNumber,
+  bcsBytes: array(number()),
+});
+export type SuiRawMoveObject = Infer<typeof SuiRawMoveObject>;
+
+export const SuiRawMovePackage = object({
+  id: ObjectId,
+  /** A mapping from module name to Move bytecode enocded in base64*/
+  moduleMap: map(string(), string()),
+});
+export type SuiRawMovePackage = Infer<typeof SuiRawMovePackage>;
+
+// TODO(chris): consolidate SuiRawParsedData and SuiRawObject using generics
+export const SuiRawData = union([
+  assign(SuiMoveObject, object({ dataType: literal('moveObject') })),
+  assign(SuiRawMovePackage, object({ dataType: literal('package') })),
+]);
+export type SuiRawData = Infer<typeof SuiRawData>;
+
+export const MIST_PER_SUI = BigInt(1000000000);
+
+export const ObjectDigest = string();
+export type ObjectDigest = Infer<typeof ObjectDigest>;
+
+export const SuiObjectData = object({
+  objectId: ObjectId,
+  version: SequenceNumber,
+  digest: ObjectDigest,
+  /**
+   * Type of the object, default to be undefined unless SuiObjectDataOptions.showType is set to true
+   */
+  type: optional(string()),
+  /**
+   * Move object content or package content, default to be undefined unless SuiObjectDataOptions.showContent is set to true
+   */
+  content: optional(SuiParsedData),
+  /**
+   * Move object content or package content in BCS bytes, default to be undefined unless SuiObjectDataOptions.showBcs is set to true
+   */
+  bcs: optional(SuiRawData),
+  /**
+   * The owner of this object. Default to be undefined unless SuiObjectDataOptions.showOwner is set to true
+   */
+  owner: optional(ObjectOwner),
+  /**
+   * The digest of the transaction that created or last mutated this object.
+   * Default to be undefined unless SuiObjectDataOptions.showPreviousTransaction is set to true
+   */
+  previousTransaction: optional(TransactionDigest),
+  /**
+   * The amount of SUI we would rebate if this object gets deleted.
+   * This number is re-calculated each time the object is mutated based on
+   * the present storage gas price.
+   * Default to be undefined unless SuiObjectDataOptions.showStorageRebate is set to true
+   */
+  storageRebate: optional(number()),
+});
+export type SuiObjectData = Infer<typeof SuiObjectData>;
+
+/**
+ * Config for fetching object data
+ */
+export const SuiObjectDataOptions = object({
+  /* Whether to fetch the object type, default to be true */
+  showType: optional(boolean()),
+  /* Whether to fetch the object content, default to be false */
+  showContent: optional(boolean()),
+  /* Whether to fetch the object content in BCS bytes, default to be false */
+  showBcs: optional(boolean()),
+  /* Whether to fetch the object owner, default to be false */
+  showOwner: optional(boolean()),
+  /* Whether to fetch the previous transaction digest, default to be false */
+  showPreviousTransaction: optional(boolean()),
+  /* Whether to fetch the storage rebate, default to be false */
+  showStorageRebate: optional(boolean()),
+});
+export type SuiObjectDataOptions = Infer<typeof SuiObjectDataOptions>;
+
+export const ObjectStatus = union([
+  literal('Exists'),
+  literal('NotExists'),
+  literal('Deleted'),
+]);
+export type ObjectStatus = Infer<typeof ObjectStatus>;
+
+export const GetOwnedObjectsResponse = array(SuiObjectInfo);
+export type GetOwnedObjectsResponse = Infer<typeof GetOwnedObjectsResponse>;
+
+export const SuiObjectResponse = object({
+  status: ObjectStatus,
+  details: union([SuiObjectData, ObjectId, SuiObjectRef]),
+});
+export type SuiObjectResponse = Infer<typeof SuiObjectResponse>;
+
+export type Order = 'ascending' | 'descending';
+
+/* -------------------------------------------------------------------------- */
+/*                              Helper functions                              */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------- SuiObjectResponse ------------------------- */
+
+export function getSuiObjectData(
+  resp: SuiObjectResponse,
+): SuiObjectData | undefined {
+  return resp.status !== 'Exists' ? undefined : (resp.details as SuiObjectData);
+}
+
+export function getObjectDeletedResponse(
+  resp: SuiObjectResponse,
+): SuiObjectRef | undefined {
+  return resp.status !== 'Deleted' ? undefined : (resp.details as SuiObjectRef);
+}
+
+export function getObjectNotExistsResponse(
+  resp: SuiObjectResponse,
+): ObjectId | undefined {
+  return resp.status !== 'NotExists' ? undefined : (resp.details as ObjectId);
+}
+
+export function getObjectReference(
+  resp: SuiObjectResponse,
+): SuiObjectRef | undefined {
+  const exists = getSuiObjectData(resp);
+  if (exists) {
+    return {
+      objectId: exists.objectId,
+      version: exists.version,
+      digest: exists.digest,
+    };
+  }
+  return getObjectDeletedResponse(resp);
+}
+
+/* ------------------------------ SuiObjectRef ------------------------------ */
+
+export function getObjectId(data: SuiObjectResponse | SuiObjectRef): ObjectId {
+  if ('objectId' in data) {
+    return data.objectId;
+  }
+  return (
+    getObjectReference(data)?.objectId ?? getObjectNotExistsResponse(data)!
+  );
+}
+
+export function getObjectVersion(
+  data: SuiObjectResponse | SuiObjectRef | SuiObjectData,
+): number | undefined {
+  if ('version' in data) {
+    return data.version;
+  }
+  return getObjectReference(data)?.version;
+}
+
+/* -------------------------------- SuiObject ------------------------------- */
+
+/**
+ * Deriving the object type from the object response
+ * @returns 'package' if the object is a package, move object type(e.g., 0x2::coin::Coin<0x2::sui::SUI>)
+ * if the object is a move object
+ */
+export function getObjectType(
+  resp: SuiObjectResponse | SuiObjectData,
+): ObjectType | undefined {
+  const data = 'status' in resp ? getSuiObjectData(resp) : resp;
+
+  if (!data?.type && 'status' in resp) {
+    if (data?.content?.dataType === 'package') {
+      return 'package';
+    }
+    return getMoveObjectType(resp);
+  }
+  return data?.type;
+}
+
+export function getObjectPreviousTransactionDigest(
+  resp: SuiObjectResponse,
+): TransactionDigest | undefined {
+  return getSuiObjectData(resp)?.previousTransaction;
+}
+
+export function getObjectOwner(
+  resp: SuiObjectResponse,
+): ObjectOwner | undefined {
+  return getSuiObjectData(resp)?.owner;
+}
+
+export function getSharedObjectInitialVersion(
+  resp: SuiObjectResponse,
+): number | undefined {
+  const owner = getObjectOwner(resp);
+  if (typeof owner === 'object' && 'Shared' in owner) {
+    return owner.Shared.initial_shared_version;
+  } else {
+    return undefined;
+  }
+}
+
+export function isSharedObject(resp: SuiObjectResponse): boolean {
+  const owner = getObjectOwner(resp);
+  return typeof owner === 'object' && 'Shared' in owner;
+}
+
+export function isImmutableObject(resp: SuiObjectResponse): boolean {
+  const owner = getObjectOwner(resp);
+  return owner === 'Immutable';
+}
+
+export function getMoveObjectType(resp: SuiObjectResponse): string | undefined {
+  return getMoveObject(resp)?.type;
+}
+
+export function getObjectFields(
+  resp: SuiObjectResponse | SuiMoveObject | SuiObjectData,
+): ObjectContentFields | undefined {
+  if ('fields' in resp) {
+    return resp.fields;
+  }
+  return getMoveObject(resp)?.fields;
+}
+
+export function getMoveObject(
+  data: SuiObjectResponse | SuiObjectData,
+): SuiMoveObject | undefined {
+  const suiObject = 'status' in data ? getSuiObjectData(data) : data;
+  if (suiObject?.content?.dataType !== 'moveObject') {
+    return undefined;
+  }
+  return suiObject.content as SuiMoveObject;
+}
+
+export function hasPublicTransfer(
+  data: SuiObjectResponse | SuiObjectData,
+): boolean {
+  return getMoveObject(data)?.hasPublicTransfer ?? false;
+}
+
+export function getMovePackageContent(
+  data: SuiObjectResponse | SuiMovePackage,
+): MovePackageContent | undefined {
+  if ('disassembled' in data) {
+    return data.disassembled;
+  }
+  const suiObject = getSuiObjectData(data);
+  if (suiObject?.content?.dataType !== 'package') {
+    return undefined;
+  }
+  return (suiObject.content as SuiMovePackage).disassembled;
+}
