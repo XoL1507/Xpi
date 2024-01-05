481ba88055616a77c1d39229fe2e9ce70b61b48e
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 20:23:45 +0000

    Initialize wrobertson/fix_wallet_state

---
+++ apps/core/src/hooks/useGetReferenceGasPrice.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useRpcClient } from '../api/RpcClientContext';
+import { useQuery } from '@tanstack/react-query';
+
+export function useGetReferenceGasPrice() {
+    const rpc = useRpcClient();
+    return useQuery(
+        ['current-epoch', 'gas-price', rpc.connection.fullnode],
+        () => rpc.getReferenceGasPrice()
+    );
+}
