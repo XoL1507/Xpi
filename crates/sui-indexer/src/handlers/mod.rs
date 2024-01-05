2638dcf33f99d40bab9ba9c9c26931db55de598b
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:08:02 +0000

    Initialize indexer-v2-4-tx-processor

---
+++ crates/sui-indexer/src/handlers/mod.rs
@@ -0,0 +1,5 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod checkpoint_handler;
+pub mod tx_processor;
---
+++ sdk/dapp-kit/test/test-utils.tsx
@@ -0,0 +1,49 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import type { SuiClient } from '@mysten/sui.js/client';
+import type { IdentifierRecord, ReadonlyWalletAccount } from '@mysten/wallet-standard';
+import { getWallets } from '@mysten/wallet-standard';
+import { SuiClientProvider, WalletProvider } from 'dapp-kit/src';
+import { MockWallet } from './mocks/mockWallet.js';
+import type { ComponentProps } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createMockAccount } from './mocks/mockAccount.js';
+
+export function createSuiClientContextWrapper(client: SuiClient) {
+	return function SuiClientContextWrapper({ children }: { children: React.ReactNode }) {
+		return <SuiClientProvider networks={{ test: client }}>{children}</SuiClientProvider>;
+	};
+}
+
+export function createWalletProviderContextWrapper(
+	providerProps: Omit<ComponentProps<typeof WalletProvider>, 'children'> = {},
+) {
+	const queryClient = new QueryClient();
+	return function WalletProviderContextWrapper({ children }: { children: React.ReactNode }) {
+		return (
+			<SuiClientProvider>
+				<QueryClientProvider client={queryClient}>
+					<WalletProvider {...providerProps}>{children}</WalletProvider>;
+				</QueryClientProvider>
+			</SuiClientProvider>
+		);
+	};
+}
+
+export function registerMockWallet({
+	walletName,
+	accounts = [createMockAccount()],
+	additionalFeatures = {},
+}: {
+	walletName: string;
+	accounts?: ReadonlyWalletAccount[];
+	additionalFeatures?: IdentifierRecord<unknown>;
+}) {
+	const walletsApi = getWallets();
+	const mockWallet = new MockWallet(walletName, accounts, additionalFeatures);
+	return {
+		unregister: walletsApi.register(mockWallet),
+		mockWallet,
+	};
+}
