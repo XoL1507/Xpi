086e4287dca796a5cd544481f62972f36b19d5b0
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 20:25:02 +0000

    Initialize jordan--explorer-max-height

---
+++ apps/explorer/src/pages/transaction-result/TransactionView.tsx
@@ -0,0 +1,152 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+    getExecutionStatusError,
+    getExecutionStatusType,
+    getTransactionDigest,
+    getTransactionKind,
+    getTransactionKindName,
+    type SuiTransactionBlockResponse,
+} from '@mysten/sui.js';
+import clsx from 'clsx';
+
+// import {
+//     eventToDisplay,
+//     getAddressesLinks,
+// } from '../../components/events/eventDisplay';
+
+import { Signatures } from './Signatures';
+
+import styles from './TransactionResult.module.css';
+
+import { useBreakpoint } from '~/hooks/useBreakpoint';
+import { TransactionData } from '~/pages/transaction-result/TransactionData';
+import { TransactionSummary } from '~/pages/transaction-result/transaction-summary';
+import { Banner } from '~/ui/Banner';
+import { PageHeader } from '~/ui/PageHeader';
+import { SplitPanes } from '~/ui/SplitPanes';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '~/ui/Tabs';
+
+export function TransactionView({
+    transaction,
+}: {
+    transaction: SuiTransactionBlockResponse;
+}) {
+    const isMediumOrAbove = useBreakpoint('md');
+
+    // const txKindData = formatByTransactionKind(txKindName, txnDetails, sender);
+    // const txEventData = transaction.events?.map(eventToDisplay);
+
+    // MUSTFIX(chris): re-enable event display
+    // let eventTitles: [string, string][] = [];
+    // const txEventDisplay = txEventData?.map((ed, index) => {
+    //     if (!ed) return <div />;
+
+    //     let key = ed.top.title + index;
+    //     eventTitles.push([ed.top.title, key]);
+    //     return (
+    //         <div className={styles.txgridcomponent} key={key}>
+    //             <ItemView data={ed.top as TxItemView} />
+    //             {ed.fields && <ItemView data={ed.fields as TxItemView} />}
+    //         </div>
+    //     );
+    // });
+
+    // let eventTitlesDisplay = eventTitles.map(([title, key]) => (
+    //     <div key={key} className={styles.eventtitle}>
+    //         {title}
+    //     </div>
+    // ));
+
+    // MUSTFIX(chris): re-enable event display
+    // const hasEvents = txEventData && txEventData.length > 0;
+    const hasEvents = false;
+
+    const txError = getExecutionStatusError(transaction);
+
+    const transactionKindName = getTransactionKindName(
+        getTransactionKind(transaction)!
+    );
+
+    const isProgrammableTransaction =
+        transactionKindName === 'ProgrammableTransaction';
+
+    const leftPane = {
+        panel: (
+            <div className="h-full overflow-y-scroll rounded-2xl border border-transparent bg-gray-40 p-6 md:p-10">
+                <TabGroup size="lg">
+                    <TabList>
+                        <Tab>Summary</Tab>
+                        {hasEvents && <Tab>Events</Tab>}
+                        {isProgrammableTransaction && <Tab>Signatures</Tab>}
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <div className="mt-10">
+                                <TransactionSummary transaction={transaction} />
+                            </div>
+                        </TabPanel>
+                        {/* {hasEvents && (
+                        <TabPanel>
+                            <div className={styles.txevents}>
+                                <div className={styles.txeventsleft}>
+                                    {eventTitlesDisplay}
+                                </div>
+                                <div className={styles.txeventsright}>
+                                    {txEventDisplay}
+                                </div>
+                            </div>
+                        </TabPanel>
+                    )} */}
+                        <TabPanel>
+                            <Signatures transaction={transaction} />
+                        </TabPanel>
+                    </TabPanels>
+                </TabGroup>
+            </div>
+        ),
+        minSize: 35,
+        collapsible: true,
+        collapsibleButton: true,
+        noHoverHidden: isMediumOrAbove,
+    };
+
+    const rightPane = {
+        panel: (
+            <div className="h-full w-full overflow-y-scroll md:overflow-y-hidden">
+                <TransactionData transaction={transaction} />
+            </div>
+        ),
+        minSize: 40,
+        defaultSize: isProgrammableTransaction ? 65 : 50,
+    };
+
+    return (
+        <div className={clsx(styles.txdetailsbg)}>
+            <div className="mb-10">
+                <PageHeader
+                    type="Transaction"
+                    title={getTransactionDigest(transaction)}
+                    subtitle={
+                        !isProgrammableTransaction
+                            ? transactionKindName
+                            : undefined
+                    }
+                    status={getExecutionStatusType(transaction)}
+                />
+                {txError && (
+                    <div className="mt-2">
+                        <Banner variant="error">{txError}</Banner>
+                    </div>
+                )}
+            </div>
+            <div className="h-verticalListLong md:h-full">
+                <SplitPanes
+                    splitPanels={[leftPane, rightPane]}
+                    direction={isMediumOrAbove ? 'horizontal' : 'vertical'}
+                />
+            </div>
+        </div>
+    );
+}
---
+++ crates/sui-types/src/signature.rs
@@ -0,0 +1,119 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::crypto::{SignatureScheme, SuiSignature};
+use crate::{base_types::SuiAddress, crypto::Signature, error::SuiError, multisig::MultiSig};
+pub use enum_dispatch::enum_dispatch;
+use fastcrypto::{
+    error::FastCryptoError,
+    traits::{EncodeDecodeBase64, ToFromBytes},
+};
+use schemars::JsonSchema;
+use serde::Serialize;
+use shared_crypto::intent::IntentMessage;
+use std::hash::Hash;
+
+/// A lightweight trait that all members of [enum GenericSignature] implement.
+#[enum_dispatch]
+pub trait AuthenticatorTrait {
+    fn verify_secure_generic<T>(
+        &self,
+        value: &IntentMessage<T>,
+        author: SuiAddress,
+    ) -> Result<(), SuiError>
+    where
+        T: Serialize;
+}
+
+/// Due to the incompatibility of [enum Signature] (which dispatches a trait that
+/// assumes signature and pubkey bytes for verification), here we add a wrapper
+/// enum where member can just implement a lightweight [trait AuthenticatorTrait].
+/// This way MultiSig (and future Authenticators) can implement its own `verify`.
+#[enum_dispatch(AuthenticatorTrait)]
+#[derive(Debug, Clone, PartialEq, Eq, JsonSchema, Hash)]
+pub enum GenericSignature {
+    MultiSig,
+    Signature,
+}
+
+/// GenericSignature encodes a single signature [enum Signature] as is `flag || signature || pubkey`.
+/// It encodes [struct MultiSig] as the MultiSig flag (0x03) concat with the bcs serializedbytes
+/// of [struct MultiSig] i.e. `flag || bcs_bytes(MultiSig)`.
+impl ToFromBytes for GenericSignature {
+    fn from_bytes(bytes: &[u8]) -> Result<Self, FastCryptoError> {
+        match SignatureScheme::from_flag_byte(
+            bytes.first().ok_or(FastCryptoError::InputTooShort(0))?,
+        ) {
+            Ok(x) => match x {
+                SignatureScheme::ED25519
+                | SignatureScheme::Secp256k1
+                | SignatureScheme::Secp256r1 => Ok(GenericSignature::Signature(
+                    Signature::from_bytes(bytes).map_err(|_| FastCryptoError::InvalidSignature)?,
+                )),
+                SignatureScheme::MultiSig => {
+                    let multisig = MultiSig::from_bytes(bytes)?;
+                    Ok(GenericSignature::MultiSig(multisig))
+                }
+                _ => Err(FastCryptoError::InvalidInput),
+            },
+            Err(_) => Err(FastCryptoError::InvalidInput),
+        }
+    }
+}
+
+/// Trait useful to get the bytes reference for [enum GenericSignature].
+impl AsRef<[u8]> for GenericSignature {
+    fn as_ref(&self) -> &[u8] {
+        match self {
+            GenericSignature::MultiSig(s) => s.as_ref(),
+            GenericSignature::Signature(s) => s.as_ref(),
+        }
+    }
+}
+
+impl ::serde::Serialize for GenericSignature {
+    fn serialize<S: ::serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        if serializer.is_human_readable() {
+            #[derive(serde::Serialize)]
+            struct GenericSignature(String);
+            GenericSignature(self.encode_base64()).serialize(serializer)
+        } else {
+            #[derive(serde::Serialize)]
+            struct GenericSignature<'a>(&'a [u8]);
+            GenericSignature(self.as_ref()).serialize(serializer)
+        }
+    }
+}
+
+impl<'de> ::serde::Deserialize<'de> for GenericSignature {
+    fn deserialize<D: ::serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        use serde::de::Error;
+
+        if deserializer.is_human_readable() {
+            #[derive(serde::Deserialize)]
+            struct GenericSignature(String);
+            let s = GenericSignature::deserialize(deserializer)?;
+            Self::decode_base64(&s.0).map_err(::serde::de::Error::custom)
+        } else {
+            #[derive(serde::Deserialize)]
+            struct GenericSignature(Vec<u8>);
+
+            let data = GenericSignature::deserialize(deserializer)?;
+            Self::from_bytes(&data.0).map_err(|e| Error::custom(e.to_string()))
+        }
+    }
+}
+
+/// This ports the wrapper trait to the verify_secure defined on [enum Signature].
+impl AuthenticatorTrait for Signature {
+    fn verify_secure_generic<T>(
+        &self,
+        value: &IntentMessage<T>,
+        author: SuiAddress,
+    ) -> Result<(), SuiError>
+    where
+        T: Serialize,
+    {
+        self.verify_secure(value, author)
+    }
+}
