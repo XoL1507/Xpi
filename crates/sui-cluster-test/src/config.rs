6749d5a1b5b63c416e4c1d1bd73cf1edc9c71794
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:35:17 +0400

    Initialize jonas/debug

---
+++ crates/sui-cluster-test/src/config.rs
@@ -0,0 +1,38 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+use clap::*;
+
+#[derive(Parser, Clone, ArgEnum)]
+pub enum Env {
+    Devnet,
+    Staging,
+    Ci,
+    CiNomad,
+    Testnet,
+    CustomRemote,
+    NewLocal,
+}
+
+#[derive(Parser)]
+#[clap(name = "", rename_all = "kebab-case")]
+pub struct ClusterTestOpt {
+    #[clap(arg_enum)]
+    pub env: Env,
+    #[clap(long)]
+    pub faucet_address: Option<String>,
+    #[clap(long)]
+    pub fullnode_address: Option<String>,
+    #[clap(long)]
+    pub epoch_duration_ms: Option<u64>,
+}
+
+impl ClusterTestOpt {
+    pub fn new_local() -> Self {
+        Self {
+            env: Env::NewLocal,
+            faucet_address: None,
+            fullnode_address: None,
+            epoch_duration_ms: None,
+        }
+    }
+}
