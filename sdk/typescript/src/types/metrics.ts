ca8341f3510620384c9108e19a43bb2d95322d61
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:28:36 +0000

    Initialize mlegner/ci-fix-changeset-version

---
+++ sdk/typescript/src/types/metrics.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { number, object, string } from 'superstruct';
+
+export const NetworkMetrics = object({
+	currentTps: number(),
+	tps30Days: number(),
+	currentCheckpoint: string(),
+	currentEpoch: string(),
+	totalAddresses: string(),
+	totalObjects: string(),
+	totalPackages: string(),
+});
+export type NetworkMetrics = typeof NetworkMetrics.TYPE;
+
+export const AddressMetrics = object({
+	checkpoint: number(),
+	epoch: number(),
+	timestampMs: number(),
+	cumulativeAddresses: number(),
+	cumulativeActiveAddresses: number(),
+	dailyActiveAddresses: number(),
+});
+export type AddressMetrics = typeof AddressMetrics.TYPE;
