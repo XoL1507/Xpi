42b7ae794f859a80d8d5ddff03c3d21f4fce5d26
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:00:06 +0000

    Initialize faucet-fix

---
+++ crates/sui-graphql-rpc/src/types/balance.rs
@@ -0,0 +1,12 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use super::big_int::BigInt;
+use async_graphql::*;
+
+#[derive(Clone, Debug, PartialEq, Eq, SimpleObject)]
+pub(crate) struct Balance {
+    // pub(crate) coin_type: MoveType,
+    pub(crate) coin_object_count: u64,
+    pub(crate) total_balance: BigInt,
+}
