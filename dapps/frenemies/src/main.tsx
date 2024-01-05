4f6b5a3ccb79df9b19e106aaf22a807de9de3e79
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 22:15:51 +0400

    Initialize chris-test-commit

---
+++ dapps/frenemies/src/main.tsx
@@ -0,0 +1,68 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import "@fontsource/inter/variable.css";
+import "./index.css";
+import Plausible from "plausible-tracker";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+import { Root } from "./routes/Root";
+// import { Home } from "./routes/Home";
+// import { Connect } from "./routes/Connect";
+// import { Setup } from "./routes/Setup";
+import { toast } from "react-hot-toast";
+import { Offline } from "./routes/Offline";
+
+const plausible = Plausible({});
+plausible.enableAutoPageviews();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      onError(error) {
+        toast.error(String(error));
+      },
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: '', element: <Offline />},
+      // {
+      //   path: "",
+      //   element: <Home />,
+      // },
+      // {
+      //   path: "connect",
+      //   element: <Connect />,
+      // },
+      // {
+      //   path: "setup",
+      //   element: <Setup />,
+      // },
+      {
+        path: "*",
+        element: <Navigate to="/" replace />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
