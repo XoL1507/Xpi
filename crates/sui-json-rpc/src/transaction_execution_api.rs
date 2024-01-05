48b325b59d7cbc26d3c562db673af348066e61b3
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:20:45 +0000

    Initialize balance-check-1-3

---
+++ crates/sui-json-rpc/src/transaction_execution_api.rs
@@ -0,0 +1,262 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use fastcrypto::encoding::Base64;
+use fastcrypto::traits::ToFromBytes;
+use jsonrpsee::core::RpcResult;
+use jsonrpsee::RpcModule;
+
+use mysten_metrics::spawn_monitored_task;
+use shared_crypto::intent::Intent;
+use sui_core::authority::AuthorityState;
+use sui_core::authority_client::NetworkAuthorityClient;
+use sui_core::transaction_orchestrator::TransactiondOrchestrator;
+use sui_json_rpc_types::{
+    DevInspectResults, DryRunTransactionBlockResponse, SuiTransactionBlock,
+    SuiTransactionBlockEvents, SuiTransactionBlockResponse, SuiTransactionBlockResponseOptions,
+};
+use sui_open_rpc::Module;
+use sui_types::base_types::SuiAddress;
+use sui_types::effects::TransactionEffectsAPI;
+use sui_types::quorum_driver_types::{
+    ExecuteTransactionRequest, ExecuteTransactionRequestType, ExecuteTransactionResponse,
+};
+use sui_types::signature::GenericSignature;
+use sui_types::sui_serde::BigInt;
+use sui_types::transaction::{Transaction, TransactionData, TransactionDataAPI, TransactionKind};
+use tracing::instrument;
+
+use crate::api::JsonRpcMetrics;
+use crate::api::WriteApiServer;
+use crate::error::{Error, SuiRpcInputError};
+use crate::read_api::get_transaction_data_and_digest;
+use crate::{
+    get_balance_changes_from_effect, get_object_changes, with_tracing, ObjectProviderCache,
+    SuiRpcModule,
+};
+
+pub struct TransactionExecutionApi {
+    state: Arc<AuthorityState>,
+    transaction_orchestrator: Arc<TransactiondOrchestrator<NetworkAuthorityClient>>,
+    metrics: Arc<JsonRpcMetrics>,
+}
+
+impl TransactionExecutionApi {
+    pub fn new(
+        state: Arc<AuthorityState>,
+        transaction_orchestrator: Arc<TransactiondOrchestrator<NetworkAuthorityClient>>,
+        metrics: Arc<JsonRpcMetrics>,
+    ) -> Self {
+        Self {
+            state,
+            transaction_orchestrator,
+            metrics,
+        }
+    }
+
+    async fn execute_transaction_block(
+        &self,
+        tx_bytes: Base64,
+        signatures: Vec<Base64>,
+        opts: Option<SuiTransactionBlockResponseOptions>,
+        request_type: Option<ExecuteTransactionRequestType>,
+    ) -> Result<SuiTransactionBlockResponse, Error> {
+        let opts = opts.unwrap_or_default();
+
+        let request_type = match (request_type, opts.require_local_execution()) {
+            (Some(ExecuteTransactionRequestType::WaitForEffectsCert), true) => {
+                return Err(Error::SuiRpcInputError(
+                    SuiRpcInputError::InvalidExecuteTransactionRequestType,
+                ));
+            }
+            (t, _) => t.unwrap_or_else(|| opts.default_execution_request_type()),
+        };
+        let tx_data: TransactionData = bcs::from_bytes(&tx_bytes.to_vec()?)?;
+        let sender = tx_data.sender();
+        let input_objs = tx_data.input_objects().unwrap_or_default();
+
+        let mut sigs = Vec::new();
+        for sig in signatures {
+            sigs.push(GenericSignature::from_bytes(&sig.to_vec()?)?);
+        }
+        let txn = Transaction::from_generic_sig_data(tx_data, Intent::sui_transaction(), sigs);
+        let digest = *txn.digest();
+        let raw_transaction = if opts.show_raw_input {
+            bcs::to_bytes(txn.data())?
+        } else {
+            vec![]
+        };
+        let transaction = if opts.show_input {
+            let epoch_store = self.state.load_epoch_store_one_call_per_task();
+            Some(SuiTransactionBlock::try_from(
+                txn.data().clone(),
+                epoch_store.module_cache(),
+            )?)
+        } else {
+            None
+        };
+
+        let transaction_orchestrator = self.transaction_orchestrator.clone();
+        let orch_timer = self.metrics.orchestrator_latency_ms.start_timer();
+        let response = spawn_monitored_task!(transaction_orchestrator.execute_transaction_block(
+            ExecuteTransactionRequest {
+                transaction: txn,
+                request_type,
+            }
+        ))
+        .await?
+        .map_err(Error::from)?;
+        drop(orch_timer);
+
+        let _post_orch_timer = self.metrics.post_orchestrator_latency_ms.start_timer();
+        let ExecuteTransactionResponse::EffectsCert(cert) = response;
+        let (effects, transaction_events, is_executed_locally) = *cert;
+        let mut events: Option<SuiTransactionBlockEvents> = None;
+        if opts.show_events {
+            let module_cache = self
+                .state
+                .load_epoch_store_one_call_per_task()
+                .module_cache()
+                .clone();
+            events = Some(SuiTransactionBlockEvents::try_from(
+                transaction_events,
+                digest,
+                None,
+                module_cache.as_ref(),
+            )?);
+        }
+
+        let object_cache = ObjectProviderCache::new(self.state.clone());
+        let balance_changes = if opts.show_balance_changes && is_executed_locally {
+            Some(
+                get_balance_changes_from_effect(&object_cache, &effects.effects, input_objs, None)
+                    .await?,
+            )
+        } else {
+            None
+        };
+        let object_changes = if opts.show_object_changes && is_executed_locally {
+            Some(
+                get_object_changes(
+                    &object_cache,
+                    sender,
+                    effects.effects.modified_at_versions(),
+                    effects.effects.all_changed_objects(),
+                    effects.effects.all_deleted(),
+                )
+                .await?,
+            )
+        } else {
+            None
+        };
+
+        Ok(SuiTransactionBlockResponse {
+            digest,
+            transaction,
+            raw_transaction,
+            effects: opts.show_effects.then_some(effects.effects.try_into()?),
+            events,
+            object_changes,
+            balance_changes,
+            timestamp_ms: None,
+            confirmed_local_execution: Some(is_executed_locally),
+            checkpoint: None,
+            errors: vec![],
+        })
+    }
+
+    async fn dry_run_transaction_block(
+        &self,
+        tx_bytes: Base64,
+    ) -> Result<DryRunTransactionBlockResponse, Error> {
+        let (txn_data, txn_digest) = get_transaction_data_and_digest(tx_bytes)?;
+        let input_objs = txn_data.input_objects()?;
+        let sender = txn_data.sender();
+        let (resp, written_objects, transaction_effects, mock_gas) = self
+            .state
+            .dry_exec_transaction(txn_data.clone(), txn_digest)
+            .await?;
+        let object_cache = ObjectProviderCache::new_with_cache(self.state.clone(), written_objects);
+        let balance_changes = get_balance_changes_from_effect(
+            &object_cache,
+            &transaction_effects,
+            input_objs,
+            mock_gas,
+        )
+        .await?;
+        let object_changes = get_object_changes(
+            &object_cache,
+            sender,
+            transaction_effects.modified_at_versions(),
+            transaction_effects.all_changed_objects(),
+            transaction_effects.all_deleted(),
+        )
+        .await?;
+
+        Ok(DryRunTransactionBlockResponse {
+            effects: resp.effects,
+            events: resp.events,
+            object_changes,
+            balance_changes,
+            input: resp.input,
+        })
+    }
+}
+
+#[async_trait]
+impl WriteApiServer for TransactionExecutionApi {
+    #[instrument(skip(self))]
+    async fn execute_transaction_block(
+        &self,
+        tx_bytes: Base64,
+        signatures: Vec<Base64>,
+        opts: Option<SuiTransactionBlockResponseOptions>,
+        request_type: Option<ExecuteTransactionRequestType>,
+    ) -> RpcResult<SuiTransactionBlockResponse> {
+        with_tracing!(async move {
+            Ok(self
+                .execute_transaction_block(tx_bytes, signatures, opts, request_type)
+                .await?)
+        })
+    }
+
+    #[instrument(skip(self))]
+    async fn dev_inspect_transaction_block(
+        &self,
+        sender_address: SuiAddress,
+        tx_bytes: Base64,
+        gas_price: Option<BigInt<u64>>,
+        _epoch: Option<BigInt<u64>>,
+    ) -> RpcResult<DevInspectResults> {
+        with_tracing!(async move {
+            let tx_kind: TransactionKind =
+                bcs::from_bytes(&tx_bytes.to_vec().map_err(Error::from)?).map_err(Error::from)?;
+            Ok(self
+                .state
+                .dev_inspect_transaction_block(sender_address, tx_kind, gas_price.map(|i| *i))
+                .await
+                .map_err(Error::from)?)
+        })
+    }
+
+    #[instrument(skip(self))]
+    async fn dry_run_transaction_block(
+        &self,
+        tx_bytes: Base64,
+    ) -> RpcResult<DryRunTransactionBlockResponse> {
+        with_tracing!(async move { Ok(self.dry_run_transaction_block(tx_bytes).await?) })
+    }
+}
+
+impl SuiRpcModule for TransactionExecutionApi {
+    fn rpc(self) -> RpcModule<Self> {
+        self.into_rpc()
+    }
+
+    fn rpc_doc_module() -> Module {
+        crate::api::WriteApiOpenRpc::module_doc()
+    }
+}
