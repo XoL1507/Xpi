3fd8fe1bdf440f44fd7261124c10a2dad0ddbbe6
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:26:35 +0000

    Initialize rpc-level-sui-error-cleanup

---
+++ crates/sui-indexer/src/metrics.rs
@@ -0,0 +1,359 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use prometheus::{
+    register_histogram_with_registry, register_int_counter_with_registry,
+    register_int_gauge_with_registry, Histogram, IntCounter, IntGauge, Registry,
+};
+
+/// Prometheus metrics for sui-indexer.
+// buckets defined in seconds
+const LATENCY_SEC_BUCKETS: &[f64] = &[
+    0.001, 0.005, 0.01, 0.02, 0.05, 0.1, 0.25, 0.5, 1.0, 2.0, 3.0, 5.0, 10.0, 20.0, 40.0, 60.0,
+    80.0, 100.0, 200.0,
+];
+
+#[derive(Clone)]
+pub struct IndexerMetrics {
+    pub total_checkpoint_received: IntCounter,
+    pub total_checkpoint_committed: IntCounter,
+    pub total_object_checkpoint_committed: IntCounter,
+    pub total_transaction_committed: IntCounter,
+    pub total_object_change_committed: IntCounter,
+    pub total_epoch_committed: IntCounter,
+    pub latest_fullnode_checkpoint_sequence_number: IntGauge,
+    pub latest_indexer_checkpoint_sequence_number: IntGauge,
+    pub latest_indexer_object_checkpoint_sequence_number: IntGauge,
+    // checkpoint E2E latency is:
+    // fullnode_download_latency + checkpoint_index_latency + db_commit_latency
+    pub fullnode_checkpoint_wait_and_download_latency: Histogram,
+    pub fullnode_checkpoint_download_latency: Histogram,
+    pub fullnode_transaction_download_latency: Histogram,
+    pub fullnode_object_download_latency: Histogram,
+    pub checkpoint_index_latency: Histogram,
+    pub checkpoint_db_commit_latency: Histogram,
+    pub object_db_commit_latency: Histogram,
+    pub object_mutation_db_commit_latency: Histogram,
+    pub object_deletion_db_commit_latency: Histogram,
+    pub epoch_db_commit_latency: Histogram,
+    // latency of event websocket subscription
+    pub subscription_process_latency: Histogram,
+    pub transaction_per_checkpoint: Histogram,
+    // FN RPC latencies on the read path
+    // read.rs
+    pub get_transaction_block_latency: Histogram,
+    pub multi_get_transaction_blocks_latency: Histogram,
+    pub get_object_latency: Histogram,
+    pub multi_get_objects_latency: Histogram,
+    pub try_get_past_object_latency: Histogram,
+    pub try_multi_get_past_objects_latency: Histogram,
+    pub get_checkpoint_latency: Histogram,
+    pub get_checkpoints_latency: Histogram,
+    pub get_events_latency: Histogram,
+    pub get_loaded_child_objects_latency: Histogram,
+    pub get_total_transaction_blocks_latency: Histogram,
+    pub get_latest_checkpoint_sequence_number_latency: Histogram,
+    // indexer.rs
+    pub get_owned_objects_latency: Histogram,
+    pub query_transaction_blocks_latency: Histogram,
+    pub query_events_latency: Histogram,
+    pub get_dynamic_fields_latency: Histogram,
+    pub get_dynamic_field_object_latency: Histogram,
+    pub get_protocol_config_latency: Histogram,
+}
+
+impl IndexerMetrics {
+    pub fn new(registry: &Registry) -> Self {
+        Self {
+            total_checkpoint_received: register_int_counter_with_registry!(
+                "total_checkpoint_received",
+                "Total number of checkpoint received",
+                registry,
+            )
+            .unwrap(),
+            total_checkpoint_committed: register_int_counter_with_registry!(
+                "total_checkpoint_committed",
+                "Total number of checkpoint committed",
+                registry,
+            )
+            .unwrap(),
+            total_object_checkpoint_committed: register_int_counter_with_registry!(
+                "total_object_checkpoint_committed",
+                "Total number of object checkpoint committed",
+                registry,
+            )
+            .unwrap(),
+            total_transaction_committed: register_int_counter_with_registry!(
+                "total_transaction_committed",
+                "Total number of transaction committed",
+                registry,
+            )
+            .unwrap(),
+            total_object_change_committed: register_int_counter_with_registry!(
+                "total_object_change_committed",
+                "Total number of object change committed",
+                registry,
+            )
+            .unwrap(),
+            total_epoch_committed: register_int_counter_with_registry!(
+                "total_epoch_committed",
+                "Total number of epoch committed",
+                registry,
+            )
+            .unwrap(),
+            latest_fullnode_checkpoint_sequence_number: register_int_gauge_with_registry!(
+                "latest_fullnode_checkpoint_sequence_number",
+                "Latest checkpoint sequence number from the Full Node",
+                registry,
+            )
+            .unwrap(),
+            latest_indexer_checkpoint_sequence_number: register_int_gauge_with_registry!(
+                "latest_indexer_checkpoint_sequence_number",
+                "Latest checkpoint sequence number from the Indexer",
+                registry,
+            )
+            .unwrap(),
+            latest_indexer_object_checkpoint_sequence_number: register_int_gauge_with_registry!(
+                "latest_indexer_object_checkpoint_sequence_number",
+                "Latest object checkpoint sequence number from the Indexer",
+                registry,
+            )
+            .unwrap(),
+            fullnode_checkpoint_wait_and_download_latency: register_histogram_with_registry!(
+                "fullnode_checkpoint_wait_and_download_latency",
+                "Time spent in waiting for a new checkpoint from the Full Node",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            fullnode_checkpoint_download_latency: register_histogram_with_registry!(
+                "fullnode_checkpoint_download_latency",
+                "Time spent in waiting for a new checkpoint from the Full Node",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            fullnode_transaction_download_latency: register_histogram_with_registry!(
+                "fullnode_transaction_download_latency",
+                "Time spent in waiting for a new transaction from the Full Node",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            fullnode_object_download_latency: register_histogram_with_registry!(
+                "fullnode_object_download_latency",
+                "Time spent in waiting for a new epoch from the Full Node",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            checkpoint_index_latency: register_histogram_with_registry!(
+                "checkpoint_index_latency",
+                "Time spent in indexing a checkpoint",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            checkpoint_db_commit_latency: register_histogram_with_registry!(
+                "checkpoint_db_commit_latency",
+                "Time spent commiting a checkpoint to the db",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            object_db_commit_latency: register_histogram_with_registry!(
+                "object_db_commit_latency",
+                "Time spent commiting a object to the db",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            object_mutation_db_commit_latency: register_histogram_with_registry!(
+                "object_mutation_db_commit_latency",
+                "Time spent commiting a object mutation to the db",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            object_deletion_db_commit_latency: register_histogram_with_registry!(
+                "object_deletion_db_commit_latency",
+                "Time spent commiting a object deletion to the db",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            epoch_db_commit_latency: register_histogram_with_registry!(
+                "epoch_db_commit_latency",
+                "Time spent commiting a epoch to the db",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            subscription_process_latency: register_histogram_with_registry!(
+                "subscription_process_latency",
+                "Time spent in process Websocket subscription",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry,
+            )
+            .unwrap(),
+            transaction_per_checkpoint: register_histogram_with_registry!(
+                "transaction_per_checkpoint",
+                "Number of transactions per checkpoint",
+                vec![1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0, 1000.0, 2000.0, 5000.0],
+                registry,
+            )
+            .unwrap(),
+            get_transaction_block_latency: register_histogram_with_registry!(
+                "get_transaction_block_latency",
+                "Time spent in get_transaction_block on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            multi_get_transaction_blocks_latency: register_histogram_with_registry!(
+                "multi_get_transaction_blocks_latency",
+                "Time spent in multi_get_transaction_blocks on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_object_latency: register_histogram_with_registry!(
+                "get_object_latency",
+                "Time spent in get_object on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            multi_get_objects_latency: register_histogram_with_registry!(
+                "multi_get_objects_latency",
+                "Time spent in multi_get_objects on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            try_get_past_object_latency: register_histogram_with_registry!(
+                "try_get_past_object_latency",
+                "Time spent in try_get_past_object on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            try_multi_get_past_objects_latency: register_histogram_with_registry!(
+                "try_multi_get_past_objects_latency",
+                "Time spent in try_multi_get_past_objects on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_checkpoint_latency: register_histogram_with_registry!(
+                "get_checkpoint_latency",
+                "Time spent in get_checkpoint on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_checkpoints_latency: register_histogram_with_registry!(
+                "get_checkpoints_latency",
+                "Time spent in get_checkpoints on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_events_latency: register_histogram_with_registry!(
+                "get_events_latency",
+                "Time spent in get_events on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_total_transaction_blocks_latency: register_histogram_with_registry!(
+                "get_total_transaction_blocks_latency",
+                "Time spent in get_total_transaction_blocks on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_latest_checkpoint_sequence_number_latency: register_histogram_with_registry!(
+                "get_latest_checkpoint_sequence_number_latency",
+                "Time spent in get_latest_checkpoint_sequence_number on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_owned_objects_latency: register_histogram_with_registry!(
+                "get_owned_objects_latency",
+                "Time spent in get_owned_objects on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            query_transaction_blocks_latency: register_histogram_with_registry!(
+                "query_transaction_blocks_latency",
+                "Time spent in query_transaction_blocks on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            query_events_latency: register_histogram_with_registry!(
+                "query_events_latency",
+                "Time spent in query_events on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_dynamic_fields_latency: register_histogram_with_registry!(
+                "get_dynamic_fields_latency",
+                "Time spent in get_dynamic_fields on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_dynamic_field_object_latency: register_histogram_with_registry!(
+                "get_dynamic_field_object_latency",
+                "Time spent in get_dynamic_field_object on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_loaded_child_objects_latency: register_histogram_with_registry!(
+                "get_loaded_child_objects_latency",
+                "Time spent in get_loaded_child_objects_latency on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+            get_protocol_config_latency: register_histogram_with_registry!(
+                "get_protocol_config_latency",
+                "Time spent in get_protocol_config_latency on the fullnode behind.",
+                LATENCY_SEC_BUCKETS.to_vec(),
+                registry
+            )
+            .unwrap(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct IndexerObjectProcessorMetrics {
+    pub total_object_batch_processed: IntCounter,
+    pub total_object_processor_error: IntCounter,
+}
+
+impl IndexerObjectProcessorMetrics {
+    pub fn new(registry: &Registry) -> Self {
+        Self {
+            total_object_batch_processed: register_int_counter_with_registry!(
+                "total_object_batch_processed",
+                "Total number of object batches processed",
+                registry,
+            )
+            .unwrap(),
+            total_object_processor_error: register_int_counter_with_registry!(
+                "total_object_processor_error",
+                "Total number of object processor error",
+                registry,
+            )
+            .unwrap(),
+        }
+    }
+}
---
+++ crates/sui-json-rpc/src/error.rs
@@ -0,0 +1,212 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use fastcrypto::error::FastCryptoError;
+use hyper::header::InvalidHeaderValue;
+use jsonrpsee::core::Error as RpcError;
+use jsonrpsee::types::error::CallError;
+use jsonrpsee::types::ErrorObject;
+use std::fmt;
+use strum_macros::{AsRefStr, EnumString};
+use sui_types::error::{SuiError, SuiObjectResponseError, UserInputError};
+use sui_types::quorum_driver_types::QuorumDriverError;
+use thiserror::Error;
+use tokio::task::JoinError;
+
+use crate::name_service::DomainParseError;
+
+pub type RpcInterimResult<T = ()> = Result<T, Error>;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    SuiError(#[from] SuiError),
+
+    #[error(transparent)]
+    InternalError(#[from] anyhow::Error),
+
+    #[error("Unexpected error: {0}")]
+    UnexpectedError(String),
+
+    #[error(transparent)]
+    RPCServerError(#[from] jsonrpsee::core::Error),
+
+    #[error(transparent)]
+    InvalidHeaderValue(#[from] InvalidHeaderValue),
+
+    #[error(transparent)]
+    UserInputError(#[from] UserInputError),
+
+    #[error(transparent)]
+    EncodingError(#[from] eyre::Report),
+
+    #[error(transparent)]
+    TokioJoinError(#[from] JoinError),
+
+    #[error(transparent)]
+    QuorumDriverError(#[from] QuorumDriverError),
+
+    #[error(transparent)]
+    FastCryptoError(#[from] FastCryptoError),
+
+    #[error(transparent)]
+    SuiObjectResponseError(#[from] SuiObjectResponseError),
+
+    #[error(transparent)]
+    ClientError(#[from] ClientError),
+
+    #[error(transparent)]
+    ServerError(#[from] ServerError),
+}
+
+impl From<Error> for RpcError {
+    fn from(e: Error) -> Self {
+        e.to_rpc_error()
+    }
+}
+
+impl Error {
+    pub fn to_rpc_error(self) -> RpcError {
+        match self {
+            Error::UserInputError(user_input_error) => {
+                RpcError::Call(CallError::InvalidParams(user_input_error.into()))
+            }
+            Error::SuiError(sui_error) => match sui_error {
+                SuiError::TransactionNotFound { .. } | SuiError::TransactionsNotFound { .. } => {
+                    RpcError::Call(CallError::InvalidParams(sui_error.into()))
+                }
+                _ => RpcError::Call(CallError::Failed(sui_error.into())),
+            },
+            Error::QuorumDriverError(err) => match err {
+                QuorumDriverError::NonRecoverableTransactionError { errors } => {
+                    let error_object = ErrorObject::owned(
+                        -32000,
+                        "Transaction has non recoverable errors from at least 1/3 of validators",
+                        Some(errors),
+                    );
+                    RpcError::Call(CallError::Custom(error_object))
+                }
+                _ => RpcError::Call(CallError::Failed(err.into())),
+            },
+            Error::ClientError(err) => err.into(),
+            Error::ServerError(err) => err.into(),
+            _ => RpcError::Call(CallError::Failed(self.into())),
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ServerError {
+    // do we really need these variants for server-side errors?
+    #[error("Serde error")]
+    Serde,
+
+    #[error("Unexpected error")]
+    Unexpected,
+}
+
+impl From<ServerError> for RpcError {
+    fn from(_e: ServerError) -> Self {
+        to_internal_error("Internal server error, please try again later")
+    }
+}
+
+fn to_internal_error(err: impl ToString) -> RpcError {
+    let error_object = ErrorObject::owned(-32603, err.to_string(), None::<String>);
+    RpcError::Call(CallError::Custom(error_object))
+}
+
+// One-to-one of the return type of the RPC method where applicable
+#[derive(Debug, Clone, Copy, AsRefStr, EnumString)]
+pub enum EntityType {
+    DelegatedStake,
+    SuiMoveNormalizedStruct,
+    SuiMoveNormalizedFunction,
+    Object,
+    Package,
+    Product,
+    SuiMoveNormalizedModule,
+    Checkpoint,
+}
+
+impl fmt::Display for EntityType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_ref())
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum InvalidReasons {
+    #[error("`exceeds limit of `{0}`")]
+    LimitExceeded(usize),
+
+    #[error("must be at least `{0}`")]
+    LimitTooSmall(usize),
+}
+
+#[derive(Debug, Error)]
+pub enum ClientError {
+    // Any kind of serialization or deserialization error
+    #[error("Serialization error on '{param}': {reason}")]
+    Serde { param: &'static str, reason: String },
+
+    #[error("Invalid combination of type and value: {0}")]
+    SerdeWithLayout(String),
+
+    #[error(transparent)]
+    Domain(#[from] DomainParseError),
+
+    #[error("Invalid '{param}': {reason}")]
+    InvalidParam { param: &'static str, reason: String },
+    // maybe InvalidParamMulti or something. param, value, reason
+    #[error("`request_type` must set to `None` or `WaitForLocalExecution` if effects is required in the response")]
+    InvalidExecuteTransactionRequestType,
+
+    #[error("{entity} '{id}' not found")]
+    NotFound { entity: EntityType, id: String },
+
+    #[error("{entity} '{id}' not found in {in_entity} '{in_id}'")]
+    NotFoundIn {
+        entity: EntityType,
+        id: String,
+        in_entity: EntityType,
+        in_id: String,
+    },
+
+    // For error scenarios that don't fit cleanly into NotFound
+    #[error("{0}")]
+    NotFoundCustom(String),
+
+    #[error("`{param}` exceeds limit of `{limit}`")]
+    LimitExceeded { param: &'static str, limit: usize },
+
+    #[error("`{param}` must be at least `{limit}`")]
+    LimitTooSmall { param: &'static str, limit: usize },
+
+    #[error("Invalid 'version': unsupported protocol version requested. Min supported: {0}, max supported: {1}")]
+    ProtocolVersionUnsupported(u64, u64),
+}
+
+impl From<ClientError> for RpcError {
+    fn from(e: ClientError) -> Self {
+        RpcError::Call(CallError::InvalidParams(e.into()))
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ObjectDisplayError {
+    #[error("Not a move struct")]
+    NotMoveStruct,
+
+    #[error("Failed to extract layout")]
+    Layout,
+
+    #[error("Failed to extract Move object")]
+    MoveObject,
+
+    #[error(transparent)]
+    Deserialization(#[from] SuiError),
+
+    #[error(transparent)] // Failed to deserialize 'VersionUpdatedEvent': {e}
+    Bcs(#[from] bcs::Error),
+}
