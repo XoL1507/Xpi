aaec1d66241cfd722439ae1f2e4d687ccf6a90d3
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:49:33 +0400

    Initialize 11.0

---
+++ crates/sui/src/config/mod.rs
@@ -0,0 +1,46 @@
+// Copyright (c) 2021, Facebook, Inc. and its affiliates
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use serde::{Deserialize, Serialize};
+use serde_with::serde_as;
+use std::fmt::{Display, Formatter, Write};
+use sui_sdk::crypto::AccountKeystore;
+use sui_sdk::crypto::Keystore;
+use sui_types::base_types::*;
+
+pub use sui_config::Config;
+pub use sui_config::PersistedConfig;
+
+pub use sui_config::utils;
+use sui_sdk::ClientType;
+
+#[serde_as]
+#[derive(Serialize, Deserialize)]
+pub struct SuiClientConfig {
+    pub keystore: Keystore,
+    pub client_type: ClientType,
+    pub active_address: Option<SuiAddress>,
+}
+
+impl Config for SuiClientConfig {}
+
+impl Display for SuiClientConfig {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut writer = String::new();
+
+        writeln!(
+            writer,
+            "Managed addresses : {}",
+            self.keystore.addresses().len()
+        )?;
+        write!(writer, "Active address: ")?;
+        match self.active_address {
+            Some(r) => writeln!(writer, "{}", r)?,
+            None => writeln!(writer, "None")?,
+        };
+        writeln!(writer, "{}", self.keystore)?;
+        write!(writer, "{}", self.client_type)?;
+        write!(f, "{}", writer)
+    }
+}
---
+++ sdk/typescript/vitest.config.ts
@@ -0,0 +1,20 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    minThreads: 1,
+    maxThreads: 8,
+  },
+  resolve: {
+    alias: {
+      '@mysten/bcs': new URL('../bcs/src', import.meta.url).toString(),
+      '@mysten/sui-open-rpc': new URL(
+        '../../crates/sui-open-rpc',
+        import.meta.url
+      ).toString(),
+    },
+  },
+});
