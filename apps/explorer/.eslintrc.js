b005236a32ad51af9626cba4faa37559d249db52
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:52:13 +0400

    Initialize fix/recovery-propose-from-penultimate-round

---
+++ apps/explorer/.eslintrc.js
@@ -0,0 +1,56 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+module.exports = {
+    plugins: ['header'],
+    extends: ['react-app', 'prettier'],
+    rules: {
+        'import/order': [
+            'warn',
+            {
+                groups: [
+                    ['builtin', 'external'],
+                    ['internal', 'parent', 'sibling', 'index'],
+                    'type',
+                ],
+                pathGroups: [
+                    {
+                        pattern: '{.,..}/**/*.css',
+                        group: 'type',
+                        position: 'after',
+                    },
+                ],
+                'newlines-between': 'always',
+                alphabetize: { order: 'asc' },
+                warnOnUnassignedImports: true,
+            },
+        ],
+        'import/no-duplicates': ['error'],
+        'import/no-anonymous-default-export': 'off',
+        '@typescript-eslint/consistent-type-imports': [
+            'error',
+            {
+                prefer: 'type-imports',
+                disallowTypeAnnotations: true,
+            },
+        ],
+        'react/jsx-key': ['error', {}],
+        'header/header': [
+            2,
+            'line',
+            [
+                ' Copyright (c) Mysten Labs, Inc.',
+                ' SPDX-License-Identifier: Apache-2.0',
+            ],
+        ],
+    },
+    overrides: [
+        {
+            files: ['*.stories.*'],
+            rules: {
+                // Story files have render functions that this rule incorrectly warns on:
+                'react-hooks/rules-of-hooks': 'off',
+            },
+        },
+    ],
+};
