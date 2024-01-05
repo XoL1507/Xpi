04a7734d84a6fd987e70f300663024321b45ea6e
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:41:35 +0400

    Initialize dependabot/cargo/prettyplease-0.1.16

---
+++ explorer/client/src/utils/stringUtils.ts
@@ -0,0 +1,58 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+export function hexToAscii(hex: string) {
+    if (!hex || typeof hex != 'string') return;
+    hex = hex.replace(/^0x/, '');
+
+    var str = '';
+    for (var n = 0; n < hex.length; n += 2)
+        str += String.fromCharCode(parseInt(hex.substring(n, 2), 16));
+
+    return str;
+}
+
+export const trimStdLibPrefix = (str: string): string =>
+    str.replace(/^0x2::/, '');
+
+export const handleCoinType = (str: string): string =>
+    str === '0x2::coin::Coin<0x2::sui::SUI>'
+        ? 'SUI'
+        : str.match(/^([a-zA-Z0-9:]*)<([a-zA-Z0-9:]*)>$/)?.[2] || str;
+
+export const findIPFSvalue = (url: string): string | undefined =>
+    url.match(/^ipfs:\/\/(.*)/)?.[1];
+
+export function transformURL(url: string) {
+    const found = findIPFSvalue(url);
+    if (!found) {
+        return url;
+    }
+    return `https://ipfs.io/ipfs/${found}`;
+}
+
+export function truncate(fullStr: string, strLen: number, separator?: string) {
+    if (fullStr.length <= strLen) return fullStr;
+
+    separator = separator || '...';
+
+    const sepLen = separator.length,
+        charsToShow = strLen - sepLen,
+        frontChars = Math.ceil(charsToShow / 2),
+        backChars = Math.floor(charsToShow / 2);
+
+    return (
+        fullStr.substr(0, frontChars) +
+        separator +
+        fullStr.substr(fullStr.length - backChars)
+    );
+}
+
+/* Currently unused but potentially useful:
+ *
+ * export const isValidHttpUrl = (url: string) => {
+ *     try { new URL(url) }
+ *         catch (e) { return false }
+ *             return /^https?/.test(url);
+ *             };
+ */
