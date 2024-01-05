3337dd5ee3c26a603c38dcc0bfd30da4ad447c6e
XoL1507 <134971149+XoL1507@users.noreply.github.com>
2024-01-05 21:52:00 +0400

    Initialize replace-hex

---
+++ crates/sui-framework-build/src/lib.rs
@@ -0,0 +1,7 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod compiled_package;
+
+const SUI_PACKAGE_NAME: &str = "Sui";
+const MOVE_STDLIB_PACKAGE_NAME: &str = "MoveStdlib";
